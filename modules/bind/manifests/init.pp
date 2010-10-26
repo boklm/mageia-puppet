34584dd34de0024873d294151cb94e10295830ee
Michael Scherer <misc@mageia.org>
2010-10-26 23:54:17 +0000

    - add bind module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@27 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/bind/manifests/init.pp
@@ -0,0 +1,45 @@
+package { bind:
+    ensure => installed
+}
+
+service { named:
+    ensure => running,
+    path => "/etc/init.d/named",
+    subscribe => [ Package["bind"]]
+}
+
+file { '/etc/named.conf':
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 644,
+    require => Package["bind"],
+    content => "",
+    notify => [Service['named']]
+}
+
+define zone_master {
+    file { "/var/lib/named/var/named/master/$name.zone":
+        ensure => present,
+        owner => root,
+        group => root,
+        mode => 644,
+        content => template("bind/zones/$name.zone"),
+        require => Package[bind],
+        notify => Service[named]
+    }
+}
+
+class bind_master inherits bind_base {
+    file { '/etc/named.conf':
+        content => template("bind/named_base.conf", "bind/named_master.conf"),
+    }
+}
+
+class bind_slave inherits bind_base {
+    file { '/etc/named.conf':
+        content => template("bind/named_base.conf", "bind/named_slave.conf"),
+    }
+}
+
+
