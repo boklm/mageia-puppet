81a469a9bbfcb0fe2fb7578b81a1f38258c8b4df
Michael Scherer <misc@mageia.org>
2010-10-26 22:46:57 +0000

    - it seems that puppet requires a real name, not a provides
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@20 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -12,7 +12,7 @@ node valstar {
     # Oct 26 13:30:01 valstar svn: No worthy mechs found
     # without it, source http://mail-index.netbsd.org/pkgsrc-users/2008/11/23/msg008706.html 
     # 
-    package {"sasl-plug-anonymous":
+    package {"lib64sasl2-plug-anonymous":
         ensure => "installed"
     }
 
