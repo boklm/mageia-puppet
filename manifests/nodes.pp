86b7c92ccae68f8f032876b96718f7b4b93b600e
Michael Scherer <misc@mageia.org>
2010-10-26 16:20:33 +0000

    - import current config from valstar
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@4 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,35 @@
-node basenode {
-
+# svn, big important server
+node valstar {
+	include default_mageia_server
 }
 
-node "mx2" inherits {
-
+# web apps
+node alamut {
+	include default_mageia_server
 }
+
+# buildnode
+node jonund {
+	include default_mageia_server
+}
+
+node ecosse {
+	include default_mageia_server
+}
+
+
+# backup server
+node fiona {
+	include default_mageia_server
+} 
+
+# gandi-vm
+node krampouezh {
+	include default_mageia_server
+}
+
+node champagne {
+	include default_mageia_server
+}
+
 
