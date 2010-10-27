b49c4bfcaa85f0c8bab90c95dd3c3099c96c6683
Michael Scherer <misc@mageia.org>
2010-10-27 00:14:45 +0000

    - fix syntax
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@30 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bind/manifests/init.pp
+++ modules/bind/manifests/init.pp
@@ -1,45 +1,48 @@
-package { bind:
-    ensure => installed
-}
+class bind {
+    class bind_base {
+        package { bind:
+            ensure => installed
+        }
 
-service { named:
-    ensure => running,
-    path => "/etc/init.d/named",
-    subscribe => [ Package["bind"]]
-}
+        service { named:
+            ensure => running,
+            path => "/etc/init.d/named",
+            subscribe => [ Package["bind"]]
+        }
+    }
 
-file { '/etc/named.conf':
-    ensure => present,
-    owner => root,
-    group => root,
-    mode => 644,
-    require => Package["bind"],
-    content => "",
-    notify => [Service['named']]
-}
-
-define zone_master {
-    file { "/var/lib/named/var/named/master/$name.zone":
+    file { '/etc/named.conf':
         ensure => present,
         owner => root,
         group => root,
         mode => 644,
-        content => template("bind/zones/$name.zone"),
-        require => Package[bind],
-        notify => Service[named]
+        require => Package["bind"],
+        content => "",
+        notify => [Service['named']]
     }
-}
 
-class bind_master inherits bind_base {
-    file { '/etc/named.conf':
-        content => template("bind/named_base.conf", "bind/named_master.conf"),
+    define zone_master {
+        file { "/var/lib/named/var/named/master/$name.zone":
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 644,
+            content => template("bind/zones/$name.zone"),
+            require => Package[bind],
+            notify => Service[named]
+        }
     }
-}
 
-class bind_slave inherits bind_base {
-    file { '/etc/named.conf':
-        content => template("bind/named_base.conf", "bind/named_slave.conf"),
+    class bind_master inherits bind_base {
+        file { '/etc/named.conf':
+            content => template("bind/named_base.conf", "bind/named_master.conf"),
+        }
     }
+
+    class bind_slave inherits bind_base {
+        file { '/etc/named.conf':
+            content => template("bind/named_base.conf", "bind/named_slave.conf"),
+        }
+    }
+
 }
-
-
