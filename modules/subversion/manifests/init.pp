78bc13a232c1a8eef8afc9e91b1298e3c73899df
Michael Scherer <misc@mageia.org>
2010-11-04 14:59:38 +0000

    - module to manage subversion snapshot
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@83 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/subversion/manifests/init.pp
@@ -0,0 +1,52 @@
+# should be replaced by vcsrepo
+# https://github.com/reductivelabs/puppet-vcsrepo
+# but not integrated in puppet directly for the moment
+class subversion {
+
+
+    class server {
+        package { "subversion-server":
+            ensure => installed,
+        }
+    }
+
+    # TODO create proper hook directory ( see zarb.org )
+    # create documentation
+    # - group who can commit 
+
+#    define repository ($group => "svn") {
+#        # $name ==> lieu du checkout 
+#        exec { "svnadmin create $name":
+#            path => "/usr/bin:/usr/sbin:/bin",
+#            creates => $name             
+#        }
+#        # TODO complete documentation
+#        file { "$name":
+#            mode => 660
+#            recurse => true
+#        } 
+#        # file pour les hooks
+#    }
+
+
+    class client {
+        package { subversion:
+            ensure => installed,
+        }
+    }
+
+    # TODO ensure that subversion ishere
+    #      allow to configure the snapshot refresh interval
+    define snapshot($source, $refresh, $user = 'root')  {
+        exec { "/usr/bin/svn co $source $name":
+            creates => $name,           
+            user => $user,  
+        }
+
+        cron { "update $name":
+           command => "cd $name && /usr/bin/svn update -q",
+           user => $user,
+           minute => '*/5'
+        }   
+    }
+}
