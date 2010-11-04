7a1765a9b7b7edf62f4a517914494c5807071248
Michael Scherer <misc@mageia.org>
2010-11-04 01:09:23 +0000

    - add a module for apache
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@80 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,32 @@
+class apache {
+
+    class base {
+        package { "apache-mpm-prefork":
+            ensure => installed
+        }
+    
+        service { apache: 
+            ensure => running,
+            subscribe => [ Package['apache-mpm-prefork'] ],
+            path => "/etc/init.d/httpd"
+        }
+    }
+    
+    class mod_php inherits base {
+        package { "apache-mod_php":
+            ensure => installed
+        }
+    }
+
+    class mod_perl inherits base {
+        package { "apache-mod_perl":
+            ensure => installed
+        }
+    }
+
+    class mod_wsgi inherits base {
+        package { "apache-mod_wsgi":
+            ensure => installed
+        }
+    }
+}
