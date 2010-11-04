9b2273958bbf305c106a382e1d752339ce0322f8
Michael Scherer <misc@mageia.org>
2010-11-04 18:56:37 +0000

    - add missing Requires
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@107 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/catdap/manifests/init.pp
+++ modules/catdap/manifests/init.pp
@@ -8,7 +8,7 @@ class catdap {
     $catdap_vhost = "identity.$domain"
 
     # TODO switch to a proper rpm packaging
-    $rpm_requirement = ['perl-Catalyst-Runtime', 
+    $rpm_requirement = ['perl-Catalyst-Runtime',"perl-FCGI", 'perl-Catalyst-Plugin-Authorization-Roles', 
 "perl-Catalyst-Action-RenderView", "perl-Catalyst-Model-LDAP-FromAuthentication", "perl-Catalyst-P-A-Store-LDAP", "perl-Catalyst-Plugin-Authentication", "perl-Catalyst-Plugin-Captcha",
 "perl-Catalyst-Plugin-ConfigLoader", "perl-Catalyst-Plugin-I18N", "perl-Catalyst-Plugin-Session-Store-File", "perl-Catalyst-Plugin-Static-Simple",
 "perl-Catalyst-P-S-State-Cookie", "perl-Catalyst-P-S-Store-File", "perl-Catalyst-View-Email",
