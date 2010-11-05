0a2c894183bfd1e87be17a45e69f2a29ce263f1b
Michael Scherer <misc@mageia.org>
2010-11-05 18:20:58 +0000

    - add default setting for exec class
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@157 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,3 +1,6 @@
+# to not repeat the setting everywhere 
+Exec { path => "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin/" }
+
 
 class base_packages {
     # packages installed everywhere 
