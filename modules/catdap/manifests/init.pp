0aeddc9d1259a148968282f54dd18a5424919538
Buchan Milne <buchan@mageia.org>
2010-11-05 12:34:15 +0000

    Add a few more dependencies --This line, and those below, will be ignoed--
    M    puppet/modules/catdap/manifests/init.pp
    
    
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@139 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/catdap/manifests/init.pp
+++ modules/catdap/manifests/init.pp
@@ -13,7 +13,9 @@ class catdap {
 "perl-Catalyst-Plugin-ConfigLoader", "perl-Catalyst-Plugin-I18N", "perl-Catalyst-Plugin-Session-Store-File", "perl-Catalyst-Plugin-Static-Simple",
 "perl-Catalyst-P-S-State-Cookie", "perl-Catalyst-P-S-Store-File", "perl-Catalyst-View-Email",
 "perl-Catalyst-View-TT", "perl-Config-General", "perl-Crypt-CBC", "perl-Data-UUID",
-"perl-Email-Valid", "perl-Moose", "perl-namespace-autoclean", "perl-Test-Simple" ]
+"perl-Email-Valid", "perl-Moose", "perl-namespace-autoclean", "perl-Test-Simple",
+"perl-Crypt-Blowfish", "perl-Email-Date-Format", "perl-YAML-LibYAML",
+]
 
     package { $rpm_requirement:
         ensure => installed
