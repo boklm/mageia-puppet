e5c418b6a8cbde126f9cd053954c69c67ef7fa14
Dexter Morgan <dmorgan@mageia.org>
2010-11-06 11:23:38 +0000

    Remove postfix require
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@179 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/transifex/manifests/init.pp
+++ modules/transifex/manifests/init.pp
@@ -2,10 +2,7 @@ class transifex {
   package { 'transifex':
     ensure => installed
   }
-  package { 'postfix':
-    ensure => installed
-  }
-
+  
   file { "check_new-blog-post":
     path => "/etc/transifex/20-engines.conf",
     ensure => present,
