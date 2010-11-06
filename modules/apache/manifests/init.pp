481ef7b43898ba2a810a9e742a168da61f0c94e1
Michael Scherer <misc@mageia.org>
2010-11-06 17:50:14 +0000

    - add django support with wsgi
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@182 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,6 +1,7 @@
 class apache {
 
     $vhost_dir = "/etc/httpd/conf/vhosts.d"
+    $wsgi_dir = "/usr/local/lib/wsgi"
  
     class base {
         package { "apache-mpm-prefork":
@@ -54,6 +55,13 @@ class apache {
         package { "apache-mod_wsgi":
             ensure => installed
         }
+
+        file { $wsgi_dir:
+            ensure => directory,
+            owner => root,
+            group => root,
+            mode => 644,
+        }
     }
 
     define vhost_redirect_ssl() {
@@ -80,6 +88,31 @@ class apache {
             mode => 644,
             notify => Service['apache'],
             content => template("apache/vhost_catalyst_app.conf")
+        }
+    }
+
+    define vhost_django_app($module, $module_path = '/usr/share') {
+        include apache::mod_wsgi
+
+        file { "$name.conf":
+            path => "$vhost_dir/$name.conf",
+            ensure => "present",
+            owner => root,
+            group => root,
+            mode => 644,
+            notify => Service['apache'],
+            content => template("apache/vhost_django_app.conf")
+        }
+
+        # fichier django wsgi
+        file { "$name.wsgi":
+            path => "$wsgi_dir/$name.wsgi",
+            ensure => "present",
+            owner => root,
+            group => root,
+            mode => 755,
+            notify => Service['apache'],
+            content => template("apache/django.wsgi")
         }
     }
 }
---
+++ modules/apache/templates/django.wsgi
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import os, sys
+sys.path.append('<%= module_path  %>')
+os.environ['DJANGO_SETTINGS_MODULE'] = '<%= module =%>.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+
