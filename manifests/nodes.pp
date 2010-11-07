eb8d4f79089967ae862e5e89c079e1c80b2504fb
Michael Scherer <misc@mageia.org>
2010-11-07 23:36:49 +0000

    - remove tabulation ( so indentation is really fixed ) - clean old comments ( ie, things we deployed )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@193 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -9,7 +9,6 @@ node valstar {
 # - setup restricted shell access to allow "mdvsys submit" to work
 # - setup maintainers database (with web interface)
 # - mirroring (Nanar)
-# - LDAP master
 #
     include default_mageia_server
     timezone::timezone { "Europe/Paris": }
@@ -40,11 +39,10 @@ node alamut {
 # - mail server
 # - mailing list server
 # - wiki
-# - pastbin
+# - pastebin
 # - LDAP slave
-# - SQL server
 # 
-	  include default_mageia_server
+    include default_mageia_server
     include bind::bind_master
     include postgresql
     bind::zone_master { "mageia.org": }
@@ -61,16 +59,16 @@ node alamut {
 node jonund {
 # Location: IELO datacenter (marseille)
 #
-	  include default_mageia_server
-	  include iurt
+    include default_mageia_server
+    include iurt
     timezone::timezone { "Europe/Paris": }
 }
 
 node ecosse {
 # Location: IELO datacenter (marseille)
 #
-	  include default_mageia_server
-	  include iurt
+    include default_mageia_server
+    include iurt
     timezone::timezone { "Europe/Paris": }
 }
 
@@ -82,8 +80,8 @@ node fiona {
 # TODO:
 # - buy the server
 # - install the server in datacenter
-#
-	  include default_mageia_server
+# - install a backup system
+    include default_mageia_server
 } 
 
 # gandi-vm
@@ -94,7 +92,7 @@ node krampouezh {
 # - secondary MX
 # - LDAP slave (for external traffic maybe)
 #
-	  include default_mageia_server
+    include default_mageia_server
     include bind::bind_master
     bind::zone_master { "mageia.org": }
     bind::zone_master { "mageia.fr": } 
@@ -110,10 +108,9 @@ node champagne {
 # - setup mageia.org web site
 # - setup blog
 #
-	  include default_mageia_server
+    include default_mageia_server
     include apache::base
     timezone::timezone { "Europe/Paris": }
-    #include postfix
     include blog
 }
 
