89af1bb7640ec7fe6ab94c8055330243e8d094ff
Damien Lallement <dams@mageia.org>
2010-11-10 14:03:49 +0000

    Fix cron for check new blog post
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@232 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/manifests/init.pp
+++ modules/blog/manifests/init.pp
@@ -20,7 +20,7 @@ class blog {
         }
 	cron { blog:
         	user => blog,
-        	minute => 15,
+        	minute => '*/15',
         	command => "/usr/local/bin/check_new-blog-post.sh",
         	require => File["check_new-blog-post"]
     	}
