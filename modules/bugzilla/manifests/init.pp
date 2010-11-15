dfdec11004fec3a7e5762b392e4fa44ab908f35b
Dexter Morgan <dmorgan@mageia.org>
2010-11-15 00:51:36 +0000

    Add params file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@254 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bugzilla/manifests/init.pp
+++ modules/bugzilla/manifests/init.pp
@@ -5,13 +5,23 @@ class bugzilla {
     }
 
     $password = extlookup("bugzilla_password")
+    $passwordLdap = extlookup("bugzilla_ldap")
+
     file { '/etc/bugzilla/localconfig':
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 644,
-        content => template("bugzilla/localconfig")
+      ensure => present,
+      owner => root,
+      group => root,
+      mode => 644,
+      content => template("bugzilla/localconfig")
     }
 
+
+    file { '/var/lib/bugzilla/params':
+      ensure => present,
+      owner => root,
+      group => root,
+      mode => 644,
+      content => template("bugzilla/params")
+    }
 }
 
