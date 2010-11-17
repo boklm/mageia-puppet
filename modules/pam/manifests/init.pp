b3d1a9045918f77b75ba21c6ef93e5c5d7033999
Michael Scherer <misc@mageia.org>
2010-11-17 15:45:22 +0000

    - better use heritance
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@266 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/pam/manifests/init.pp
+++ modules/pam/manifests/init.pp
@@ -1,26 +1,28 @@
 class pam {
 
-  package { ["pam_ldap","nss_ldap", "pam_mkhomedir"]:
-    ensure => installed,  
-  }
+  class base {
+      package { ["pam_ldap","nss_ldap", "pam_mkhomedir"]:
+        ensure => installed,  
+      }
 
-
-  file { "system-auth":
-     path => "/etc/pam.d/system-auth",
-     owner => root,
-     group => root,
-     mode => 644,
-     content => template("openldap/system-auth")
-  }
+      file { "system-auth":
+         path => "/etc/pam.d/system-auth",
+         owner => root,
+         group => root,
+         mode => 644,
+         content => template("openldap/system-auth")
+      }
+  } 
   
   # for server where only admin can connect
-  class admin_access {
+  class admin_access inherits base {
     $access_class = "admin"
+    # not sure if this line is needed anymore, wil check later
     file { "system-auth": }
   }
 
   # for server where people can connect with ssh ( git, svn )
-  class commiters_access {
+  class commiters_access inherits base {
     $access_class = "commiters"
     file { "system-auth": }
   }
