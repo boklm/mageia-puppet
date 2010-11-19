77ccbe1e1b0348c65b16cca79f8717916a4183db
Michael Scherer <misc@mageia.org>
2010-11-19 00:53:08 +0000

    - fix packages name for non x86_64 ( such as my test vm )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@310 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/subversion/manifests/init.pp
+++ modules/subversion/manifests/init.pp
@@ -149,8 +149,13 @@ class subversion {
         # svn spam log with 
         # Oct 26 13:30:01 valstar svn: No worthy mechs found
         # without it, source http://mail-index.netbsd.org/pkgsrc-users/2008/11/23/msg008706.html 
-        # 
-        package {"lib64sasl2-plug-anonymous":
+        #
+	$sasl2_package = $architecture ? {
+        	x86_64 => "lib64sasl2-plug-anonymous",
+        	default => "libsasl2-plug-anonymous"
+    	}
+ 	
+        package {"$sasl2_package":
             ensure => "installed"
         }
     }
