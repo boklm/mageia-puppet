3173efceeaf0ecd59204db13375c56f3e8424b56
Nicolas Vigier <boklm@mageia.org>
2010-11-21 20:54:45 +0000

    add testvm module, used to run test VMs
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@357 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/testvm/manifests/init.pp
@@ -0,0 +1,33 @@
+class testvm
+{
+    $testvm_login = "testvm"
+    $testvmdir = "/home/testvm"
+
+    group {"$testvm_login":
+	ensure => present,
+    }
+
+    user {"$testvm_login":
+	ensure => present,
+	comment => "System user used to run test VMs",
+	managehome => true,
+	gid => $vmtest_login,
+	shell => "/bin/bash",
+    }
+
+    file { "$testvmdir/bin/_vm":
+	ensure => present,
+        owner => root,
+	group => root,
+	mode => 644,
+	source => "puppet:///modules/testvm/_vm",
+    }
+
+    file { "$testvmdir/bin/vm-jonund":
+	ensure => present,
+        owner => root,
+	group => $testvm_login,
+	mode => 750,
+	source => "puppet:///modules/testvm/vm-jonund",
+    }
+}
