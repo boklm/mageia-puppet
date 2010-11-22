0bbc5ee496e6376d27f21faa24cf9f064a246139
Olivier Blin <blino@mageia.org>
2010-11-22 07:21:22 +0000

    use again include instead of require to avoid deps cycle
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@381 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/init.pp
+++ modules/buildsystem/manifests/init.pp
@@ -9,7 +9,7 @@ class buildsystem {
     }
 
     class mainnode inherits base {
-        require iurtuser
+        include iurtuser
         ssh::auth::server { $build_login: }
 
         package { "task-bs-cluster-main":
@@ -60,7 +60,7 @@ class buildsystem {
 
     class iurt {
         include sudo
-        require iurtuser
+        include iurtuser
         ssh::auth::client { $build_login: }
 
         # build node common settings
