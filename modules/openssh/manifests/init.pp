3266e6005d36e481bf06b020f35c5600d45935a9
Michael Scherer <misc@mageia.org>
2010-11-23 23:17:47 +0000

    move the ldap key from ssh logic to openssh module ( more logical ), and add the hook in openssh config file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@436 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/openssh/manifests/init.pp
+++ modules/openssh/manifests/init.pp
@@ -22,4 +22,43 @@ class openssh {
         require => Package["openssh-server"],
         content => template("openssh/sshd_config")
     }
+
+ 
+    class ssh_keys_from_ldap {
+
+        package { 'python-ldap':
+            ensure => installed,
+        }
+
+        $pubkeys_directory = "/var/lib/pubkeys"
+        file { $pubkeys_directory:
+            ensure => directory,
+            owner => root,
+            group => root,
+            mode => 755,
+        #    before => Class["openssh"] 
+        }
+
+        file { "$pubkeys_directory/root":
+            ensure => directory,
+            owner => root,
+            group => root,
+            mode => 700,
+        }
+
+        file { "$pubkeys_directory/root/authorized_keys":
+            ensure => "/root/.ssh/authorized_keys",
+            mode => 700,
+        }
+
+        $ldap_pwfile = "/etc/ldap.secret"
+        file { '/usr/local/bin/ldap-sshkey2file.py':
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 755,
+            content => template("restrictshell/ldap-sshkey2file.py"),
+            require => Package['python-ldap']
+        }
+    } 
 }
--- modules/restrictshell/manifests/init.pp
+++ modules/restrictshell/manifests/init.pp
@@ -23,43 +23,6 @@ class restrictshell {
             content => template("restrictshell/membersh-conf.pl"),
         }
     }
-    
-    class ssh_keys_from_ldap {
-
-        package { 'python-ldap':
-            ensure => installed,
-        }
-
-        $pubkeys_directory = "/var/lib/pubkeys"
-        file { $pubkeys_directory:
-            ensure => directory,
-            owner => root,
-            group => root,
-            mode => 755,
-        }
-
-        file { "$pubkeys_directory/root":
-            ensure => directory,
-            owner => root,
-            group => root,
-            mode => 700,
-        }
-
-        file { "$pubkeys_directory/root/authorized_keys":
-            ensure => "/root/.ssh/authorized_keys",
-            mode => 700,
-        }
-
-        $ldap_pwfile = "/etc/ldap.secret"
-        file { '/usr/local/bin/ldap-sshkey2file.py':
-            ensure => present,
-            owner => root,
-            group => root,
-            mode => 755,
-            content => template("restrictshell/ldap-sshkey2file.py"),
-            requires => Package['python-ldap']
-        } 
-    }
 
     define allow {
         include shell
