2df44c3fa950280db71c9eaf328172d729db03f3
Michael Scherer <misc@mageia.org>
2010-11-30 19:21:42 +0000

    split the declaration of openssl in a class so we can have multiple declaration of self_signed_certificate
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@483 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/openssl/manifests/init.pp
+++ modules/openssl/manifests/init.pp
@@ -1,8 +1,13 @@
 class openssl {
-	define self_signed_cert($directory = '/etc/certs') {
+    class base {
         package { 'openssl':
             ensure => installed
         }
+    } 
+
+	define self_signed_cert($directory = '/etc/certs') {
+        include openssl::base
+        
         $pem_file = "$name.pem"
 	    exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $pem_file -out $pem_file -subj  '/CN=$name.$domain'":
             cwd => "$directory",
