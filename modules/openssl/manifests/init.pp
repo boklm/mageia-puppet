166597d487877b16315b2ed97acd88b1345b7fd2
Michael Scherer <misc@mageia.org>
2010-12-07 02:40:29 +0000

    add a type for selfsigned certificate in 2 file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@530 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/openssl/manifests/init.pp
+++ modules/openssl/manifests/init.pp
@@ -15,4 +15,29 @@ class openssl {
             require => Package['openssl']
         }
 	}
+
+	define self_signed_splitted_cert($filename = '', 
+                                     $directory = '/etc/certs', 
+                                     $owner = "root",
+                                     $group = 'root', 
+                                     $mode = '600') {
+        include openssl::base
+        
+        $crt_file = "$filename.crt"
+        $key_file = "$filename.key"
+	    exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $key_file -out $crt_file -subj  '/CN=$name'":
+            cwd => "$directory",
+            creates => "$directory/$key_file",
+            require => Package['openssl'],
+            before => [ File["$directory/$key_file"], 
+                        File["$directory/$crt_file"]]
+        }
+
+        file { ["$directory/$key_file","$directory/$crt_file"]:
+            owner => $owner,
+            group => $group,
+            mode => $mode
+        }
+	}
+
 }
