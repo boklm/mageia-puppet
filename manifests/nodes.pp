36d1304cd60af324952188c0a274394dc9a012b3
Michael Scherer <misc@mageia.org>
2011-01-06 20:43:36 +0000

    - fix description, as pointed by wobo and remmy on ml
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@679 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -66,47 +66,47 @@ node alamut {
     # please check that the list use the proper code for
     # language ( not to be confused with tld or country code )
     sympa::public_list {"i18n-af":
-        subject => "List about translation in Afrikaans",
+        subject => "List about translation to Afrikaans",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-de":
-        subject => "List about translation in German",
+        subject => "List about translation to German",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-et":
-        subject => "List about translation in Estonian",
+        subject => "List about translation to Estonian",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-fr":
-        subject => "List about translation in French",
+        subject => "List about translation to French",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-nl":
-        subject => "List about translation in Dutch",
+        subject => "List about translation to Dutch",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-pt_br":
-        subject => "List about translation in Brazilian Portuguese",
+        subject => "List about translation to Brazilian Portuguese",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-pl":
-        subject => "List about translation in Polish",
+        subject => "List about translation to Polish",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-ru":
-        subject => "List about translation in Russian",
+        subject => "List about translation to Russian",
         topics => "i18n",
     }
 
     sympa::public_list {"i18n-tr":
-        subject => "List about translation in Turkish",
+        subject => "List about translation to Turkish",
         topics => "i18n",
     }
 
