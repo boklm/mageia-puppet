0279cd00ec6bda0538e0b4f0d7b5403b49276daa
Michael Scherer <misc@mageia.org>
2011-01-09 11:15:13 +0000

    use mysql module, reindent the file to be consistent with space and more compact
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@746 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/manifests/init.pp
+++ modules/blog/manifests/init.pp
@@ -1,41 +1,34 @@
 #TODO: 
 # - add the creation of the user 'blog' in puppet
 class blog {
-	package { 'mysql':
-        	ensure => installed
-    	}
-
-        package { 'php-mysql':
-                ensure => installed
-        }
-
     include apache::mod_php
+    include mysql
 
-    	package { 'wget':
-        	ensure => installed
-    	}
+    package { ['wget','php-mysql']:
+        ensure => installed
+    }
 
 
 	file { "check_new-blog-post":
-        	path => "/usr/local/bin/check_new-blog-post.sh",
-        	ensure => present,
-        	owner => blog,
-        	group => blog,
-        	mode => 755,
-        	content => template("blog/check_new-blog-post.sh")
-    	}
+        path => "/usr/local/bin/check_new-blog-post.sh",
+        ensure => present,
+        owner => blog,
+        group => blog,
+        mode => 755,
+        content => template("blog/check_new-blog-post.sh")
+    }
 
-	file { "/var/lib/blog":
-                ensure => directory,
-                owner => blog,
-                group => blog,
-                mode => 644,
-        }
+    file { "/var/lib/blog":
+        ensure => directory,
+        owner => blog,
+        group => blog,
+        mode => 644,
+    }
 
-	cron { blog:
-        	user => blog,
-        	minute => '*/15',
-        	command => "/usr/local/bin/check_new-blog-post.sh",
-        	require => File["check_new-blog-post"]
-    	}
+    cron { blog:
+        user => blog,
+        minute => '*/15',
+        command => "/usr/local/bin/check_new-blog-post.sh",
+        require => File["check_new-blog-post"]
+    }
 }
