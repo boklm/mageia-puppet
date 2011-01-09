dad82cf5f8b7e4238e28d16f45609780399afa39
Michael Scherer <misc@mageia.org>
2011-01-09 16:42:44 +0000

    move subversion repository in their own file ( cleaner )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@753 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ deployment/repositories/manifests/subversion.pp
@@ -0,0 +1,40 @@
+class repositories::subversion {
+
+    subversion::repository { "/svn/adm/":
+        group => "mga-sysadmin",
+        commit_mail => ['mageia-sysadm@mageia.org', "sysadmin-commits@ml.$domain"],
+        syntax_check1 => 'check_puppet_templates',
+        syntax_check2 => 'check_puppet',
+        cia_post => true,
+        cia_module => "sysadm",
+    }
+
+    subversion::repository { "/svn/soft/":
+	    group => "mga-packagers",
+	    commit_mail => ['mageia-sysadm@mageia.org'],
+	    cia_post => true,
+	    cia_module => "soft",
+    }
+
+    subversion::repository { "/svn/web/":
+	    group => "mga-committers",
+        cia_post => true,
+        cia_module => "web",
+    }
+
+    subversion::repository { "/svn/packages/":
+        group => "mga-packagers",
+        no_binary => true,
+        cia_post => true,
+        cia_module => "packages",
+        cia_ignore_author => '^schedbot$',
+    }
+
+    subversion::repository { "/svn/binrepos/cauldron/":
+        group => "mga-packagers",
+        cia_post => true,
+        cia_module => "binrepos",
+        cia_ignore_author => '^schedbot$',
+    }
+
+}
