a04b9a0ef967b045804f122eb1fb6871e509ad6c
Nicolas Vigier <boklm@mageia.org>
2011-01-18 18:42:28 +0000

    ssh_access not needed anymore
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@848 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bcd/manifests/init.pp
+++ modules/bcd/manifests/init.pp
@@ -1,33 +1,23 @@
 class bcd {
     class variable {
-
         $bcd_login = 'bcd'
         $bcd_home = '/home/bcd'
     }
 
     class base inherits variable {
-        include sudo
+	include sudo
 
         buildsystem::sshuser { $bcd_login:
             homedir => $bcd_home,
             comment => "User for creating ISOs",
-	    }
+	}
 
         package { bcd:
-            ensure => 'installed',
+	    ensure => 'installed',
         }
 
         sudo::sudoers_config { "bcd":
-            content => template("bcd/sudoers.bcd")
-        }
-    }
-
-    define ssh_access($type, $key) {
-        include bcd::variable
-        ssh_authorized_key{$name:
-            type => $type,
-            key => $key,
-            user => $bcd_login,
+	    content => template("bcd/sudoers.bcd")
         }
     }
 }
