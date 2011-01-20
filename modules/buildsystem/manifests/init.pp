527ad7cf57f58538d286b121457559589f4ea4ca
Michael Scherer <misc@mageia.org>
2011-01-20 15:55:48 +0000

    add script made by pascal to check packages that have missing deps ( temporary, quick and dirty )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@859 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/init.pp
+++ modules/buildsystem/manifests/init.pp
@@ -262,4 +262,20 @@ class buildsystem {
             content => template("buildsystem/mgacreatehome")
 	}
     }
+
+    class check_missing_deps {
+        file { "/usr/local/bin/missing-deps.sh":
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 700,
+            content => "puppet:///modules/buildsystem/missing-deps.sh",
+        }
+    
+        # FIXME hardcoded path
+        cron { "check missing deps":
+            command => "cd /var/www/bs/data && /usr/local/bin/missing-deps.sh",
+            minute => "*/15",
+        }
+    }
 }
