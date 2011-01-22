270ba4f680d921e737fc1ee36e09601931b407f4
Michael Scherer <misc@mageia.org>
2011-01-22 23:04:52 +0000

    - no longer use the main ml to send svn commits ( as said on 05/01/11 )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@879 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/subversion.pp
+++ deployment/repositories/manifests/subversion.pp
@@ -2,7 +2,7 @@ class repositories::subversion {
 
     subversion::repository { "/svn/adm/":
         group => "mga-sysadmin",
-        commit_mail => ['mageia-sysadm@mageia.org', "sysadmin-commits@ml.$domain"],
+        commit_mail => ["sysadmin-commits@ml.$domain"],
         syntax_check => ['check_puppet_templates','check_puppet'],
         cia_post => true,
         cia_module => "sysadm",
