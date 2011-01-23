54220319b3fcfa081c49ce1168bf3ad609a9b169
Michael Scherer <misc@mageia.org>
2011-01-23 14:32:17 +0000

    - add libvirtd module, to have the minimal support for using virt-manager
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@881 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/libvirtd/manifests/init.pp
@@ -0,0 +1,23 @@
+class libvirtd {
+    class base {
+        # make sure to use a recent enough version
+        # dnsmasq-base -> for nat network
+        # netcat-openbsd -> for ssh remote access
+        # iptables -> for dhcp, message error was quite puzzling
+        package { ["libvirtd","dnsmasq-base","netcat-openbsd","iptables"]:
+        }
+
+        service { libvirtd:
+            ensure => running,
+            path => "/etc/init.d/libvirtd",
+        }
+    }
+
+    class kvm inherits base {
+        # pull cyrus-sasl, should be checked
+        package { "qemu":
+
+        }
+    
+    }
+}
