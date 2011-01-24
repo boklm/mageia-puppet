b701f23eace07c4ad346486103d80e5dfd7d83b2
Michael Scherer <misc@mageia.org>
2011-01-24 12:22:05 +0000

    - add a list of svn repository to mirror for viewvc
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@888 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ deployment/repositories/manifests/mirror.pp
@@ -0,0 +1,24 @@
+class repositories::mirror {
+    file { "/svn":
+        ensure => directory,
+    }
+
+    subversion::mirror_repository { "/svn/adm/":
+        source => "svn://svn.$domain/svn/adm/"
+    }
+
+    subversion::mirror_repository { "/svn/soft/":
+        source => "svn://svn.$domain/svn/soft/"
+    }
+
+    subversion::mirror_repository { "/svn/web/":
+        source => "svn://svn.$domain/svn/web/"
+    }
+
+    subversion::mirror_repository { "/svn/packages/":
+        source => "svn://svn.$domain/svn/packages/"
+    }
+
+    # no binrepos, too big to mirror
+}
+
