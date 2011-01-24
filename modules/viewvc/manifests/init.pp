6a190957cde2c1ef9b64ef00054d8accfc434487
Michael Scherer <misc@mageia.org>
2011-01-24 12:22:11 +0000

    add viewvc module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@891 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/viewvc/manifests/init.pp
@@ -0,0 +1,28 @@
+class viewvc {
+    package { ['viewvc','python-svn']:
+        ensure => installed
+    }
+    # http_expiration_time = 600
+    # svn_roots = admin: svn://svn.mageia.org/svn/adm/
+
+    file { 'viewvc.conf':
+        ensure => present,
+        path => '/etc/viewvc/viewvc.conf',
+        content => template('viewvc/viewvc.conf'),
+        notify => Service['apache'],
+    }
+
+    file { 'webapps.d/viewvc.conf':
+        ensure => present,
+        path => '/etc/httpd/conf/webapps.d/viewvc.conf',
+        content => template('viewvc/webapp.conf'),
+        notify => Service['apache'],
+    }
+
+    # need newer version of viewvc
+    apache::vhost_wsgi{ "viewvc.$domain":
+        wsgi_path => "/usr/share/viewvc/bin/wsgi/viewvc.wsgi",
+        aliases => { "/viewvc" => "/var/www/viewvc/" }, 
+    }
+}
+
