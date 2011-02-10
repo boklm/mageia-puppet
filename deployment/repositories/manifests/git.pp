653838eaad48c6635ab345bb1af4760857a470c9
Michael Scherer <misc@mageia.org>
2011-02-10 02:58:04 +0000

    manage the permission of /git ( as it is incorrectly set on valstar as a side effect of some script )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@980 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/git.pp
+++ deployment/repositories/manifests/git.pp
@@ -1,4 +1,11 @@
 class repositories::git {
+    file { "/git":
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => 755, 
+    }
+
     git::repository { "/git/forum":
         description => "Reference code for forum.$domain",
         group => "mga-forum-developers", 
