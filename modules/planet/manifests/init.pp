6af3421a45ccbd929a39b2b9757e3abaa236b15b
Damien Lallement <dams@mageia.org>
2011-02-11 14:33:20 +0000

    switch managehome to true
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1000 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/planet/manifests/init.pp
+++ modules/planet/manifests/init.pp
@@ -3,13 +3,13 @@
 # - add the user 'planet' to the 'apache' group (usermod -a -G apache blog)
 class planet {
     user { "planet":
-	groups => 'apache',
-	comment => 'This user is used for planet.mageia.org',
-	ensure => 'present',
-	managehome => 'false',
+	group => apache,
+	comment => "This user is used for planet.mageia.org",
+	ensure => present,
+	managehome => true,
     }
-    include apache::mod_php
 
+    include apache::mod_php
     apache::vhost_other_app { "planet.$domain":
         vhost_file => "planet/02_planet_vhosts.conf",
     }
