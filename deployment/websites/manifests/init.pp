2ddf401f9c8233638308f33fa9bb3d8741bbaf11
Nicolas Vigier <boklm@mageia.org>
2011-02-11 16:46:37 +0000

    add vhost for static.mageia.org
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1010 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/init.pp
+++ deployment/websites/manifests/init.pp
@@ -1,9 +1,34 @@
 class websites {
+    class base {
+       # FIXME :
+       # We should be able to define this path on each host.
+       # Maybe using Facter ?
+       $webdatadir = '/srv/web1-dd0/www'
+
+       file { "$webdatadir":
+          ensure => directory,
+	  mode => 755,
+       }
+    }
+
     # should expire on June 2011
     class donate {
         apache::vhost_other_app { "donate.$domain":
             vhost_file => "websites/vhost_donate.conf",
         }
+    }
+
+    # vhost to host static files used by web sites
+    class static inherits base {
+        $vhostdir = "$webdatadir/static.$domain"
+	$svn_location = "svn://svn.$domain/svn/web/www/trunk/g/"
+    	apache::vhost_other_app { "static.$domain":
+	    vhost_file => 'websites/vhost_static.conf',
+	}
+
+	subversion::snapshot { $vhostdir:
+	    source => $svn_location
+	}
     }
 
     class svn {
