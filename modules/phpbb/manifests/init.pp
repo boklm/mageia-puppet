d41e2a9c3392e36a11b3c54564b8087a14d41ab5
Michael Scherer <misc@mageia.org>
2011-02-12 16:05:10 +0000

    - initial module to setup phpbb
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1053 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/phpbb/manifests/init.pp
@@ -0,0 +1,18 @@
+class phpbb {
+
+    include apache::mod_php
+    include mysql
+
+    package { ["php-gd","php-xml","php-zlib","php-ftp","php-magickwand"
+        ensure => installed
+    }
+
+    # TODO ldap account configuration
+
+    # TODO apache setup
+
+    # TODO git checkout 
+
+    # TODO mysql vm creation
+
+}
