30a6379ac0cd42483f6c5b81a8330bbc9bcee119
Michael Scherer <misc@mageia.org>
2011-02-15 00:48:24 +0000

    add a define to manage part of the phpbb config from puppet by directly changing the configuration in the db
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1074 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/phpbb/files/phpbb_apply_config.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Env qw(VALUE);
+use DBI;
+
+# DBI will use default value coming from env
+# see puppet manifests 
+my $key = $ARGV[1];
+
+my $dbh = DBI->connect("dbi:Pg:","","");
+
+my $table = "phpbb_config";
+
+my $update = $dbh->prepare("UPDATE $table SET config_value = ? WHERE config_name = ?");
+my $insert = $dbh->prepare("INSERT INTO $table ( config_value, config_name, is_dynamic ) VALUES ( ? , ? , ? )");
+
+my $res = $update->execute($VALUE ,$key) or die "cannot do update $?";
+if ($res == 0 ) {
+     $insert->execute($VALUE ,$key, 1) or die "cannot do insert $?";
+}
+
--- modules/phpbb/manifests/init.pp
+++ modules/phpbb/manifests/init.pp
@@ -11,7 +11,27 @@ class phpbb {
 
     # TODO ldap account configuration
 
-    # TODO apache setup
+    file { "/usr/local/bin/phpbb_apply_config.pl":
+         ensure => present,
+         owner => root,
+         group => root,
+         mode => 755,
+         source => 'puppet:///modules/phpbb/phpbb_apply_config.pl',
+    }
+
+    # TODO ldap account configuration
+    # ldap_user
+    # ldap_server
+    # ldap_password ldap_base_dn cookie_domain 
+    # board_contact
+    # 
+    define phpbb_config($value) {
+        exec { "/usr/local/bin/phpbb_apply_config.pl $name":
+            user => root,
+            environment => "PGDATABASE=$database PGUSER=$user PGPASSWORD=$password PGHOST=pgsql.$domain VALUE=$value",
+            require => File["/usr/local/bin/phpbb_apply_config.pl"],
+        }
+    }
 
     # TODO git checkout 
 
