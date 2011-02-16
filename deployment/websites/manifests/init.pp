55c44cdae380624cd88ba01ecfe22d6fea7b4f32
Michael Scherer <misc@mageia.org>
2011-02-16 17:30:03 +0000

    - add a class for proxying the forum from alamut to friteuse ( while friteuse is on our server )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1104 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/init.pp
+++ deployment/websites/manifests/init.pp
@@ -41,4 +41,23 @@ class websites {
             url => "http://svnweb.$domain/",
         }
     }
+
+    class forum_proxy {
+
+        $web_domain = "forums.$domain"
+        host { "$web_domain":
+            ip => '192.168.122.131',
+            ensure => 'present',
+        }
+
+        apache::vhost_reverse_proxy { "$web_domain":
+            url => "http://$web_domain/",
+        } 
+
+        apache::vhost_reverse_proxy { "ssl_$web_domain":
+            vhost => $web_domain,
+            use_ssl => true,
+            url => "http://$web_domain/",
+        } 
+    }
 }
