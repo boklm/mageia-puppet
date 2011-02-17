66691f0e4df3bf2a896fce6adfb5294a1c01f6f5
Damien Lallement <dams@mageia.org>
2011-02-17 12:29:41 +0000

    start to deploy the deployment script
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1107 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/planet/templates/deploy_new-planet.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Initialization
+PATH_TO_FILE=${PATH_TO_FILE:-/var/lib/planet}
+
+#Ask for new locale name if no parameter given
+echo -n "Locale name: "
+read locale
+
+# Display the answer and ask for confirmation
+echo -e -n "Do you confirm the entry: \"$locale\"? (y/n) "
+read answer
+if [ "$answer" == "y" ] 
+then
+	FILE="$PATH_TO_FILE/$locale/"
+	if test -d $FILE
+	then
+		echo "Aborted, $FILE already exist."
+		exit 2
+	else
+		/bin/mkdir $FILE
+	fi
+else
+	echo "Aborted, please try again."
+	exit 2
+fi
