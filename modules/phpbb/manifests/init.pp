b87acc434dfcd76eb4a03e274630d88fa9888013
Michael Scherer <misc@mageia.org>
2011-02-21 15:14:47 +0000

    - use a define to allow multiple forum installation ( for numerous locale )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1147 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/phpbb/manifests/init.pp
+++ modules/phpbb/manifests/init.pp
@@ -1,69 +1,94 @@
 class phpbb {
-    $database = "phpbb"
-    $user = "phpbb"
+    class base {
+        $db = "phpbb"
+        $user = "phpbb"
 
-    include apache::mod_php
-    include mysql
+        include apache::mod_php
+        include mysql
 
-    package { ["php-gd","php-xml","php-zlib","php-ftp","php-magickwand","php-pgsql" ] :
-        ensure => installed
-    }
+        package { ["php-gd","php-xml","php-zlib","php-ftp","php-magickwand","php-pgsql" ] :
+            ensure => installed
+        }
 
-    package { "perl-DBD-Pg":
-        ensure => installed
-    }
+        package { "perl-DBD-Pg":
+            ensure => installed
+        }
 
-    file { "/usr/local/bin/phpbb_apply_config.pl":
-         ensure => present,
-         owner => root,
-         group => root,
-         mode => 755,
-         source => 'puppet:///modules/phpbb/phpbb_apply_config.pl',
-    }
+        file { "/usr/local/bin/phpbb_apply_config.pl":
+             ensure => present,
+             owner => root,
+             group => root,
+             mode => 755,
+             source => 'puppet:///modules/phpbb/phpbb_apply_config.pl',
+        }
 
-    # TODO phpbb config
-    # cookie_domain 
-    # board_contact
-    # 
-    define phpbb_config($value) {
-        exec { "/usr/local/bin/phpbb_apply_config.pl $name":
-            user => root,
-            environment => ["PGDATABASE=$database", "PGUSER=$user", "PGPASSWORD=$pgsql_password", "PGHOST=pgsql.$domain", "VALUE=$value"],
-            require => File["/usr/local/bin/phpbb_apply_config.pl"],
+        # TODO phpbb config
+        # cookie_domain 
+        # board_contact
+        # 
+        define phpbb_config($value) {
+            exec { "/usr/local/bin/phpbb_apply_config.pl $name":
+                user => root,
+                environment => ["PGDATABASE=$database", "PGUSER=$user", "PGPASSWORD=$pgsql_password", "PGHOST=pgsql.$domain", "VALUE=$value"],
+                require => File["/usr/local/bin/phpbb_apply_config.pl"],
+            }
+        }
+
+        $pgsql_password = extlookup("phpbb_pgsql",'x')
+        @@postgresql::user { $user:
+            password => $pgsql_password,
+        }
+
+        $forums_dir = "/var/www/forums/"
+        file { "$forums_dir":
+            ensure => directory,
+            owner => root,
+            group => root,
         }
     }
 
-    phpbb_config { "ldap_user":
-        value => "cn=phpbb-friteuse,ou=System Accounts,$dc_suffix",
-    }
+    define php_instance($lang) {
+        $database = "${db}_$lang"
 
-    phpbb_config { "ldap_server":
-        value => "ldap.$domain",
-    }
+        include git::client
 
-    $ldap_password = extlookup("phpbb_ldap",'x')
-    phpbb_config { "ldap_password":
-        value => $ldap_password,
-    }
+        exec { "git clone git://git.$domain/forum/ $lang":
+            cwd => $forums_dir,
+            creates => "$forums_dir/$lang",   
+        }
 
-    phpbb_config { "ldap_base_dn":
-        value => "ou=People,$dc_suffix",
-    }
+        file { "$forums_dir/$lang/phpBB/config.php":
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 644,
+            content => template("phpbb/config.php"),
+        }
+
+        include php::base
+
+        @@postgresql::database { $database:
+            description => "Phpbb database",
+            user => $user,
+            require => Postgresql::User[$user]
+        }
 
 
+        phpbb_config { "ldap_user":
+            value => "cn=phpbb-friteuse,ou=System Accounts,$dc_suffix",
+        }
 
-    # TODO git checkout of the code
-    include git::client
+        phpbb_config { "ldap_server":
+            value => "ldap.$domain",
+        }
 
-    # TODO phpbb database configuration
-    $pgsql_password = extlookup("phpbb_pgsql",'x')
-    @@postgresql::user { $user:
-        password => $pgsql_password,
-    }
+        $ldap_password = extlookup("phpbb_ldap",'x')
+        phpbb_config { "ldap_password":
+            value => $ldap_password,
+        }
 
-    @@postgresql::database { $database:
-        description => "Phpbb database",
-        user => $user,
-        require => Postgresql::User[$user]
+        phpbb_config { "ldap_base_dn":
+            value => "ou=People,$dc_suffix",
+        }
     }
 }
