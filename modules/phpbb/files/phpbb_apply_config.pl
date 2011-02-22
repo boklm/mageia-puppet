8f412ca58f381df9c7add926bc5f157fd15ac660
Michael Scherer <misc@mageia.org>
2011-02-22 02:16:18 +0000

    - rearrange order of argument, so they are in the same order in both query
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1179 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/phpbb/files/phpbb_apply_config.pl
+++ modules/phpbb/files/phpbb_apply_config.pl
@@ -13,10 +13,10 @@ my $dbh = DBI->connect("dbi:Pg:","","");
 my $table = "phpbb_config";
 
 my $update = $dbh->prepare("UPDATE $table SET config_value = ?, is_dynamic = ? WHERE config_name = ?");
-my $insert = $dbh->prepare("INSERT INTO $table ( config_value, config_name, is_dynamic ) VALUES ( ? , ? , ? )");
+my $insert = $dbh->prepare("INSERT INTO $table ( config_value, is_dynamic, config_name ) VALUES ( ? , ? , ? )");
 
-my $res = $update->execute($VALUE ,1, $key) or die "cannot do update $?";
+my $res = $update->execute($VALUE, 1, $key) or die "cannot do update $?";
 if ($res == 0 ) {
-     $insert->execute($VALUE ,$key, 1) or die "cannot do insert $?";
+     $insert->execute($VALUE, 1, $key) or die "cannot do insert $?";
 }
 
