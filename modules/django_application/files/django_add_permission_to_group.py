ab132d5488a96613bba035980b1c4a624e6f6c0e
Michael Scherer <misc@mageia.org>
2011-02-25 18:25:49 +0000

    - allow to give the application name ( as the codename is not enough )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1210 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/django_application/files/django_add_permission_to_group.py
+++ modules/django_application/files/django_add_permission_to_group.py
@@ -3,9 +3,18 @@ import sys
 group_name = sys.argv[1]
 permission = sys.argv[2]
 
-from django.contrib.auth.models import Group,Permission
+# as codename is not unique, we need to give the application name
+app = ''
+if len(sys.argv) > 3:
+        app = sys.argv[3]
+
+from django.contrib.auth.models import Group, Permission
 g = Group.objects.get(name=group_name)
-p = Permission.objects.get(codename=permission)
+
+p = Permission.objects.filter(codename=permission)
+if app:
+    p = p.filter(content_type__app_label__exact=app)
+p = p[0]
 
 g.permissions.add(p)
 g.save()
