f5025ad3d214b4a611f76855b3bdcd2c58e127a5
Damien Lallement <dams@mageia.org>
2011-03-03 18:01:31 +0000

    update init.pp to split script in different classes
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1267 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/manifests/init.pp
+++ modules/blog/manifests/init.pp
@@ -1,54 +1,58 @@
 class blog {
 
-    user { "blog":
+    class base {
+        user { "blog":
         groups => apache,
         comment => "Mageia Blog",
         ensure => present,
         managehome => true,
         home => "/var/lib/blog",
+        }
     }
+    
+    class champagne inherits base {
+        package { ['wget','php-mysql']:
+            ensure => installed
+        }
 
-    package { ['wget','php-mysql']:
-        ensure => installed
-    }
-
-    file { "check_new-blog-post":
-        path => "/usr/local/bin/check_new-blog-post.sh",
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 755,
-        content => template("blog/check_new-blog-post.sh")
-    }
+        file { "check_new-blog-post":
+            path => "/usr/local/bin/check_new-blog-post.sh",
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 755,
+            content => template("blog/check_new-blog-post.sh")
+        }
    
-    cron { blog:
-        user => blog,
-        minute => '*/15',
-        command => "/usr/local/bin/check_new-blog-post.sh",
-        require => [File["check_new-blog-post"], User['blog']],
-    }
+        cron { blog:
+            user => blog,
+            minute => '*/15',
+            command => "/usr/local/bin/check_new-blog-post.sh",
+            require => [File["check_new-blog-post"], User['blog']],
+        }
 
-    include apache::mod_php
+        include apache::mod_php
 
-    $blog_location = "/var/www/html/blog.$domain"
-    $blog_domain = "blog-test.$domain"
+        $blog_location = "/var/www/html/blog.$domain"
+        $blog_domain = "blog-test.$domain"
 
-    apache::vhost_base { "$blog_domain":
-        location => $blog_location,
-        content => template('blog/blogs_vhosts.conf'),
-    }
+        apache::vhost_base { "$blog_domain":
+            location => $blog_location,
+            content => template('blog/blogs_vhosts.conf'),
+        }
 
-    apache::vhost_base { "ssl_$blog_domain":
-        use_ssl => true,
-        vhost => $blog_domain,
-        location => $blog_location,
-        content => template('blog/blogs_vhosts.conf'),
-    }
+        apache::vhost_base { "ssl_$blog_domain":
+            use_ssl => true,
+            vhost => $blog_domain,
+            location => $blog_location,
+            content => template('blog/blogs_vhosts.conf'),
+        }
 
-    file { "$blog_location":
-	    ensure => directory,
-	    owner => blog,
-	    group => apache,
-	    mode => 644,
+        file { "$blog_location":
+	        ensure => directory,
+	        owner => blog,
+	        group => apache,
+	        mode => 644,
+        }
     }
 }
