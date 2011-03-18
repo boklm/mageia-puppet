616c63735ead9633673bac4249598bfd182c89f5
Damien Lallement <dams@mageia.org>
2011-03-18 18:41:02 +0000

    typo
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1335 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/templates/backup_blog-db.sh
+++ modules/blog/templates/backup_blog-db.sh
@@ -4,5 +4,5 @@
 PATH_TO_FILE=${PATH_TO_FILE:-/var/lib/blog/backup}
 for locale in de el en es fr it nl pl pt ro ru tr
 do
-	/usb/bin/mysqldump --add-drop-table -h localhost blog_$locale | bzip2 -c > $PATH_TO_FILE/mageia_$locale.bak.sql.bz2
+	/usr/bin/mysqldump --add-drop-table -h localhost blog_$locale | bzip2 -c > $PATH_TO_FILE/mageia_$locale.bak.sql.bz2
 done
