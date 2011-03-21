a1853c3f55542d8b4113abc0a51c6f7727c8bbbc
Damien Lallement <dams@mageia.org>
2011-03-21 15:24:26 +0000

    udpate script to have a week of backup
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1338 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/templates/backup_blog-db.sh
+++ modules/blog/templates/backup_blog-db.sh
@@ -2,7 +2,22 @@
 
 # Initialization
 PATH_TO_FILE=${PATH_TO_FILE:-/var/lib/blog/backup}
+[ ! -f $PATH_TO_FILE/count ] && echo 0 > $PATH_TO_FILE/count
+COUNT=$(cat "$PATH_TO_FILE/count")
+# Backup each locale DB
 for locale in de el en es fr it nl pl pt ro ru tr
 do
-	/usr/bin/mysqldump --add-drop-table -h localhost blog_$locale | bzip2 -c > $PATH_TO_FILE/mageia_$locale.bak.sql.bz2
+	if [ ! -d $PATH_TO_FILE/$locale ]
+	then
+		/bin/mkdir $PATH_TO_FILE/$locale
+	fi
+	/usr/bin/mysqldump --add-drop-table -h localhost blog_$locale | bzip2 -c > $PATH_TO_FILE/$locale/mageia_$locale-$COUNT.bak.sql.bz2
 done
+# Check count file to have a week of backup in the directory
+if [ $COUNT -ne 6 ]
+then
+	COUNT=$(expr $COUNT + 1)
+else
+	COUNT="0"
+fi
+echo $COUNT > $PATH_TO_FILE/count
