994769ace071294ccd14032112e09c7fb50bce50
Michael Scherer <misc@mageia.org>
2011-03-22 15:17:55 +0000

    - add the public_restricted_list, as asked by rda
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1345 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/sympa/manifests/init.pp
+++ modules/sympa/manifests/init.pp
@@ -290,6 +290,21 @@ class sympa {
         }
     }
 
+    # list where only people from the ldap_group can post, ad where they are subscribe
+    # by default, but anybody else can subscribe to read and receive messages
+    define public_restricted_list($subject, $subscriber_ldap_group, $language = 'en', $topics = false) {
+       list{ $name:
+            subject => $subject,
+            profile => "",
+            topics => $topics,
+            language => $language,
+            subscriber_ldap_group => $subscriber_ldap_group,
+            sender_ldap_group => $subscriber_ldap_group,
+            subscription_open => true,
+        }
+    }
+
+
     # same as restricted list, but anybody can post
     define restricted_list_open($subject, $subscriber_ldap_group, $language = 'en', $topics = false) {
        list{ $name:
