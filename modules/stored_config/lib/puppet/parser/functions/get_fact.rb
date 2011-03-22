573ec9ed122c0f008ad1f77764c622b02f1a9cc8
Michael Scherer <misc@mageia.org>
2011-03-22 17:27:03 +0000

    add a function to fetch stored fact from the db
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1351 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/stored_config/lib/puppet/parser/functions/get_fact.rb
@@ -0,0 +1,18 @@
+require 'puppet/rails'
+
+#  get_fact($node,$fact)
+#    -> return the fact, from stored config
+
+module Puppet::Parser::Functions
+    newfunction(:get_fact, :type => :rvalue) do |args| 
+        node = args[0]
+        fact = args[1]
+
+        Puppet::Rails.connect()
+        return Puppet::Rails::FactValue.find( :first,
+                                              :joins => [ :host, :fact_name ],
+                                              :conditions => { :fact_names => {:name => fact },
+                                                               :hosts => {:name => node }} 
+                                            ).value
+    end
+end
