ef59148222533607ca440e9dd35db149ae3065e0
Michael Scherer <misc@mageia.org>
2011-03-28 22:44:15 +0000

    - use the new type for automatic inclusion of the member of board
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1408 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/lists/manifests/init.pp
+++ deployment/lists/manifests/init.pp
@@ -131,12 +131,7 @@ class lists {
             topics => "developers",
         }
 
-        # rda asked for a list where posting is restricted to a ldap
-        # group, and where everybody can subscribe. While the name is not
-        # really reflecting the usage, the functionnal requirements are fullfilled
-        # FIXME people in the board group should be subscribed to this list
-        #      
-        sympa::announce_list_group { "board-public":
+        sympa::public_restricted_list { "board-public":
             subject => "List for public board discussion",
             reply_to => false,
             sender_ldap_group => "mga-board", 
