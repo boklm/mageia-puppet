839d023972158af9cdb41946980d7dd2eb7de2c5
Michael Scherer <misc@mageia.org>
2011-03-29 22:47:59 +0000

    add a type to be able to manage storage directory for libvirtd
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1421 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/libvirtd/files/storage_add.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import libvirt
+import sys
+
+name = sys.argv[1]
+path = sys.argv[2]
+
+storage_xml = """
+<pool type='dir'>
+  <name>%s</name>
+  <capacity>0</capacity>
+  <allocation>0</allocation>
+  <available>0</available>
+  <source>
+  </source>
+  <target>
+    <path>%s</path>
+    <permissions>
+      <mode>0700</mode>
+      <owner>-1</owner>
+      <group>-1</group>
+    </permissions>
+  </target>
+</pool>""" % ( name, path )
+
+c=libvirt.open("qemu:///system")
+c.storagePoolCreateXML(storage_xml,0)
+
--- modules/libvirtd/manifests/init.pp
+++ modules/libvirtd/manifests/init.pp
@@ -11,6 +11,23 @@ class libvirtd {
             ensure => running,
             path => "/etc/init.d/libvirtd",
         }
+
+        #TODO remove once libvirt package is fixed to manage the directory
+        file { "/etc/libvirt/storage":
+            ensure => directory,
+        }
+
+        file { "/etc/libvirt/storage/autostart":
+            ensure => directory,
+        }
+        
+        file { "/usr/local/bin/storage_add.py":
+            ensure => present,
+            owner => root,
+            group => root,
+            mode => 755,
+            source => "puppet:///libvirtd/storage_add.py", 
+        }
     }
 
     class kvm inherits base {
@@ -31,6 +48,22 @@ class libvirtd {
             mode => 644,
             ensure => present,
             content => template("libvirtd/50-template-libvirt-remote-access.pkla"),
+        }
+    }
+
+    define storage($path, $autostart = true) {
+        include libvirtd::base
+
+        exec { "/usr/local/bin/storage_add.py $name $path":
+            creates => "/etc/libvirt/storage/$name.xml",
+            require => File['/usr/local/bin/storage_add.py'],
+        }
+
+        file { "/etc/libvirt/storage/autostart/$name.xml":
+            ensure => $autostart ? {
+                            true => "/etc/libvirt/storage/$name.xml",
+                            false => "absent"
+                      }
         }
     }
 }
