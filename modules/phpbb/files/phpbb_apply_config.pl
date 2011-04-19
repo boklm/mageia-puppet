d16a906fb6f28d48dd0351ee8b1da722aba367c8
Michael Scherer <misc@mageia.org>
2011-04-19 12:46:07 +0000

    use a transaction and close the database, maybe this will help to avoid the timeout problem I see from time to time
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1541 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/phpbb/files/phpbb_apply_config.pl
+++ modules/phpbb/files/phpbb_apply_config.pl
@@ -8,10 +8,15 @@ my $key = $ARGV[0];
 
 # DBI will use default value coming from env
 # see puppet manifests 
-my $dbh = DBI->connect("dbi:Pg:","","");
+my $dbh = DBI->connect("dbi:Pg:","","", {
+    AutoCommit => 0,
+    RaiseError => 1,
+});
 
 my $table = "phpbb_config";
 
+# FIXME add rollback if there is a problem 
+# http://docstore.mik.ua/orelly/linux/dbi/ch06_03.htm
 my $update = $dbh->prepare("UPDATE $table SET config_value = ?, is_dynamic = ? WHERE config_name = ?");
 my $insert = $dbh->prepare("INSERT INTO $table ( config_value, is_dynamic, config_name ) VALUES ( ? , ? , ? )");
 
@@ -19,4 +24,5 @@ my $res = $update->execute($VALUE, 1, $key) or die "cannot do update $?";
 if ($res == 0 ) {
      $insert->execute($VALUE, 1, $key) or die "cannot do insert $?";
 }
-
+$dbh->commit();
+$dbh->disconnect();
