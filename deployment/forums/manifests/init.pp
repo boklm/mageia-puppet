8518839da5f5c861dcf6f5d7871be4d5e03a11c9
Nicolas Vigier <boklm@mageia.org>
2011-05-30 13:02:37 +0000

    add redirection to french forums
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1678 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/forums/manifests/init.pp
+++ deployment/forums/manifests/init.pp
@@ -3,4 +3,8 @@ class forums {
     phpbb::instance { "en": }
 
     phpbb::instance { "de": }
+
+    phpbb:redirection_instance{ "fr":
+       url => "https://forums.mageia.org/en/viewforum.php?f=19"
+    }
 }
