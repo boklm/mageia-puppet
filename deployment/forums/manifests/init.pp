bb4f3ab87ee52ef54dfe8e3fb51df576142f2f37
Nicolas Vigier <boklm@mageia.org>
2011-05-30 13:06:37 +0000

    fix syntax
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1679 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/forums/manifests/init.pp
+++ deployment/forums/manifests/init.pp
@@ -4,7 +4,7 @@ class forums {
 
     phpbb::instance { "de": }
 
-    phpbb:redirection_instance{ "fr":
+    phpbb::redirection_instance{ "fr":
        url => "https://forums.mageia.org/en/viewforum.php?f=19"
     }
 }
