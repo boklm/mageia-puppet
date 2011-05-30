db082c45b2295ce2a3a7a070ae7a2f86b3a917eb
Nicolas Vigier <boklm@mageia.org>
2011-05-30 13:24:44 +0000

    use pt-br instead of br, and zh-cn instead of zh
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1682 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/forums/manifests/init.pp
+++ deployment/forums/manifests/init.pp
@@ -12,11 +12,11 @@ class forums {
        url => "https://forums.mageia.org/en/viewforum.php?f=22"
     }
 
-    phpbb::redirection_instance{ "zh":
+    phpbb::redirection_instance{ "zh-cn":
        url => "https://forums.mageia.org/en/viewforum.php?f=27"
     }
 
-    phpbb::redirection_instance{ "br":
+    phpbb::redirection_instance{ "pt-br":
        url => "https://forums.mageia.org/en/viewforum.php?f=28"
     }
 }
