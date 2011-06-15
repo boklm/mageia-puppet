adfc876b69d1c8a89a87adc87e50a1779e594e59
Michael Scherer <misc@mageia.org>
2011-06-15 23:01:59 +0000

    add new class for artwork team access
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1743 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/access_classes/manifests/init.pp
+++ deployment/access_classes/manifests/init.pp
@@ -36,4 +36,10 @@ class access_classes {
       access_classes => ['mga-web','mga-sysadmin']
     }
   }
+
+  class web_and_artwork {
+    pam::multiple_ldap_access { "web_artwork":
+      access_classes => ['mga-web','mga-sysadmin','mga-artwork']
+    }
+  }
 }
