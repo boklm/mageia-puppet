012d6bcac47cdad0ad6524cb1655c16fa1b3a460
Michael Scherer <misc@mageia.org>
2011-06-15 17:17:20 +0000

    add the skeleton of a manifest for mediawiki
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1735 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/mediawiki/manifests/init.pp
@@ -0,0 +1,27 @@
+class mediawiki {
+    class base {
+        package { "mediawiki":
+            ensure => installed,
+        }
+        
+        $pgsql_password = extlookup("mediawiki_pgsql",'x')
+        @@postgresql::user { $user:
+            password => $pgsql_password,
+        }
+
+        # TODO create the ldap user   
+        $ldap_password = extlookup('mediawiki_ldap','x')
+
+        # TODO write the web configuration
+
+        # add index.php
+    }        
+
+    # do wiki basic installation
+
+    define instance {
+        # define a db per instance
+        # install / link source code
+        # add config file
+    }
+}
