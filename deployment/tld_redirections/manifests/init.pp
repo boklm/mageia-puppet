74585dc8dd70242b5c47fd28867a42a660a4d238
Michael Scherer <misc@mageia.org>
2011-07-04 22:20:34 +0000

    declare tld zone on server, if needed
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1807 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/tld_redirections/manifests/init.pp
+++ deployment/tld_redirections/manifests/init.pp
@@ -1,8 +1,18 @@
 class tld_redirections {
-    define redirection {
+    define domain {
+        dns::zone { "mageia.$name": }
+    }
+
+    define redirection($managed_dns = false) {
+
+        if ($managed_dns) {
+            @@tld_redirections::domain { $name: } 
+        }
+
         apache::vhost_redirect { "mageia.$name":
             url => "http://www.mageia.org/?fromtld=$name"
         }
+
         apache::vhost_redirect { "www.mageia.$name":
             url => "http://www.mageia.org/?fromtld=$name"
         }
