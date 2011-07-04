5343cb98271c998131f1975de1773b9f8e7a0ee0
Michael Scherer <misc@mageia.org>
2011-07-04 22:20:31 +0000

    do not manage mageia.fr here
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1805 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/dns/manifests/init.pp
+++ deployment/dns/manifests/init.pp
@@ -3,7 +3,6 @@ class dns {
     class server {
         include bind::bind_master
         dns::zone { "mageia.org": }
-        dns::zone { "mageia.fr": } 
 
         dns::reverse_zone { "7.0.0.0.2.0.0.0.8.7.1.2.2.0.a.2.ip6.arpa": }
     }
