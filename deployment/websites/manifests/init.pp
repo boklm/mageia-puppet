939f385f518865d78fe9adaacbf2b046ac8e4c3e
Michael Scherer <misc@mageia.org>
2011-07-08 13:45:36 +0000

    classe for pkgcpan, asked by jq ( requires a backport before being deployed )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1825 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/init.pp
+++ deployment/websites/manifests/init.pp
@@ -80,4 +80,28 @@ class websites {
             url => "http://$web_domain/",
         } 
     }
+
+    class pkgcpan inherits base {
+        $vhost = "pkgcpan.$domain"
+        $vhostdir = "$webdatadir/$vhost"
+
+    	apache::vhost_base { "$vhost":
+	    location => $vhostdir,
+	    options => [ "Indexes" ]
+	}                
+        
+        file { $vhostdir:
+            ensure => directory,
+        }
+
+        package { "perl-Module-Packaged-Generator":
+            ensure => installed,        
+        }
+        
+        cron { "update cpanpkg":
+            hour => 23,
+            require => Package['perl-Module-Packaged-Generator'],
+            command => "pkgcpan -q -f $vhostdir/cpan_Mageia.db -d Mageia",
+        }
+    }
 }
