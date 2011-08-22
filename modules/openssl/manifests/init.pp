36c7763aa781caf2d0eb29234359a664902c41ce
Nicolas Vigier <boklm@mageia.org>
2011-08-22 15:36:44 +0000

    fix indent
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@1974 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/openssl/manifests/init.pp
+++ modules/openssl/manifests/init.pp
@@ -1,43 +1,43 @@
 class openssl {
     class base {
-        package { 'openssl':
-            ensure => installed
-        }
+	package { 'openssl':
+	    ensure => installed
+	}
     } 
 
-	define self_signed_cert($directory = '/etc/certs') {
-        include openssl::base
+    define self_signed_cert($directory = '/etc/certs') {
+	include openssl::base
+
+	$pem_file = "$name.pem"
+	exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $pem_file -out $pem_file -subj  '/CN=$name'":
+	    cwd => "$directory",
+	    creates => "$directory/$name.pem",
+	    require => Package['openssl']
+	}
+    }
+
+    define self_signed_splitted_cert($filename = '', 
+				     $directory = '/etc/certs', 
+				     $owner = "root",
+				     $group = 'root', 
+				     $mode = '600') {
+	include openssl::base
         
-        $pem_file = "$name.pem"
-	    exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $pem_file -out $pem_file -subj  '/CN=$name'":
-            cwd => "$directory",
-            creates => "$directory/$name.pem",
-            require => Package['openssl']
-        }
+	$crt_file = "$filename.crt"
+	$key_file = "$filename.key"
+	exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $key_file -out $crt_file -subj  '/CN=$name'":
+	    cwd => "$directory",
+	    creates => "$directory/$key_file",
+	    require => Package['openssl'],
+	    before => [ File["$directory/$key_file"], 
+                        File["$directory/$crt_file"]]
 	}
 
-	define self_signed_splitted_cert($filename = '', 
-                                     $directory = '/etc/certs', 
-                                     $owner = "root",
-                                     $group = 'root', 
-                                     $mode = '600') {
-        include openssl::base
-        
-        $crt_file = "$filename.crt"
-        $key_file = "$filename.key"
-	    exec { "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $key_file -out $crt_file -subj  '/CN=$name'":
-            cwd => "$directory",
-            creates => "$directory/$key_file",
-            require => Package['openssl'],
-            before => [ File["$directory/$key_file"], 
-                        File["$directory/$crt_file"]]
-        }
-
-        file { ["$directory/$key_file","$directory/$crt_file"]:
+	file { ["$directory/$key_file","$directory/$crt_file"]:
             owner => $owner,
             group => $group,
             mode => $mode
-        }
 	}
+    }
 
 }
