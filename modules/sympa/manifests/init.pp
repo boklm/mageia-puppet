bedec0444505c5aefbe83b332f66ca1788d5b266
Michael Scherer <misc@mageia.org>
2011-09-12 21:38:16 +0000

    add header to the mail on the sympa, popular request asked on sysadmin. Disable previous commit since the xml file is used only when the ml is created ( and not used after anymore )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2003 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/sympa/manifests/init.pp
+++ modules/sympa/manifests/init.pp
@@ -186,7 +186,7 @@ class sympa {
                 $subscriber_ldap_group = false,
                 $public_archive = true,
                 $subscription_open = false,
-    		$custom_subject = '') {
+    		) {
 
         include sympa::variable
         $ldap_password = extlookup("sympa_ldap",'x')
