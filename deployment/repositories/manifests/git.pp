589756de06e0fc404bcd929578d730458fd41322
Michael Scherer <misc@mageia.org>
2011-09-19 00:04:13 +0000

    remove duplicate definition, do trigger a error with puppet 2.7.3
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2008 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/git.pp
+++ deployment/repositories/manifests/git.pp
@@ -1,11 +1,4 @@
 class repositories::git {
-    file { "/git":
-        ensure => directory,
-        owner => root,
-        group => root,
-        mode => 755, 
-    }
-
     git::repository { "/git/forum":
         description => "Reference code for forum.$domain",
         group => "mga-forum-developers", 
