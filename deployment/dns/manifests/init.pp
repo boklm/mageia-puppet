d5c34f3ec26bd0108aa083501c58aeec1a841360
Pascal Terjan <pterjan@mageia.org>
2011-10-16 15:39:03 +0000

    Hack to get a reverse for rabbit's IPv6 internally
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2040 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/dns/manifests/init.pp
+++ deployment/dns/manifests/init.pp
@@ -5,6 +5,7 @@ class dns {
         dns::zone { "mageia.org": }
 
         dns::reverse_zone { "7.0.0.0.2.0.0.0.8.7.1.2.2.0.a.2.ip6.arpa": }
+        dns::reverse_zone { "2.1.0.0.0.0.0.1.b.0.e.0.1.0.a.2.ip6.arpa": }
     }
 
     define zone {
