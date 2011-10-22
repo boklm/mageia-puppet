3282345d89ec416a832b1077320a8447030aa4b7
Nicolas Vigier <boklm@mageia.org>
2011-10-22 23:55:54 +0000

    define wiki vhost in deployement/wikis
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2064 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/wikis/manifests/init.pp
+++ deployment/wikis/manifests/init.pp
@@ -1,12 +1,25 @@
 class wikis {
+    $wikis_root = '/srv/wiki'
     class { "mediawiki::config":
 	pgsql_password => extlookup("mediawiki_pgsql",'x'),
 	secretkey => extlookup("mediawiki_secretkey",'x'),
 	ldap_password => extlookup('mediawiki_ldap','x'),
+	root => $wikis_root,
+	vhost => false,
     }
 
+    $wiki_languages = [ 'en', 'fr' ]
     mediawiki::instance { "en": 
         title => "Mageia wiki",
 	wiki_settings => template('wikis/wiki_settings'),
+    }
+
+    apache::vhost_redirect_ssl { "wiki.$domain": }
+
+    apache::vhost_base { "ssl_wiki.$domain":
+	location => '/dev/null',
+	use_ssl => true,
+	vhost => "wiki.$domain",
+	content => template("wiki/wiki_vhost.conf"),
     }
 }
