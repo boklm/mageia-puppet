88f61d41a7d69d5a9c1093f6d775a89064d483fc
Nicolas Vigier <boklm@mageia.org>
2011-10-24 18:14:29 +0000

    checkout mediawiki templates
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2071 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/wikis/manifests/init.pp
+++ deployment/wikis/manifests/init.pp
@@ -1,11 +1,16 @@
 class wikis {
     $wikis_root = '/srv/wiki'
+    $wikis_templates = '/srv/wiki-templates'
     class { "mediawiki::config":
 	pgsql_password => extlookup("mediawiki_pgsql",'x'),
 	secretkey => extlookup("mediawiki_secretkey",'x'),
 	ldap_password => extlookup('mediawiki_ldap','x'),
 	root => $wikis_root,
 	vhost => false,
+    }
+
+    subversion::snapshot { $wikis_templates:
+	source => "svn://svn.$domain/svn/web/templates/mediawiki'
     }
 
     $wiki_languages = [ 'en', 'fr' ]
