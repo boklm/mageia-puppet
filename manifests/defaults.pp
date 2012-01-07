037747efce7432ce866d37fec9418892f05935f0
Michael Scherer <misc@mageia.org>
2012-01-07 22:17:50 +0000

    explictely set default value for Package and file, thus permitting us to remove it from the rest of the manifests
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2134 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -1,4 +1,15 @@
-# to not repeat the setting everywhere
-Exec { path => "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin/" }
+# to not repeat the settings everywhere
+Exec { 
+    path => "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin/", 
+}
 
+Package {
+    ensure => "present",
+}
+
+File {
+    ensure => "present",
+    owner => root,
+    group => root,
+}
 
