8de1d1d884ab1ac9a160ce142b43539510ec065f
Michael Scherer <misc@mageia.org>
2012-01-08 20:45:21 +0000

    cleaning xinetd module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2211 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/xinetd/manifests/init.pp
+++ modules/xinetd/manifests/init.pp
@@ -1,21 +1,15 @@
 class xinetd {
-    package { "xinetd":
-        ensure => installed 
-    }
+    package { "xinetd": }
 
     service { xinetd:
         ensure => running,
         path => "/etc/init.d/xinetd",
         subscribe => [ Package["xinetd"] ]
     }
-   
+
     define service($content) {
         include xinetd
         file { "/etc/xinetd.d/$name":
-            ensure => present,
-            owner => root,
-            group => root,
-            mode => 644,
             require => Package["xinetd"],
             content => $content,
             notify => Service['xinetd']
