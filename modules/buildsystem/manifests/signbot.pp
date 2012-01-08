debff80a41bc8774fb07444b7b5b18dec19fbc47
Michael Scherer <misc@mageia.org>
2012-01-08 00:48:56 +0000

    reapply commit 2142, since it was not the problem in the end
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2145 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/signbot.pp
+++ modules/buildsystem/manifests/signbot.pp
@@ -24,17 +24,17 @@ class buildsystem {
         }
 
 	    sudo::sudoers_config { "signpackage":
-            content => template("buildsystem/sudoers.signpackage")
+            content => template("buildsystem/signbot/sudoers.signpackage")
         }
 
         file { "$sign_home_dir/.rpmmacros":
 	        mode => 644,
-	        content => template("buildsystem/signbot-rpmmacros")
+	        content => template("buildsystem/signbot/signbot-rpmmacros")
 	    }
 
         file { "/usr/local/bin/sign-check-package":
             mode => 755,
-            content => template("buildsystem/sign-check-package")
+            content => template("buildsystem/signbot/sign-check-package")
         }
     }
 }
