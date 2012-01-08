faabb48158668382366d00d81fc70fe3cc766466
Michael Scherer <misc@mageia.org>
2012-01-08 12:51:23 +0000

    add a define for local_script, and set explicitely the default value for File ( 644 on file, 755 on directory )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2155 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -11,5 +11,19 @@ File {
     ensure => "present",
     owner => root,
     group => root,
+    # on directory, this will be 755
+    # see http://docs.puppetlabs.com/references/2.7.0/type.html#file
+    mode => 644,
 }
 
+define local_script($content, 
+                    $owner = "root", 
+                    $group = 'root', 
+                    $mode = '755') {
+    file { "/usr/local/bin/$name":
+        owner => $owner,
+        group => $group,
+        mode => $mode,
+        content => $content,
+    }
+}
