502cf9479a9c18bbf1d120e91d19f03bdd12f9c6
Michael Scherer <misc@mageia.org>
2012-01-08 13:45:11 +0000

    do not prefix login with signbot_, clearer this way
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2162 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/signbot.pp
+++ modules/buildsystem/manifests/signbot.pp
@@ -1,14 +1,14 @@
 class buildsystem {
     class signbot {
-        $sign_login = "signbot"
-        $sign_home_dir = "/var/lib/$sign_login"
+        $login = "signbot"
+        $sign_home_dir = "/var/lib/$login"
         $sign_keydir = "$sign_home_dir/keys"
 	    # FIXME: maybe keyid should be defined at an other place
 	    $keyid = "80420F66"
         # FIXME refactor with base class ( once variables are placed in a separate module )
         $sched_login = "schedbot"
 	
-        sshuser { $sign_login:
+        sshuser { $login:
             homedir => $sign_home_dir,
             comment => "System user used to sign packages",
 	        groups => [$sched_login],
@@ -18,7 +18,7 @@ class buildsystem {
             email => "packages@$domain",
 	        #FIXME there should be a variable somewhere to change the name of the distribution
   	        key_name => 'Mageia Packages',
-	        login => $sign_login,
+	        login => $login,
 	        batchdir => "$sign_home_dir/batches",
 	        keydir => $sign_keydir,
         }
