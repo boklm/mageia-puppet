b6f7dbc07f8e03bd9cfc4a69cfe3cae42cfa43b6
Michael Scherer <misc@mageia.org>
2012-01-08 20:45:23 +0000

    cleaning of postgrey module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2212 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/postgrey/manifests/init.pp
+++ modules/postgrey/manifests/init.pp
@@ -1,31 +1,21 @@
 class postgrey {
-    package { postgrey:
-        ensure => installed
-    }
+    package { postgrey: }
     
     service { postgrey:
         ensure => running,
         path => "/etc/init.d/postgrey",
-        subscribe => [ Package[postgrey]]
+        subscribe => Package[postgrey],
     }
 
-    file { "/etc/sysconfig/postgrey":
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 644,
-        content => template("postgrey/postgrey.sysconfig"),
-        notify => [ Service[postgrey] ],
+    File {
+        notify => Service[postgrey],
         require => Package[postgrey],    
     }
 
-    file { "/etc/postfix/postgrey_whitelist_clients.local":
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 644,
-        content => template("postgrey/whitelist_clients.local"),
-        require => Package[postgrey],
-        notify => [ Service[postgrey]],
+    file {
+        "/etc/sysconfig/postgrey":
+            content => template("postgrey/postgrey.sysconfig");
+        "/etc/postfix/postgrey_whitelist_clients.local":
+            content => template("postgrey/whitelist_clients.local");
     }
 }
