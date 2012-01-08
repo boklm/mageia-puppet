d561823de5c94ba5fbdc774ad8ffce90ca9cd398
Michael Scherer <misc@mageia.org>
2012-01-08 22:40:52 +0000

    cleanup and refactoring of pam module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2228 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/pam/manifests/init.pp
+++ modules/pam/manifests/init.pp
@@ -1,53 +1,30 @@
 class pam {
+    class base {
+        package { ["pam_ldap","nss_ldap","nscd"]: }
 
-  class base {
-      package { ["pam_ldap","nss_ldap","nscd"]:
-        ensure => installed,  
-      }
+        service { nscd:
+            ensure => running,
+            path => '/etc/init.d/nscd',
+        }
 
-      service { nscd:
-        ensure => running,
-        path => '/etc/init.d/nscd',
-      } 
+        file {
+            "/etc/pam.d/system-auth": content => template("pam/system-auth");
+            "/etc/nsswitch.conf": content => template("pam/nsswitch.conf");
+            "/etc/ldap.conf": content => template("pam/ldap.conf");
+        }
 
-      file { "system-auth":
-         path => "/etc/pam.d/system-auth",
-         owner => root,
-         group => root,
-         mode => 644,
-         content => template("pam/system-auth")
-      }
-
-      file { "nsswitch.conf":
-         path => "/etc/nsswitch.conf",
-         owner => root,
-         group => root,
-         mode => 644,
-         content => template("pam/nsswitch.conf")
-      }
-
-      $ldap_password = extlookup("${fqdn}_ldap_password",'x')
-      file { "ldap.secret":
-         path => "/etc/ldap.secret",
-         owner => root,
-         group => root,
-         mode => 600,
-         content => $ldap_password
-      }
- 
-      file { "ldap.conf":
-         path => "/etc/ldap.conf",
-         owner => root,
-         group => root,
-         mode => 644,
-         content => template("pam/ldap.conf")
-      }
-  } 
-
-  define multiple_ldap_access($access_classes,$restricted_shell = false) {
-    if $restricted_shell {
-      include restrictshell
+        $ldap_password = extlookup("${fqdn}_ldap_password",'x')
+        file { "ldap.secret":
+            path => "/etc/ldap.secret",
+            mode => 600,
+            content => $ldap_password
+        }
     }
-    include base
-  }
+
+    define multiple_ldap_access($access_classes,$restricted_shell = false) {
+        if $restricted_shell {
+            include restrictshell
+        }
+        include base
+    }
 }
