0ac013513ae1d8888bc8450c96f77251dbf5d734
Michael Scherer <misc@mageia.org>
2012-01-08 00:39:25 +0000

    revert commit 2140 ( since this does not work as i expected after reading source code )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2143 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/signbot.pp
+++ modules/buildsystem/manifests/signbot.pp
@@ -24,17 +24,17 @@ class buildsystem {
         }
 
 	    sudo::sudoers_config { "signpackage":
-            content => template("buildsystem/signbot/sudoers.signpackage")
+            content => template("buildsystem/sudoers.signpackage")
         }
 
         file { "$sign_home_dir/.rpmmacros":
 	        mode => 644,
-	        content => template("buildsystem/signbot/signbot-rpmmacros")
+	        content => template("buildsystem/signbot-rpmmacros")
 	    }
 
         file { "/usr/local/bin/sign-check-package":
             mode => 755,
-            content => template("buildsystem/signbot/sign-check-package")
+            content => template("buildsystem/sign-check-package")
         }
     }
 }
