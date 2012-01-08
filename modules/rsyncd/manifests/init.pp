78bc884a0fbd22885d27c4b0cb61750f4175b39c
Michael Scherer <misc@mageia.org>
2012-01-08 23:49:23 +0000

    cleaning of rsyncd module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2236 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/rsyncd/manifests/init.pp
+++ modules/rsyncd/manifests/init.pp
@@ -6,10 +6,6 @@ class rsyncd($rsyncd_conf = 'rsyncd/rsyncd.conf') {
 
     file { "rsyncd.conf":
         path => "/etc/rsyncd.conf",
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 644,
         require => Package["rsync"],
         content => template($rsyncd_conf)
     }
