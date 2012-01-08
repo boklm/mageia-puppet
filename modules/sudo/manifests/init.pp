45446125a0565de4bb2181685a5b37beb7ffc9fe
Michael Scherer <misc@mageia.org>
2012-01-08 20:45:17 +0000

    cleaning of the sudo module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2209 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,29 +1,20 @@
 class sudo {
-    package { sudo:
-	ensure => installed;
-    }
+    package { sudo: }
 
     file { "/etc/sudoers.d":
         ensure => directory,
         mode => 711,
-        owner => root,
-        group => root,
     }
 
     file { "/etc/sudoers":
-	ensure => present,
-	owner => root,
-	group => root,
-	mode => 440,
-	content => template("sudo/sudoers")
+        mode => 440,
+        content => template("sudo/sudoers")
     }
 
     define sudoers_config($content) {
-	file { "/etc/sudoers.d/$name":
-            owner => root,
-            group => root,
+        file { "/etc/sudoers.d/$name":
             mode => 440,
             content => $content,
-	}
+        }
     }
 }
