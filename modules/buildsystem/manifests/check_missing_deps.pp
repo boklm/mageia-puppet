7c360245b739dc0d6d363e9af1464001a55611fa
Michael Scherer <misc@mageia.org>
2012-01-08 13:45:04 +0000

    remove value already set by defaults
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2159 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/check_missing_deps.pp
+++ modules/buildsystem/manifests/check_missing_deps.pp
@@ -1,9 +1,6 @@
 class buildsystem {
     class check_missing_deps {
         file { "/usr/local/bin/missing-deps.sh":
-            ensure => present,
-            owner => root,
-            group => root,
             mode => 755,
             source => "puppet:///modules/buildsystem/missing-deps.sh",
         }
