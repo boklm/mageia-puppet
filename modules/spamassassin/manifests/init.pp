1a926b1a3acc42519324909cdf97f5d98e2a57b8
Michael Scherer <misc@mageia.org>
2012-01-08 22:24:47 +0000

    cleanup of spamassassin module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2220 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/spamassassin/manifests/init.pp
+++ modules/spamassassin/manifests/init.pp
@@ -1,19 +1,12 @@
 class spamassassin {
     # it should also requires make, bug fixed in cooker
     package { "spamassassin-sa-compile":
-        ensure => installed,
         notify => Exec["sa-compile"],
     }
 
-    package { "spamassassin":
-        ensure => installed, 
-    }
+    package { "spamassassin": }
 
     file { "/etc/mail/spamassassin/local.cf":
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => 644,
         require => Package["spamassassin"],
         content => template('spamassassin/local.cf')
     }
