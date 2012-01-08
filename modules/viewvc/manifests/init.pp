0cec294575862033a6e309c520b3d36ee729426d
Michael Scherer <misc@mageia.org>
2012-01-08 20:25:11 +0000

    clean viewvc module, some refactoring
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2206 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/viewvc/manifests/init.pp
+++ modules/viewvc/manifests/init.pp
@@ -1,46 +1,31 @@
 class viewvc {
-    package { ['viewvc','python-svn','python-flup']:
-        ensure => installed
-    }
+    package { ['viewvc','python-svn','python-flup']: }
     # http_expiration_time = 600
     # svn_roots = admin: svn://svn.mageia.org/svn/adm/
 
-    file { 'viewvc.conf':
-        ensure => present,
-        path => '/etc/viewvc/viewvc.conf',
+    file { '/etc/viewvc/viewvc.conf':
         content => template('viewvc/viewvc.conf'),
         notify => Service['apache'],
-        require => Package['viewvc']
+        require => Package['viewvc'],
     }
 
-    file { 'webapps.d/viewvc.conf':
-        ensure => present,
-        path => '/etc/httpd/conf/webapps.d/viewvc.conf',
+    apache::webapp_other { 'viewvc':
         content => template('viewvc/webapp.conf'),
-        notify => Service['apache'],
-        require => Package['apache'],
     }
 
-    $kill_viewvc_path = '/usr/local/sbin/kill_viewvc'
-    file { "$kill_viewvc_path":
-    	ensure => present,
-	mode => 755,
-	owner => root,
-	group => root,
-	content => template('viewvc/kill_viewvc.sh'),
+    local_script { "kill_viewvc":
+	    content => template('viewvc/kill_viewvc.sh'),
     }
 
     cron { 'kill_viewvc':
-	command => "$kill_viewvc_path",
-	hour => "*",
-	minute => "*/5",
-	user => "apache",
-	environment => "MAILTO=root",
+        command => "/usr/local/bin/kill_viewvc",
+        hour => "*",
+        minute => "*/5",
+        user => "apache",
+        environment => "MAILTO=root",
     }
 
-    # need newer version of viewvc
     apache::vhost_base { "svnweb.$domain":
-        # TODO created a full fledged type
         aliases => { "/viewvc" => "/var/www/viewvc/",
                      "/" => "/usr/share/viewvc/bin/wsgi/viewvc.fcgi/" }, 
         content => template("viewvc/vhost.conf")
