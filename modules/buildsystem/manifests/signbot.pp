9161131f3251c9d16527fad69ad10e64890bed49
Michael Scherer <misc@mageia.org>
2012-01-08 15:51:40 +0000

    do not use a prefix for the variable ( not needed since we placed it in its own submobule )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2181 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/signbot.pp
+++ modules/buildsystem/manifests/signbot.pp
@@ -1,15 +1,15 @@
 class buildsystem {
     class signbot {
         $login = "signbot"
-        $sign_home_dir = "/var/lib/$login"
-        $sign_keydir = "$sign_home_dir/keys"
+        $home_dir = "/var/lib/$login"
+        $sign_keydir = "$home_dir/keys"
 	    # FIXME: maybe keyid should be defined at an other place
 	    $keyid = "80420F66"
         # FIXME refactor with base class ( once variables are placed in a separate module )
         $sched_login = "schedbot"
 	
         sshuser { $login:
-            homedir => $sign_home_dir,
+            homedir => $home_dir,
             comment => "System user used to sign packages",
 	        groups => [$sched_login],
         }
@@ -19,7 +19,7 @@ class buildsystem {
 	        #FIXME there should be a variable somewhere to change the name of the distribution
   	        key_name => 'Mageia Packages',
 	        login => $login,
-	        batchdir => "$sign_home_dir/batches",
+	        batchdir => "$home_dir/batches",
 	        keydir => $sign_keydir,
         }
 
@@ -27,7 +27,7 @@ class buildsystem {
             content => template("buildsystem/signbot/sudoers.signpackage")
         }
 
-        file { "$sign_home_dir/.rpmmacros":
+        file { "$home_dir/.rpmmacros":
 	        content => template("buildsystem/signbot/signbot-rpmmacros")
 	    }
 
