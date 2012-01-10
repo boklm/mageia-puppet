boklm/mageia-puppet

commit fad03547848c900c7ff7ee562779a6e2810e11df

refactor, reindent and cleanup the mirror module
git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2202 db665093-e049-48a9-b3c2-13f1584ce46d

modules/mirror/manifests/init.pp
@@ -1,84 +1,75 @@
 class mirror {
     class base {
-	$bindir = '/usr/local/bin'
-	$locksdir = '/home/mirror/locks'
-	file { "$locksdir":
-	    ensure => directory,
+        $locksdir = '/home/mirror/locks'
+
+        file { "$locksdir":
+            ensure => directory,
             owner => mirror,
-	    group => mirror,
-	    mode => 755
-	}
+            group => mirror,
+        }
 
-	group {"mirror":
-	    ensure => present,
-	}
+        group {"mirror": }
 
-	user {"mirror":
-	    ensure => present,
-		   comment => "System user use to run mirror scripts",
-		   managehome => true,
-		   gid => mirror,
-		   shell => "/bin/bash",
-	}
+        user {"mirror":
+            comment => "System user use to run mirror scripts",
+            gid => mirror,
+        }
     }
 
     define mirrordir ($remoteurl, $localdir, $rsync_options="-avH --delete") {
     	include base
-	$lockfile = "$locksdir/$name"
-	file { "$localdir":
-	    ensure => directory,
-            owner => mirror,
-	    group => mirror,
-	    mode => 755,
-	}
-    	file { "mirror_$name":
-	    path => "$bindir/mirror_$name",
-	    mode => 755,
-	    content => template("mirror/mirrordir"),
-	}
+        $lockfile = "$locksdir/$name"
 
-	cron { "mirror_$name":
-	    user => mirror,
-	    minute => '*/10',
-	    command => "$bindir/mirror_$name",
-	    require => File["mirror_$name"],
-	}
+        file { "$localdir":
+            ensure => directory,
+            owner => mirror,
+            group => mirror,
+        }
+
+    	local_script { "mirror_$name":
+            content => template("mirror/mirrordir"),
+        }
+
+        cron { "mirror_$name":
+            user => mirror,
+            minute => '*/10',
+            command => "$bindir/mirror_$name",
+            require => File["mirror_$name"],
+        }
     }
 
     # For main Mageia mirror
     class main inherits base {
-	file { "update_timestamp":
-	    path => "$bindir/update_timestamp",
-	    mode => 755,
-	    content => template("mirror/update_timestamp")
-	}
+        local_script { "update_timestamp":
+            content => template("mirror/update_timestamp")
+        }
 
-	cron { mirror:
-	    user => mirror,
-	    minute => '*/10',
-	    command => "$bindir/update_timestamp",
-	    require => [File["update_timestamp"], User['mirror']],
-	}
+        cron { mirror:
+            user => mirror,
+            minute => '*/10',
+            command => "/usr/local/bin/update_timestamp",
+            require => [File["update_timestamp"], User['mirror']],
+        }
     }
 
     class mageia inherits base {
     	mirrordir { "mageia":
-	    remoteurl => "rsync://rsync.$domain/mageia",
-	    localdir => '/distrib/mageia',
-	}
+            remoteurl => "rsync://rsync.$domain/mageia",
+            localdir => '/distrib/mageia',
+        }
     }
 
     class newrelease inherits base {
     	mirrordir { "newrelease":
-	    remoteurl => "rsync://rsync.$domain/newrelease",
-	    localdir => '/distrib/newrelease',
-	}
+            remoteurl => "rsync://rsync.$domain/newrelease",
+            localdir => '/distrib/newrelease',
+	    }
     }
 
     class mdv2010spring inherits base {
     	mirrordir { "mdv2010.1":
-	    remoteurl => "rsync://distrib-coffee.ipsl.jussieu.fr/pub/linux/MandrivaLinux/official/2010.1",
-	    localdir => '/distrib/mandriva/',
-	}
+            remoteurl => "rsync://distrib-coffee.ipsl.jussieu.fr/pub/linux/MandrivaLinux/official/2010.1",
+            localdir => '/distrib/mandriva/',
+        }
     }
 }

commit 57ba15c51cc9ffaa20a5204a31ba105c37a68a98

fix mirroring by adding back bindir (broken by r2202)
git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2239 db665093-e049-48a9-b3c2-13f1584ce46d

modules/mirror/manifests/init.pp
@@ -1,5 +1,6 @@
 class mirror {
     class base {
+        $bindir = '/usr/local/bin'
         $locksdir = '/home/mirror/locks'
 
         file { "$locksdir":