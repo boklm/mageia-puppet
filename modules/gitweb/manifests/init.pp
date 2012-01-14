9a5afc6f60a5b7552f3fad227be0e52ba5b613c6
Michael Scherer <misc@mageia.org>
2012-01-14 19:26:11 +0000

    clean the gitweb module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2242 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/gitweb/manifests/init.pp
+++ modules/gitweb/manifests/init.pp
@@ -1,32 +1,21 @@
 class gitweb {
-    package { 'gitweb':
-        ensure => installed,
-    }
+    package { 'gitweb': }
     # TODO some rpm may be needed ( like perl-FCGI )
     # git >= 17.2 is needed for fastcgi support
 
     # TODO fix git rpm to show the css, the js, and others missing file
 
-    file { 'gitweb.conf':
-        ensure => present,
-        path => '/etc/gitweb.conf',
+    file { '/etc/gitweb.conf':
         content => template('gitweb/gitweb.conf'),
         notify => Service['apache'],
-        require => Package['gitweb']
+        require => Package['gitweb'],
     }
 
-    file { 'webapps.d/gitweb.conf':
-        ensure => present,
-        path => '/etc/httpd/conf/webapps.d/gitweb.conf',
-        content => template('gitweb/webapp.conf'),
-        notify => Service['apache'],
-        require => Package['apache'],
+    apache::webapp_other { 'gitweb':
+        webapp_file => 'gitweb/webapp.conf',
     }
    
-    file { 'gitweb.wrapper.sh':
-        ensure => present,
-        mode => 755,
-        path => '/usr/local/bin/gitweb.wrapper.sh',
+    local_script { 'gitweb.wrapper.sh':
         content => template('gitweb/wrapper.sh'),
         notify => Service['apache'],
     }
