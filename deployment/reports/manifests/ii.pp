426c8341cb2da2b26e89b74b811bd93fcaec4a3c
Michael Scherer <misc@mageia.org>
2012-01-15 10:11:59 +0000

    add reports::ii module, to report failure on irc
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2256 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ deployment/reports/manifests/ii.pp
@@ -0,0 +1,17 @@
+class reports {
+    class ii {
+        $channel = "#mageia-sysadm"
+        $server = "irc.freenode.net"
+        # tribute to Masamune Shirow
+        $nick = "project_2501"
+        
+        ii::bot { $nick:
+            channel => $channel,
+            server => $server,
+        }
+
+        file { "/etc/puppet/socket.yaml":
+            content => "reports/socket.yaml",
+        }
+    }
+}
