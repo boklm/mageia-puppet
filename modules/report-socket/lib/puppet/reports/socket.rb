8825a9807ddc911df1dfd35d4f682568e8565563
Michael Scherer <misc@mageia.org>
2012-01-15 11:15:04 +0000

    do not spam ml with non loadable reports
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2258 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/report-socket/lib/puppet/reports/socket.rb
+++ modules/report-socket/lib/puppet/reports/socket.rb
@@ -7,11 +7,16 @@ end
 
 Puppet::Reports.register_report(:socket) do
     configfile = File.join([File.dirname(Puppet.settings[:config]), "socket.yaml"])
-    raise(Puppet::ParseError, "Socket report config file #{configfile} not readable") unless File.exist?(configfile)
+    # do not raise a error since this will show in puppet log
+    #    raise(Puppet::ParseError, "Socket report config file #{configfile} not readable") unless
+    if File.exist?(configfile)
 
-    config = YAML.load_file(configfile)
-    SOCKET_PATH = config[:socket_path]
-    # TODO add support for using another user ?
+        # TODO add support for using another user ?
+        config = YAML.load_file(configfile)
+        SOCKET_PATH = config[:socket_path]
+    else
+        SOCKET_PATH = nil
+    end
 
     desc <<-DESC
               Send notification of failed reports to a socket.
