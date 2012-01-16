47d4049c79946316b011e46e1f448a4c1ce4e087
Michael Scherer <misc@mageia.org>
2012-01-16 15:25:03 +0000

    clean cron module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2286 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -2,7 +2,6 @@ class cron {
     package { "cronie": }
 
     service { crond:
-        ensure => running,
-        subscribe => [ Package["cronie"] ]
+        subscribe => Package["cronie"],
     }
 }
