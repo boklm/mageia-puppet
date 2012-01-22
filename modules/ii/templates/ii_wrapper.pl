549b03035aedc8c6cbd9452e9c9cca52310b7171
Michael Scherer <misc@mageia.org>
2012-01-22 23:31:09 +0000

    make ii run as nobody
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2330 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/ii/templates/ii_wrapper.pl
+++ modules/ii/templates/ii_wrapper.pl
@@ -1,10 +1,15 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use POSIX;
 use Proc::Daemon;
 my $nick = "<%= nick %>";
 my $server = "<%= server %>";
 
+
 Proc::Daemon::Init();
+my (undef, undef, $uid) = getpwname("nobody"); 
+POSIX::setuid($uid);
+
 fork() || exec "ii -n $nick -i /var/lib/ii/$nick -s $server";
 wait();
