8e14b83ef5f9d92ec2c5079f08a084e72252a532
Nicolas Vigier <boklm@mageia.org>
2012-02-05 23:54:47 +0000

    add mgasoft module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2349 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/mgasoft/manifests/init.pp
@@ -0,0 +1,38 @@
+class mgasoft(
+    $anonsvn_soft = 'svn+ssh://svn.mageia.org/svn/soft',
+    $pubinfodir = '/var/lib/mgasoft/infos',
+    $pubmirrordir = '/distrib/mirror/software',
+    $svn_soft_publish = 'svn+ssh://svn.mageia.org/svn/soft_publish',
+    $mgasoft_login = 'mgasoft'
+) {
+    group { $mgasoft_login:
+	ensure => present,
+    }
+    user { $mgasoft_login:
+	ensure => present,
+	comment => "System user to publish software",
+	managehome => true,
+	home => "/var/lib/$mgasoft_login",
+	gid => $mgasoft_login,
+	require => Group[$mgasoft_login],
+    }
+
+    package { 'mgasoft-publish':
+	ensure => installed,
+    }
+
+    file { '/etc/mgasoft.conf':
+	ensure => present,
+	owner => root,
+	group => root,
+	mode => 644,
+	content => template('mgasoft/mgasoft.conf'),
+    }
+
+    subversion::snapshot { $pubinfodir:
+	source => $svn_soft_publish,
+	user => $mgasoft_login,
+	refresh => '0',
+	requires => User[$mgasoft_login],
+    }
+}
