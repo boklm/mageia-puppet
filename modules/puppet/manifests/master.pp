d17df05797f1fda052ec42692eabb4b94e140e1b
Michael Scherer <misc@mageia.org>
2012-02-09 09:10:53 +0000

    split puppet::master in a separate file, reindent it and add a include for puppet::queue since that's needed
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2368 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/puppet/manifests/master.pp
@@ -0,0 +1,38 @@
+class puppet::master {
+    include puppet::client
+    include puppet::queue
+
+    # rails and sqlite3 are used for stored config
+    package { ['ruby-sqlite3', 'puppet-server', 'ruby-rails']: }
+
+    service { puppetmaster:
+        subscribe => [ Package['puppet-server'], File['/etc/puppet/puppet.conf']]
+    }
+
+    file { '/etc/puppet/extdata':
+        ensure => directory,
+        owner  => puppet,
+        group  => puppet,
+        mode   => '0700',
+        recurse => true,
+    }
+
+    file { '/etc/puppet/tagmail.conf':
+        content => template("puppet/tagmail.conf"),
+    }
+
+    tidy { '/var/lib/puppet/reports':
+        age     => "4w",
+        matches => "*.yaml",
+        recurse => true,
+        type    => "mtime",
+    }
+
+    file { '/etc/puppet/autosign.conf':
+        ensure  => $environment ? {
+                    'test'  => 'present',
+                    default => 'absent',
+        },
+        content => '*',
+    }
+}
