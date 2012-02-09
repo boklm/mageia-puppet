2dcfd47e059b6fae6cc692f6bc927b0f2d3269c4
Michael Scherer <misc@mageia.org>
2012-02-09 15:35:52 +0000

    make puppet-lint happier ( but still some issue ) on sudo module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2381 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,19 +1,19 @@
 class sudo {
-    package { sudo: }
+    package { 'sudo': }
 
-    file { "/etc/sudoers.d":
+    file { '/etc/sudoers.d':
         ensure => directory,
-        mode => 711,
+        mode   => '0711',
     }
 
-    file { "/etc/sudoers":
-        mode => 440,
-        content => template("sudo/sudoers")
+    file { '/etc/sudoers':
+        mode    => '0440',
+        content => template('sudo/sudoers'),
     }
 
     define sudoers_config($content) {
         file { "/etc/sudoers.d/$name":
-            mode => 440,
+            mode    => '0440',
             content => $content,
         }
     }
