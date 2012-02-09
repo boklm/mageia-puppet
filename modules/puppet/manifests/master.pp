a1c662d2c3d641ac18452e2ea6b92dd1f50b88ae
Michael Scherer <misc@mageia.org>
2012-02-09 09:11:02 +0000

    make it puppet-lint compliant, and use proper scoping for puppet 2.8 migration ( ie, use $::foo instead of $foo for facts )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2373 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -5,31 +5,32 @@ class puppet::master {
     # rails and sqlite3 are used for stored config
     package { ['ruby-sqlite3', 'puppet-server', 'ruby-rails']: }
 
-    service { puppetmaster:
-        subscribe => [ Package['puppet-server'], File['/etc/puppet/puppet.conf']]
+    service { 'puppetmaster':
+        subscribe => [Package['puppet-server'],
+                      File['/etc/puppet/puppet.conf']],
     }
 
     file { '/etc/puppet/extdata':
-        ensure => directory,
-        owner  => puppet,
-        group  => puppet,
-        mode   => '0700',
+        ensure  => directory,
+        owner   => puppet,
+        group   => puppet,
+        mode    => '0700',
         recurse => true,
     }
 
     file { '/etc/puppet/tagmail.conf':
-        content => template("puppet/tagmail.conf"),
+        content => template('puppet/tagmail.conf'),
     }
 
     tidy { '/var/lib/puppet/reports':
-        age     => "4w",
-        matches => "*.yaml",
+        age     => '4w',
+        matches => '*.yaml',
         recurse => true,
-        type    => "mtime",
+        type    => 'mtime',
     }
 
     file { '/etc/puppet/autosign.conf':
-        ensure  => $environment ? {
+        ensure  => $::environment ? {
                     'test'  => 'present',
                     default => 'absent',
         },
