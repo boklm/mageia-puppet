2312ec6ceef81c1d846df510f5f03dd862eb9f26
Michael Scherer <misc@mageia.org>
2012-02-09 12:20:51 +0000

    set locale system wide to english one, as french locale caused issue with ifconfig ( but not using them on installation make the keyboard in qwerty, causing annoying issues )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2377 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/common/manifests/init.pp
+++ deployment/common/manifests/init.pp
@@ -103,8 +103,9 @@ class common {
         include openssh::server
         include common::default_ssh_root_key
         include common::base_packages
-	include common::export_ssh_keys
-	include common::import_ssh_keys
+        include common::export_ssh_keys
+        include common::import_ssh_keys
+        include common::i18n
         include ntp
         include common::urpmi_update
         include puppet::client
