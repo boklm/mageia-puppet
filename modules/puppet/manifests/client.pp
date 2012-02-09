d413b36e72e20f592cd95b13230cadacb3c8589e
Michael Scherer <misc@mageia.org>
2012-02-09 09:11:01 +0000

    make puppet-lint happier, and use short form option for puppet invocation
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2372 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/client.pp
+++ modules/puppet/manifests/client.pp
@@ -1,23 +1,21 @@
 class puppet::client {
-    package { puppet: }
+    package { 'puppet': }
 
-    file { "/etc/puppet/puppet.conf":
-        content => template("puppet/puppet.conf"),
-        require => Package[puppet]
+    file { '/etc/puppet/puppet.conf':
+        content => template('puppet/puppet.conf'),
+        require => Package[puppet],
     }
 
-    cron { "puppet":
-        command => "/usr/sbin/puppetd --onetime --no-daemonize --logdest syslog > /dev/null 2>&1",
-        user => "root",
-        minute => fqdn_rand( 60 ),
-        ensure => present,
+    cron { 'puppet':
+        ensure  => present,
+        command => '/usr/sbin/puppetd -o --no-daemonize -l syslog >/dev/null 2>&1',
+        user    => 'root',
+        minute  => fqdn_rand( 60 ),
     }
 
     # we are using cron, so no need for the service
-    service { puppet:
-        enable => false,
+    service { 'puppet':
+        enable    => false,
         hasstatus => true,
     }
 }
-
- 
