ee1f49bf1fb4ab3d21989ee8fc8647dfe35aa2e1
Michael Scherer <misc@mageia.org>
2012-02-10 14:19:57 +0000

    clean, and make puppet-lint silent
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2385 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/wikis/manifests/init.pp
+++ deployment/wikis/manifests/init.pp
@@ -1,31 +1,30 @@
 class wikis {
     $wikis_root = '/srv/wiki'
     $wikis_templates = '/srv/wiki-templates'
-    class { "mediawiki::config":
-	pgsql_password => extlookup("mediawiki_pgsql",'x'),
-	secretkey => extlookup("mediawiki_secretkey",'x'),
-	ldap_password => extlookup('mediawiki_ldap','x'),
-	root => $wikis_root,
-	vhost => false,
+    class { 'mediawiki::config':
+        pgsql_password => extlookup('mediawiki_pgsql','x'),
+        secretkey      => extlookup('mediawiki_secretkey','x'),
+        ldap_password  => extlookup('mediawiki_ldap','x'),
+        root           => $wikis_root,
+        vhost          => false,
     }
 
     subversion::snapshot { $wikis_templates:
-	source => "svn://svn.$domain/svn/web/templates/mediawiki"
+        source => "svn://svn.$::domain/svn/web/templates/mediawiki"
     }
 
     $wiki_languages = [ 'en' ]
-    mediawiki::instance { "en": 
-        title => "Mageia wiki",
-	wiki_settings => template('wikis/wiki_settings'),
-	skinsdir => "$wikis_templates/skins",
+    mediawiki::instance { 'en':
+        title         => 'Mageia wiki',
+        wiki_settings => template('wikis/wiki_settings'),
+        skinsdir      => "$wikis_templates/skins",
     }
 
-    apache::vhost_redirect_ssl { "wiki.$domain": }
+    apache::vhost_redirect_ssl { "wiki.$::domain": }
 
-    apache::vhost_base { "ssl_wiki.$domain":
-	location => '/dev/null',
-	use_ssl => true,
-	vhost => "wiki.$domain",
-	content => template("wikis/wiki_vhost.conf"),
+    apache::vhost_base { "ssl_wiki.$::domain":
+        use_ssl => true,
+        vhost   => "wiki.$::domain",
+        content => template('wikis/wiki_vhost.conf'),
     }
 }
