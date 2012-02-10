63fdda1d7edaf7276fa10c3123f90cb3feda1cce
Michael Scherer <misc@mageia.org>
2012-02-10 14:20:09 +0000

    no longer touch to /etc/hosts, not needed anymore since last commit on apache module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2391 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/forum_proxy.pp
+++ deployment/websites/manifests/forum_proxy.pp
@@ -2,11 +2,6 @@ class buildsystem {
     class forum_proxy {
         $web_domain = "forums.$domain"
         
-        host { "$web_domain":
-            ip => '192.168.122.131',
-            ensure => 'present',
-        }
-
         apache::vhost_reverse_proxy { "$web_domain":
             url => "http://$web_domain/",
         } 
