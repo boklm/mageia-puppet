eedd46e55fd953a57bb5845ae71d0a816b79140d
Michael Scherer <misc@mageia.org>
2012-02-10 14:20:15 +0000

    make puppet-lint happy and make sure we are future proof for puppet 2.8 ( variable scoping )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2394 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/pkgcpan.pp
+++ deployment/websites/manifests/pkgcpan.pp
@@ -1,24 +1,23 @@
-class websites { 
-    class pkgcpan inherits base {
-        $vhost = "pkgcpan.$domain"
-        $vhostdir = "$webdatadir/$vhost"
+class websites::pkgcpan {
+    include websites::base
+    $vhost = "pkgcpan.$::domain"
+    $vhostdir = "$websites::base::webdatadir/$vhost"
 
-        apache::vhost_base { "$vhost":
-            location => $vhostdir,
-            options => [ "Indexes" ],
-        }                
-
-        file { $vhostdir:
-            ensure => directory,
-        }
-
-        package { "perl-Module-Packaged-Generator": }
-
-        # FIXME do not run as root ( apache or nobody should enough ) 
-        cron { "update cpanpkg":
-            hour => 23,
-            require => Package['perl-Module-Packaged-Generator'],
-            command => "pkgcpan -q -f $vhostdir/cpan_Mageia.db -d Mageia",
-        }
+    apache::vhost_base { $vhost:
+        location => $vhostdir,
+        options  => [ 'Indexes' ],
     }
-} 
+
+    file { $vhostdir:
+        ensure => directory,
+    }
+
+    package { 'perl-Module-Packaged-Generator': }
+
+    # FIXME do not run as root ( apache or nobody should enough )
+    cron { 'update cpanpkg':
+        hour    => 23,
+        require => Package['perl-Module-Packaged-Generator'],
+        command => "pkgcpan -q -f $vhostdir/cpan_Mageia.db -d Mageia",
+    }
+}
