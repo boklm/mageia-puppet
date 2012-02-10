5942821d8d812fbd068823fd3b04a4c8f88d420b
Michael Scherer <misc@mageia.org>
2012-02-10 14:20:03 +0000

    make shadow module puppet-lint clean
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2388 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/shadow/manifests/init.pp
+++ deployment/shadow/manifests/init.pp
@@ -1,8 +1,8 @@
 class shadow {
-    file {"/etc/login.defs":
-    	owner => 'root',
-	group => 'shadow',
-    	mode => 640,
-	source => 'puppet:///modules/shadow/login.defs',
+    file { '/etc/login.defs':
+        owner  => 'root',
+        group  => 'shadow',
+        mode   => '0640',
+        source => 'puppet:///modules/shadow/login.defs',
     }
 }
