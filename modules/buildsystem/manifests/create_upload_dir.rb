f597e864e2e367a1c1b52fa5d810f12107e568c6
Pascal Terjan <pterjan@mageia.org>
2012-02-18 12:57:28 +0000

    Start making create_upload_dir more flexible
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2428 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/create_upload_dir.rb
+++ modules/buildsystem/manifests/create_upload_dir.rb
@@ -1,36 +1,44 @@
 define "create_upload_dir", :owner, :group do
 
+    #FIXME: move this config info outside of this code
+    releases = {
+        'cauldron' => { 
+            'core' => ['release','updates_testing','backports_testing','backports','updates'],
+            'nonfree' => ['release','updates_testing','backports_testing','backports','updates'],
+            'tainted' => ['release','updates_testing','backports_testing','backports','updates'],
+        },
+        '1' => { 
+            'core' => ['release','updates_testing','backports_testing','backports','updates'],
+            'nonfree' => ['release','updates_testing','backports_testing','backports','updates'],
+            'tainted' => ['release','updates_testing','backports_testing','backports','updates'],
+        },
+        'infra_1' => { 
+            'infra' => ['release']
+        },
+    }
+             
+
     states = ["todo","done","failure","queue","rejected"]
-    releases = ["cauldron", "1"]
-    repositories = ["core","nonfree","tainted"]
-    medias = ['release','updates_testing','backports_testing','backports','updates']
 
     file @name, :ensure => 'directory', :owner => @owner, :group => @group 
 
     for st in states do
         file [@name, st].join('/'), :ensure => 'directory', :owner => @owner, :group => @group 
         
-        for rel in releases do
+        releases.each{|rel, repositories|
             file [@name, st, rel].join('/'), :ensure => 'directory', :owner => @owner, :group => @group
         
-            for rep in repositories do
+            repositories.each{|rep, medias|
                 file [@name, st, rel, rep].join('/'), :ensure => 'directory', :owner => @owner, :group => @group
         
                 for med in medias do
-		    if st == 'done'
+                    if st == 'done'
                        file [@name, st, rel, rep, med].join('/'), :ensure => 'directory', :owner => @owner, :group => @group, :mode => 0775
-		    else
+                    else
                        file [@name, st, rel, rep, med].join('/'), :ensure => 'directory', :owner => @owner, :group => @group
-		    end
+                    end
                 end 
-            end
-        end
-
-	rel = 'infra_1'
-	rep = 'infra'
-	med = 'release'
-	file [@name, st, rel].join('/'), :ensure => 'directory', :owner => @owner, :group => @group
-	file [@name, st, rel, rep].join('/'), :ensure => 'directory', :owner => @owner, :group => @group
-	file [@name, st, rel, rep, med].join('/'), :ensure => 'directory', :owner => @owner, :group => @group, :mode => 0775
+            }
+        }
     end
 end
