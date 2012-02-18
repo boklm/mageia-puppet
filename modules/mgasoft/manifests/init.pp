0870d968065f8ecb9ebbb6977f26b330743d26bc
Michael Scherer <misc@mageia.org>
2012-02-18 17:17:15 +0000

    some cleaning ( ie, do not repeat default values ), some puppet-lint compliance
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2436 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/mgasoft/manifests/init.pp
+++ modules/mgasoft/manifests/init.pp
@@ -5,41 +5,33 @@ class mgasoft(
     $svn_soft_publish = 'file:///svn/soft_publish',
     $mgasoft_login = 'mgasoft'
 ) {
-    group { $mgasoft_login:
-	ensure => present,
-    }
+    group { $mgasoft_login: }
+
     user { $mgasoft_login:
-	ensure => present,
-	comment => "System user to publish software",
-	managehome => true,
-	home => "/var/lib/$mgasoft_login",
-	gid => $mgasoft_login,
-	require => Group[$mgasoft_login],
+        comment    => 'System user to publish software',
+        managehome => true,
+        home       => "/var/lib/$mgasoft_login",
+        gid        => $mgasoft_login,
+        require    => Group[$mgasoft_login],
     }
 
-    package { 'mgasoft-publish':
-	ensure => installed,
-    }
+    package { 'mgasoft-publish': }
 
     file { '/etc/mgasoft.conf':
-	ensure => present,
-	owner => root,
-	group => root,
-	mode => 644,
-	content => template('mgasoft/mgasoft.conf'),
+        content => template('mgasoft/mgasoft.conf'),
     }
 
     subversion::snapshot { $pubinfodir:
-	source => $svn_soft_publish,
-	user => $mgasoft_login,
-	refresh => '0',
-	require => User[$mgasoft_login],
+        source  => $svn_soft_publish,
+        user    => $mgasoft_login,
+        refresh => '0',
+        require => User[$mgasoft_login],
     }
 
     cron { "mgasoft-publish":
-	command => '/usr/bin/mgasoft-publish',
-	user => $mgasoft_login,
-	minute => '*/5',
+        command => '/usr/bin/mgasoft-publish',
+        user    => $mgasoft_login,
+        minute  => '*/5',
         require => User[$mgasoft_login],
     }
 }
