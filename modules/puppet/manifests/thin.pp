ac203255442eac47c6340b2f01d3669979300f53
Michael Scherer <misc@mageia.org>
2012-02-19 17:45:07 +0000

    add apache configuration for the proxy ( ie, apache will serve static file and take care of ssl negocation, and thin take care of the backend )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2449 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/thin.pp
+++ modules/puppet/manifests/thin.pp
@@ -1,5 +1,12 @@
 class puppet::thin {
     package { 'ruby-thin': }
+
+    include apache::mod_ssl
+    include apache::mod_proxy
+
+    apache::vhost_other_app { 'puppet_proxy':
+	vhost_file => 'puppet/apache_proxy_vhost.conf',
+    }
    
     apache::config { "/etc/httpd/conf.d/puppet.conf":
                 content => "Listen 8140",
