88a49ff89f77748610ce709fa5af9d984edabbad
Michael Scherer <misc@mageia.org>
2012-02-19 17:45:16 +0000

    add service for the thin application server
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2453 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/thin.pp
+++ modules/puppet/manifests/thin.pp
@@ -15,9 +15,20 @@ class puppet::thin {
     $service_name = 'thin_puppet_master'
     file { '/etc/puppet/thin.yml':
 	content => template('puppet/thin.yml'),
+	notify => Service[$service_name],
     }
 
     file { '/usr/local/share/puppet.config.ru':
         content => template('puppet/config.ru'),
+    }
+
+    service { $service_name:
+        provider => base,
+        require  => [Package['ruby-thin'], 
+                     File['/etc/puppet/thin.yml'],
+                     File['/usr/local/share/puppet.config.ru']],
+        start    => 'thin -C /etc/puppet/thin.yml start',
+        stop     => 'thin -C /etc/puppet/thin.yml stop',
+        restart  => 'thin -C /etc/puppet/thin.yml restart',
     }
 }
