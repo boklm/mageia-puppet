938a1c27217261385e52349436c7980653c96684
Michael Scherer <misc@mageia.org>
2012-02-20 18:34:29 +0000

    clean and make puppet-lint happy for postgrey module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2463 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/postgrey/manifests/init.pp
+++ modules/postgrey/manifests/init.pp
@@ -1,19 +1,19 @@
 class postgrey {
-    package { postgrey: }
-    
-    service { postgrey:
-        subscribe => Package[postgrey],
+    package { 'postgrey': }
+
+    service { 'postgrey':
+        subscribe => Package['postgrey'],
     }
 
     File {
-        notify => Service[postgrey],
-        require => Package[postgrey],    
+        notify  => Service['postgrey'],
+        require => Package['postgrey'],
     }
 
     file {
-        "/etc/sysconfig/postgrey":
-            content => template("postgrey/postgrey.sysconfig");
-        "/etc/postfix/postgrey_whitelist_clients.local":
-            content => template("postgrey/whitelist_clients.local");
+        '/etc/sysconfig/postgrey':
+            content => template('postgrey/postgrey.sysconfig');
+        '/etc/postfix/postgrey_whitelist_clients.local':
+            content => template('postgrey/whitelist_clients.local');
     }
 }
