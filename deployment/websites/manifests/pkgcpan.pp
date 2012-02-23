3609f9fe271f4a0f9406ac1759c6db6d558e4d64
Nicolas Vigier <boklm@mageia.org>
2012-02-23 11:28:14 +0000

    don't run pkgcpan as root, create a user for pkgcpan
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2467 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/pkgcpan.pp
+++ deployment/websites/manifests/pkgcpan.pp
@@ -2,6 +2,13 @@ class websites::pkgcpan {
     include websites::base
     $vhost = "pkgcpan.$::domain"
     $vhostdir = "$websites::base::webdatadir/$vhost"
+    $pkgcpan_login = 'pkgcpan'
+    $pkgcpan_homedir = "/var/lib/$pkgcpan_login"
+
+    user { $pkgcpan_login:
+	managehome => true,
+	home => $pkgcpan_homedir,
+    }
 
     apache::vhost_base { $vhost:
         location => $vhostdir,
@@ -10,14 +17,17 @@ class websites::pkgcpan {
 
     file { $vhostdir:
         ensure => directory,
+	owner => $pkgcpan_login,
+	group => $pkgcpan_login,
     }
 
     package { 'perl-Module-Packaged-Generator': }
 
-    # FIXME do not run as root ( apache or nobody should enough )
     cron { 'update cpanpkg':
         hour    => 23,
         require => Package['perl-Module-Packaged-Generator'],
         command => "pkgcpan -q -f $vhostdir/cpan_Mageia.db -d Mageia",
+	user => $pkgcpan_login,
+	require => User[$pkgcpan_login],
     }
 }
