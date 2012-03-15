dbb61c5e7d56b1132e5e79e529024e9379260be7
Michael Scherer <misc@mageia.org>
2012-03-15 15:56:33 +0000

    split and clean phpbb::base
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2490 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/phpbb/manifests/base.pp
@@ -0,0 +1,49 @@
+class phpbb::base {
+    $db = 'phpbb'
+    $user = 'phpbb'
+    $forums_dir = '/var/www/forums/'
+
+    include apache::mod_php
+
+    package {['php-gd',
+              'php-xml',
+              'php-zlib',
+              'php-ftp',
+              'php-apc',
+              'php-magickwand',
+              'php-pgsql',
+              'php-ldap']: }
+
+    package { 'perl-DBD-Pg': }
+
+    file { '/usr/local/bin/phpbb_apply_config.pl':
+        mode   => '0755',
+        source => 'puppet:///modules/phpbb/phpbb_apply_config.pl',
+    }
+
+    $pgsql_password = extlookup('phpbb_pgsql','x')
+    postgresql::remote_user { $user:
+        password => $pgsql_password,
+    }
+
+    file { $forums_dir:
+        ensure => directory,
+    }
+
+    # TODO check that everything is locked down
+    apache::vhost_base { "forums.$::domain":
+        content => template('phpbb/forums_vhost.conf'),
+    }
+
+    apache::vhost_base { "ssl_forums.$::domain":
+        use_ssl => true,
+        vhost   => "forums.$::domain",
+        content => template('phpbb/forums_vhost.conf'),
+    }
+
+    file { '/etc/httpd/conf/vhosts.d/forums.d/':
+        ensure => directory,
+    }
+}
+
+
