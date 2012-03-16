4491222f39cf9478a0c00a66336e1a51fc3cca76
Michael Scherer <misc@mageia.org>
2012-03-16 22:46:12 +0000

    clean signbot.pp for puppet-lint compliance
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2503 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/signbot.pp
+++ modules/buildsystem/manifests/signbot.pp
@@ -1,39 +1,39 @@
-class buildsystem {
-    class signbot {
-        $login = "signbot"
-        $home_dir = "/var/lib/$login"
-        $sign_keydir = "$home_dir/keys"
-	    # FIXME: maybe keyid should be defined at an other place
-	    $keyid = "80420F66"
-        # FIXME refactor with base class ( once variables are placed in a separate module )
-        $sched_login = "schedbot"
-	
-        sshuser { $login:
-            homedir => $home_dir,
-            comment => "System user used to sign packages",
-	        groups => [$sched_login],
-        }
+class buildsystem::signbot {
+    $login = 'signbot'
+    $home_dir = "/var/lib/$login"
+    $sign_keydir = "$home_dir/keys"
+    # FIXME: maybe keyid should be defined at an other place
+    $keyid = '80420F66'
+    # FIXME refactor with base class ( once variables are
+    # placed in a separate module )
+    $sched_login = 'schedbot'
 
-    	gnupg::keys{"packages":
-            email => "packages@$domain",
-	        #FIXME there should be a variable somewhere to change the name of the distribution
-  	        key_name => 'Mageia Packages',
-	        login => $login,
-	        batchdir => "$home_dir/batches",
-	        keydir => $sign_keydir,
-        }
+    sshuser { $login:
+        homedir => $home_dir,
+        comment => 'System user used to sign packages',
+        groups  => [$sched_login],
+    }
 
-	    sudo::sudoers_config { "signpackage":
-            content => template("buildsystem/signbot/sudoers.signpackage")
-        }
+    gnupg::keys{ 'packages':
+        email    => "packages@$::domain",
+        #FIXME there should be a variable somewhere to change
+        # the name of the distribution
+        key_name => 'Mageia Packages',
+        login    => $login,
+        batchdir => "$home_dir/batches",
+        keydir   => $sign_keydir,
+    }
 
-        file { "$home_dir/.rpmmacros":
-	        content => template("buildsystem/signbot/signbot-rpmmacros")
-	    }
+    sudo::sudoers_config { 'signpackage':
+        content => template('buildsystem/signbot/sudoers.signpackage')
+    }
 
-        local_script {
-            "sign-check-package": content => template("buildsystem/signbot/sign-check-package");
-            "mga-signpackage": content => template("buildsystem/signbot/mga-signpackage");
-        }
+    file { "$home_dir/.rpmmacros":
+        content => template('buildsystem/signbot/signbot-rpmmacros')
+    }
+
+    local_script {
+        'sign-check-package': content => template('buildsystem/signbot/sign-check-package');
+        'mga-signpackage':    content => template('buildsystem/signbot/mga-signpackage');
     }
 }
