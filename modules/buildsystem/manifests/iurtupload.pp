1fb421c3ef98152dc4fa19ced26cf6b39595cfdb
Michael Scherer <misc@mageia.org>
2012-03-16 22:46:25 +0000

    add a check to warn use if the iurt config is invalid ( doesn't prevent breakage, but will warn faster )
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2509 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/iurtupload.pp
+++ modules/buildsystem/manifests/iurtupload.pp
@@ -4,8 +4,13 @@ class buildsystem::iurtupload {
 
     file { '/etc/iurt/upload.conf':
         require => File['/etc/iurt'],
-        content => template('buildsystem/upload.conf')
+        content => template('buildsystem/upload.conf'),
+        notify  => Exec['check iurt config'],
+    }
+
+    exec { 'check iurt config':
+        refreshonly => true,
+        cmd         => 'perl -cw /etc/iurt/upload.conf',
+        logoutput   => 'on_failure',
     }
 }
-
-
