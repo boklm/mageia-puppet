1cb12da27193e49a34d28db6ccceecdd1e4f8925
Michael Scherer <misc@mageia.org>
2012-03-16 22:46:37 +0000

    clean binrepo module for puppet-lint compliance
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2515 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/binrepo.pp
+++ modules/buildsystem/manifests/binrepo.pp
@@ -1,38 +1,42 @@
-class buildsystem {
-    class binrepo inherits base {
+class buildsystem::binrepo {
+        include buildsystem::base
         include sudo
-        $binrepo_login = "binrepo"
-        $binrepo_homedir = "/var/lib/$binrepo_login"
-        $binrepodir = "$binrepo_homedir/data"
-        $uploadinfosdir = "$binrepo_homedir/infos"
-        $uploadbinpath = '/usr/local/bin/upload-bin'
-        $uploadmail_from = "root@$domain"
-        $uploadmail_to = "packages-commits@ml.$domain"
+        $login = 'binrepo'
+        $homedir = "/var/lib/$login"
+        $repodir = "$homedir/data"
 
+        $uploadinfosdir = "$homedir/infos"
+        $uploadbinpath = '/usr/local/bin/upload-bin'
+        $uploadmail_from = "root@$::domain"
+        $uploadmail_to = "packages-commits@ml.$::domain"
+
+        # used in templates
         $packagers_committers_group = $buildsystem::base::packagers_committers_group
 
-        user {"$binrepo_login":
-            comment => "Binary files repository",
-            home => "$binrepo_homedir",
+        user { $login:
+            comment => 'Binary files repository',
+            home    => $homedir,
         }
 
-        file { [$binrepodir, $uploadinfosdir]:
+        file { [$repodir, $uploadinfosdir]:
             ensure => directory,
-            owner => $binrepo_login,
+            owner  => $login,
         }
 
         local_script {
-            "upload-bin": content => template('buildsystem/binrepo/upload-bin');
-            "wrapper.upload-bin": content => template('buildsystem/binrepo/wrapper.upload-bin');
+            'upload-bin':
+                content => template('buildsystem/binrepo/upload-bin');
+            'wrapper.upload-bin':
+                content => template('buildsystem/binrepo/wrapper.upload-bin');
         }
 
-        sudo::sudoers_config { "binrepo":
-            content => template("buildsystem/binrepo/sudoers.binrepo")
+        sudo::sudoers_config { 'binrepo':
+            content => template('buildsystem/binrepo/sudoers.binrepo')
         }
 
-        apache::vhost_base { "binrepo.$domain":
-            location => $binrepodir,
-            content => template("buildsystem/binrepo/vhost_binrepo.conf"),
+        apache::vhost_base { "binrepo.$::domain":
+            location => $repodir,
+            content  => template('buildsystem/binrepo/vhost_binrepo.conf'),
         }
     }
 }
