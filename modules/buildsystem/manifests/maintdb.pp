2c73f6bcd1cc1baef2c7b0a712054c9999cf6df7
Michael Scherer <misc@mageia.org>
2012-03-16 22:46:43 +0000

    clean maintdb.pp from all puppetlint errors
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2518 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/maintdb.pp
+++ modules/buildsystem/manifests/maintdb.pp
@@ -1,56 +1,53 @@
-class buildsystem {
-    class maintdb {
-    	include sudo
-        $login = "maintdb"
-        $homedir = "/var/lib/maintdb"
-        $dbdir = "$homedir/db"
-        $binpath = "/usr/local/sbin/maintdb"
-        $dump = "/var/www/bs/data/maintdb.txt"
-        $unmaintained = "/var/www/bs/data/unmaintained.txt"
+class buildsystem::maintdb {
+    include sudo
+    $login = 'maintdb'
+    $homedir = '/var/lib/maintdb'
+    $dbdir = "$homedir/db"
+    $binpath = '/usr/local/sbin/maintdb'
+    $dump = '/var/www/bs/data/maintdb.txt'
+    $unmaintained = '/var/www/bs/data/unmaintained.txt'
 
-        user {"$login":
-            comment => "Maintainers database",
-            home => "$homedir",
-        }
+    user { $login:
+        comment => 'Maintainers database',
+        home    => $homedir,
+    }
 
-        file { ["$homedir","$dbdir"]:
-            ensure => directory,
-            owner => "$login",
-            group => "$login",
-            mode => 711,
-            require => User["$login"],
-        }
+    file { [$homedir,$dbdir]:
+        ensure  => directory,
+        owner   => $login,
+        group   => $login,
+        mode    => '0711',
+        require => User[$login],
+    }
 
-        file { "$binpath":
-            mode => 755,
-            content => template("buildsystem/maintdb/maintdb.bin")
-        }
+    file { $binpath:
+        mode    => '0755',
+        content => template('buildsystem/maintdb/maintdb.bin')
+    }
 
-        local_script { "wrapper.maintdb":
-            content => template("buildsystem/maintdb/wrapper.maintdb")
-        }
+    local_script { 'wrapper.maintdb':
+        content => template('buildsystem/maintdb/wrapper.maintdb')
+    }
 
-        sudo::sudoers_config { "maintdb":
-            content => template("buildsystem/maintdb/sudoers.maintdb")
-        }
+    sudo::sudoers_config { 'maintdb':
+        content => template('buildsystem/maintdb/sudoers.maintdb')
+    }
 
-        file { ["$dump","$dump.new",
-                "$unmaintained","$unmaintained.new"]:
-            owner => $login,
-            require => File["/var/www/bs/data"],
-        }
+    file { [$dump,"$dump.new",
+            $unmaintained,"$unmaintained.new"]:
+        owner   => $login,
+        require => File['/var/www/bs/data'],
+    }
 
-        cron { "update maintdb export":
-            user => $login,
-            command => "$binpath root get > $dump.new; cp -f $dump.new $dump; grep ' nobody\$' $dump | sed 's/ nobody\$//' > $unmaintained.new; cp -f $unmaintained.new $unmaintained",
-            minute => "*/30",
-            require => User[$login],
-        }
+    cron { 'update maintdb export':
+        user    => $login,
+        command => "$binpath root get > $dump.new; cp -f $dump.new $dump; grep ' nobody\$' $dump | sed 's/ nobody\$//' > $unmaintained.new; cp -f $unmaintained.new $unmaintained",
+        minute  => '*/30',
+        require => User[$login],
+    }
 
-        apache::vhost_base { "maintdb.$domain":
-            location => $dbdir,
-            content => template("buildsystem/maintdb/vhost_maintdb.conf"),
-        }
+    apache::vhost_base { "maintdb.$::domain":
+        location => $dbdir,
+        content  => template('buildsystem/maintdb/vhost_maintdb.conf'),
     }
 }
-
