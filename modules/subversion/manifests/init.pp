51786eefc8c2ea1e910486cb8bcad357c4c21a01
Michael Scherer <misc@mageia.org>
2012-03-17 17:55:42 +0000

    mainly style fix for init.pp, just to have less error on puppet-lint global check
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2593 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/subversion/manifests/init.pp
+++ modules/subversion/manifests/init.pp
@@ -2,57 +2,57 @@
 # https://github.com/reductivelabs/puppet-vcsrepo
 # but not integrated in puppet directly for the moment
 class subversion {
-   class server {
+    class server {
         include subversion::tools
 
-        package { "subversion-server": }
-        
+        package { 'subversion-server': }
+
         $svn_base_path = '/svn/'
 
-        xinetd::service { "svnserve":
+        xinetd::service { 'svnserve':
             content => template('subversion/xinetd')
         }
 
-        file { "$svn_base_path":
+        file { $svn_base_path:
             ensure => directory,
         }
 
-        package { ["perl-SVN-Notify-Config", "perl-SVN-Notify-Mirror"]: }
-       
-        $local_dir = "/usr/local/share/subversion/"
-        $local_dirs = ["$local_dir/pre-commit.d", "$local_dir/post-commit.d"] 
+        package { ['perl-SVN-Notify-Config', 'perl-SVN-Notify-Mirror']: }
+
+        $local_dir = '/usr/local/share/subversion/'
+        $local_dirs = ["$local_dir/pre-commit.d", "$local_dir/post-commit.d"]
         file { [$local_dir,$local_dirs]:
-             ensure => directory,
+            ensure => directory,
         }
 
         # workaround the lack of umask command in puppet < 2.7
-        local_script { "create_svn_repo.sh":
-             content => template('subversion/create_svn_repo.sh') 
+        local_script { 'create_svn_repo.sh':
+            content => template('subversion/create_svn_repo.sh')
         }
 
         file { "$local_dir/pre-commit.d/no_binary":
-            mode => 755,
-            content => template('subversion/no_binary') 
+            mode    => '0755',
+            content => template('subversion/no_binary')
         }
 
         file { "$local_dir/pre-commit.d/no_root_commit":
-            mode => 755,
-            content => template('subversion/no_root_commit') 
+            mode    => '0755',
+            content => template('subversion/no_root_commit')
         }
 
         file { "$local_dir/pre-commit.d/no_empty_message":
-            mode => 755,
-            content => template('subversion/no_empty_message') 
+            mode    => '0755',
+            content => template('subversion/no_empty_message')
         }
 
         file { "$local_dir/pre-commit.d/single_word_commit":
-            mode => 755,
-            content => template('subversion/single_word_commit') 
+            mode    => '0755',
+            content => template('subversion/single_word_commit')
         }
 
         file { "$local_dir/pre-revprop-change":
-            mode => 755,
-            content => template('subversion/pre-revprop-change') 
+            mode    => '0755',
+            content => template('subversion/pre-revprop-change')
         }
 
         # TODO : add check for
@@ -66,47 +66,45 @@ class subversion {
 
         define syntax_check($regexp_ext,$check_cmd) {
             file { "$local_dir/pre-commit.d/$name":
-                mode => 755,
-                content => template('subversion/syntax_check.sh') 
+                mode    => '0755',
+                content => template('subversion/syntax_check.sh')
             }
         }
 
 
-        syntax_check{"check_perl":
+        syntax_check{'check_perl':
             regexp_ext => '\.p[lm]$',
-            check_cmd => 'perl -c'
+            check_cmd  => 'perl -c'
         }
 
-        syntax_check{"check_puppet":
+        syntax_check{'check_puppet':
             regexp_ext => '\.pp$',
-            check_cmd => 'puppet parser validate -'
+            check_cmd  => 'puppet parser validate -'
         }
 
-        syntax_check{"check_ruby":
+        syntax_check{'check_ruby':
             regexp_ext => '\.rb$',
-            check_cmd => 'ruby -c'
+            check_cmd  => 'ruby -c'
         }
 
-        syntax_check{"check_puppet_templates":
+        syntax_check{'check_puppet_templates':
             regexp_ext => 'modules/.*/templates/.*$',
-            check_cmd => 'erb -P -x -T - | ruby -c'
+            check_cmd  => 'erb -P -x -T - | ruby -c'
         }
 
-        syntax_check{"check_po":
+        syntax_check{'check_po':
             regexp_ext => '\.po$',
-            check_cmd => 'msgfmt -c -'
+            check_cmd  => 'msgfmt -c -'
         }
 
-        syntax_check{"check_php":
+        syntax_check{'check_php':
             regexp_ext => '\.php$',
-            check_cmd => 'php -d display_errors=1 -d error_reporting="E_ALL|E_STRICT" -l'
+            check_cmd  => 'php -d display_errors=1 -d error_reporting="E_ALL|E_STRICT" -l'
         }
 
-	# needed for check_php
-        package { "php-cli": }
-
+        # needed for check_php
+        package { 'php-cli': }
     }
-
-    # TODO 
+    # TODO
     #   deploy a cronjob to make a backup file ( ie, dump in some directory )
 }
