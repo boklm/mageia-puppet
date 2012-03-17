f39eb968c3418d5239e9e3f111425beb48ac6f82
Michael Scherer <misc@mageia.org>
2012-03-17 16:53:29 +0000

    clean the module from puppet-lint error, and make sure scoping is properly used for fact. Do not hardcode the distro name either.
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2578 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/websites/manifests/www.pp
+++ deployment/websites/manifests/www.pp
@@ -1,51 +1,51 @@
-class websites {
-    class www inherits base {
-        $vhost = "www.$domain"
-        $vhostdir = "$webdatadir/www.$domain"
-        $svn_location = "svn://svn.$domain/svn/web/www/trunk"
+class websites::www {
+    include websites::base
+    $vhost = "www.$::domain"
+    $vhostdir = "$websites::base::webdatadir/$vhost"
+    $svn_location = "svn://svn.$::domain/svn/web/www/trunk"
 
-        include apache::mod_php
-        include apache::mod_geoip
+    include apache::mod_php
+    include apache::mod_geoip
 
-        # for mailman reverse proxy, on ssl
-        include apache::mod_proxy
-        include apache::mod_ssl
+    # for mailman reverse proxy, on ssl
+    include apache::mod_proxy
+    include apache::mod_ssl
 
-        subversion::snapshot { $vhostdir:
-            source => $svn_location,
-        }
-
-        file { "$vhostdir/var/tmp/cache":
-            ensure => directory,
-            owner => root,
-            group => $apache::base::apache_group,
-            mode => 0660,
-        }
-
-        apache::vhost_base { "$vhost":
-            content => template('websites/vhost_www.conf', "websites/vhost_proxy_mailman.conf"),
-            location => $vhostdir,
-            options => ['FollowSymLinks'],
-        }
-
-        apache::vhost_base { "ssl_$vhost":
-            use_ssl => true,
-            vhost => $vhost,
-            content => template('websites/vhost_www.conf', "websites/vhost_proxy_mailman_ssl.conf"),
-            location => $vhostdir,
-            options => ['FollowSymLinks'],
-        }
-
-        apache::vhost_redirect { $domain:
-            url => 'http://www.mageia.org/',
-        }
-
-        apache::vhost_redirect { "ssl_$domain":
-            use_ssl => true,
-            vhost => $domain,
-            url => 'https://www.mageia.org/',
-        }
-
-        package { ['php-mbstring', 'php-mcrypt', 'php-gettext', 'php-geoip']: }
+    subversion::snapshot { $vhostdir:
+        source => $svn_location,
     }
+
+    file { "$vhostdir/var/tmp/cache":
+        ensure => directory,
+        group  => $apache::base::apache_group,
+        mode   => '0660',
+    }
+
+    $mailman_content = template('websites/vhost_www.conf',
+                                'websites/vhost_proxy_mailman.conf')
+    apache::vhost_base { $vhost:
+        content  => $mailman_content,
+        location => $vhostdir,
+        options  => ['FollowSymLinks'],
+    }
+
+    apache::vhost_base { "ssl_$vhost":
+        use_ssl  => true,
+        vhost    => $vhost,
+        content  => $mailman_content,
+        location => $vhostdir,
+        options  => ['FollowSymLinks'],
+    }
+
+    apache::vhost_redirect { $::domain:
+        url => "http://www.$::domain/",
+    }
+
+    apache::vhost_redirect { "ssl_$::domain":
+        use_ssl => true,
+        vhost   => $::domain,
+        url     => "https://www.$::domain/",
+    }
+
+    package { ['php-mbstring', 'php-mcrypt', 'php-gettext', 'php-geoip']: }
 }
