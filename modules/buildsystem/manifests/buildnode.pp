dedc94a09db533f950c3dd3e6540745c6de16efa
Michael Scherer <misc@mageia.org>
2012-03-17 00:52:48 +0000

    add missing include
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2550 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/buildnode.pp
+++ modules/buildsystem/manifests/buildnode.pp
@@ -1,6 +1,7 @@
 class buildsystem::buildnode {
     include buildsystem::base
     include buildsystem::iurt
+    include buildsystem::scheduler::var
     # permit to scheduler to run iurt
     ssh::auth::server { $buildsystem::scheduler::var::login:
                         user => $buildsystem::iurt::user::login,
