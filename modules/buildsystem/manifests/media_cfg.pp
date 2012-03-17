252e7e5956c0a69d2c1d1e2a5d3e4bda6132fdce
Michael Scherer <misc@mageia.org>
2012-03-17 17:10:55 +0000

    fix variable name after having renamed it yesterday
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2587 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/media_cfg.pp
+++ modules/buildsystem/manifests/media_cfg.pp
@@ -5,7 +5,7 @@ define buildsystem::media_cfg() {
     $arch = $name
     $sched_login = $buildsystem::scheduler::var::login
     
-    file { "$buildsystem::repository::root/distrib/cauldron/$arch/media/media_info/media.cfg":
+    file { "$buildsystem::repository::dir/distrib/cauldron/$arch/media/media_info/media.cfg":
         owner  => $buildsystem::base::sched_login,
         group  => $buildsystem::base::sched_login,
         source => "puppet:///modules/buildsystem/$arch/media.cfg",
