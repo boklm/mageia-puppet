de5865043da015dbd7359975fd62bf44656f6c6b
Michael Scherer <misc@mageia.org>
2012-03-17 16:53:41 +0000

    split database from main postgresql file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2583 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -139,22 +139,5 @@ class postgresql {
         }
     }
 
-    # TODO convert it to a regular type ( so we can later change user and so on )
-    define database($description = "", 
-                    $user = "postgres",
-                    $callback_notify = "") {
-        exec { "createdb -O $user -U postgres $name '$description'":
-            user => root,
-            unless => "psql -A -t -U postgres -l | grep '^$name|'",
-            require => Service['postgresql'],
-        }
-
-        # this is fetched by the manifest asking the database creation, once the db have been created
-        # FIXME proper ordering ?
-        @@postgresql::database_callback { $name:
-            tag => $name,
-            callback_notify => $callback_notify,
-        }
-    }
-    
+   
 }
