2fd7abde610d08dc71cfbac4b533e8cd192b8f6f
Michael Scherer <misc@mageia.org>
2012-03-17 11:23:52 +0000

    fix variable usage
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2565 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bcd/manifests/web.pp
+++ modules/bcd/manifests/web.pp
@@ -1,16 +1,15 @@
 class bcd::web {
     include bcd::base 
-    # TODO simplify this
-    $bcd_home = $bcd::home
+    $location = "$bcd::home/public_html"
 
     apache::vhost_base { "bcd.$::domain":
-	    location => "$bcd_home/public_html",
+	    location => $location,
 	    content  => template('bcd/vhost_bcd.conf'),
 	}
 
     # not sure if that's useful, since the file is public and trivially 
     # bruteforced
-	file { "$bcd_home/htpasswd":
+	file { "$bcd::home/htpasswd":
         content => template('bcd/htpasswd')
     }
 }
