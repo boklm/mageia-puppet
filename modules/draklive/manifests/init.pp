df03ffcb95cb2b5df3f92296ce43d3b06dd409ab
Michael Scherer <misc@mageia.org>
2012-03-17 11:40:32 +0000

    - clean the module, - merge all in one single module, - do not use draklive_ prefix for namespacing purpose, as this not used or better replaced by real puppet namespace - fix puppet-lint complaint
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2569 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/draklive/manifests/init.pp
+++ modules/draklive/manifests/init.pp
@@ -1,64 +1,58 @@
 class draklive {
-    class variable {
-        $draklive_login = 'draklive'
-        $draklive_home = '/home/draklive'
-        $draklive_config = "$draklive_home/live-config"
-        $draklive_var_data = "$draklive_home/var-data"
+    $login = 'draklive'
+    $home = '/home/draklive'
+    $config = "$home/live-config"
+    $var_data = "$home/var-data"
+    # TODO merge with bcd
 	$isomakers_group = 'mga-iso_makers'
-    }
 
-    class base inherits variable {
 	include sudo
 
-        group { $draklive_login: }
+    group { $login: }
  
-        user { $draklive_login:
-            home       => $draklive_home,
-            managehome => true,
-            comment    => 'User for creating live ISOs',
-        }
+    user { $login:
+        home       => $home,
+        managehome => true,
+        comment    => 'User for creating live ISOs',
+    }
 
-        package { 'draklive': }
+    package { 'draklive': }
 
-        sudo::sudoers_config { "draklive":
-	    content => template("draklive/sudoers.draklive")
-        }
+    sudo::sudoers_config { 'draklive':
+        content => template('draklive/sudoers.draklive')
+    }
 
-	file { $draklive_var_data:
+	file { $var_data:
 	    ensure => directory,
-	    owner => $draklive_login,
-	    group => $draklive_login,
-	    mode => 755,
+	    owner  => $login,
+	    group  => $login,
+	    mode   => '0755',
 	}
 
-	file { "/var/lib/draklive":
+	file { '/var/lib/draklive':
 	     ensure => symlink,
-	     target => $draklive_var_data,
+	     target => $var_data,
 	}
 
-        subversion::snapshot { $draklive_config:
-            source => "svn://svn.$domain/soft/images-config/draklive/trunk/",
-        }
+    subversion::snapshot { $config:
+        source => "svn://svn.$::domain/soft/images-config/draklive/trunk/",
+    }
 
-        cron { "build live images":
-            command => "$draklive_config/tools/build_live.sh",
-            user => $draklive_login,
-            hour => "4",
-            minute => "30",
-        }
+    cron { 'build live images':
+        command => "$config/tools/build_live.sh",
+        user    => $login,
+        hour    => '4',
+        minute  => '30',
+    }
 
-        file { "/usr/local/bin/clean-live.sh":
-            ensure => present,
-            owner => root,
-            group => root,
-            mode => 755,
-            source => "puppet:///modules/draklive/clean-live.sh",
-        }
+    file { '/usr/local/bin/clean-live.sh':
+        mode   => '0755',
+        source => 'puppet:///modules/draklive/clean-live.sh',
+    }
     
-        cron { "clean live build data":
-            command => "/usr/local/bin/clean-live.sh",
-            hour => "4",
-            minute => "20",
-        }
+    cron { 'clean live build data':
+        command => '/usr/local/bin/clean-live.sh',
+        hour    => '4',
+        minute  => '20',
     }
 }
