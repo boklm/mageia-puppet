066ec6265eed3e1b75452570ddad2fac0409398f
Michael Scherer <misc@mageia.org>
2012-03-17 00:45:16 +0000

    fix indentation
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2548 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/iurt.pp
+++ modules/buildsystem/manifests/iurt.pp
@@ -19,7 +19,7 @@ class buildsystem::iurt {
         ensure => directory,
     }
 
-   buildsystem::iurt::config { ['1','cauldron','mandriva2010.1','infra_1']: }
+    buildsystem::iurt::config { ['1','cauldron','mandriva2010.1','infra_1']: }
 
     sudo::sudoers_config { 'iurt':
         content => template('buildsystem/iurt/sudoers.iurt')
