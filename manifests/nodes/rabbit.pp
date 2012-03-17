b2b8285892a93cf9c28ffc180acc25251a69a57a
Michael Scherer <misc@mageia.org>
2012-03-17 15:50:59 +0000

    fix usage of draklive module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2577 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes/rabbit.pp
+++ manifests/nodes/rabbit.pp
@@ -8,7 +8,7 @@ node rabbit {
     include bcd::base
     include bcd::web
     include bcd::rsync
-    include draklive::base
+    include draklive
     include access_classes::iso_makers
     include openssh::ssh_keys_from_ldap
     include mirror::mageia
