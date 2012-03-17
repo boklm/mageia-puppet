4b365d84e450cc1a98b3a9893746ad187912904c
Michael Scherer <misc@mageia.org>
2012-03-17 21:28:04 +0000

    style fix, followed puppet-lint
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2600 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes/alamut.pp
+++ manifests/nodes/alamut.pp
@@ -7,12 +7,12 @@ node alamut {
 # - api
 # - pastebin
 # - LDAP slave
-# 
+#
     include common::default_mageia_server_no_smtp
     include postgresql::server
-    postgresql::tagged { "default": }
+    postgresql::tagged { 'default': }
 
-    timezone::timezone { "Europe/Paris": }
+    timezone::timezone { 'Europe/Paris': }
 
     include catdap
     include mga-mirrors
@@ -24,9 +24,9 @@ node alamut {
 
     # temporary, just the time the vm is running there
     host { 'friteuse':
-        ip => '192.168.122.131',
+        ensure       => 'present',
+        ip           => '192.168.122.131',
         host_aliases => [ "friteuse.$domain", "forums.$domain" ],
-        ensure => 'present',
     }
 
     # to create all phpbb database on alamut
@@ -34,20 +34,20 @@ node alamut {
 
     apache::vhost_redirect_ssl { "forums.$domain": }
     apache::vhost_redirect { "forum.$domain":
-    	url => "https://forums.$domain/",
+        url => "https://forums.$domain/",
     }
     apache::vhost_redirect { "ssl_forum.$domain":
-        url => "https://forums.$domain/",
-        vhost => "forum.$domain",
+        url     => "https://forums.$domain/",
+        vhost   => "forum.$domain",
         use_ssl => true,
     }
 
-    # connect to ssl so the proxy do not shoke if trying to 
+    # connect to ssl so the proxy do not shoke if trying to
     # enforce ssl ( note that this has not been tested, maybe this
     # is uneeded )
     apache::vhost_reverse_proxy { "ssl_forums.$domain":
-        url => "https://forums.$domain/",
-        vhost => "forums.$domain",
+        url     => "https://forums.$domain/",
+        vhost   => "forums.$domain",
         use_ssl => true,
     }
 
@@ -55,7 +55,7 @@ node alamut {
 
     include libvirtd::kvm
     include lists
-    include dns::server 
+    include dns::server
     include repositories::svn_mirror
     include viewvc
 
@@ -65,27 +65,27 @@ node alamut {
 
     include xymon::server
     apache::vhost_simple { "xymon.$domain":
-        location => "/var/lib/xymon/www",
+        location => '/var/lib/xymon/www',
     }
 
-    youri-check::report_www {"check": }
+    youri-check::report_www { 'check': }
 
-    youri-check::config {"config_cauldron":
-	version => "cauldron",
+    youri-check::config {'config_cauldron':
+        version => 'cauldron',
     }
-    youri-check::report {"report_cauldron":
-	version => "cauldron",
-	hour => "*",
-	minute => 24
+    youri-check::report { 'report_cauldron':
+        version => 'cauldron',
+        hour    => '*',
+        minute  => '24'
     }
 
-    youri-check::config {"config_1":
-	version => "1",
+    youri-check::config {'config_1':
+        version => '1',
     }
-    youri-check::report {"report_1":
-	version => "1",
-	hour => "*",
-	minute => 54
+    youri-check::report {'report_1':
+        version => '1',
+        hour    => '*',
+        minute  => '54'
     }
 
     include wikis
