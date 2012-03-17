03ab9d4b56b40b35580b9f63fca81db34e24bee0
Michael Scherer <misc@mageia.org>
2012-03-17 01:25:02 +0000

    add key declaration, or ssh::auth::server complain
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2551 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/buildnode.pp
+++ modules/buildsystem/manifests/buildnode.pp
@@ -1,9 +1,14 @@
 class buildsystem::buildnode {
     include buildsystem::base
     include buildsystem::iurt
-    include buildsystem::scheduler::var
+    include buildsystem::scheduler::user
+
+    ssh::auth::key { $buildsystem::scheduler::var::login:
+        home => $buildsystem::scheduler::var::homedir,
+
+    }
     # permit to scheduler to run iurt
     ssh::auth::server { $buildsystem::scheduler::var::login:
-                        user => $buildsystem::iurt::user::login,
+        user => $buildsystem::iurt::user::login,
     }
 }
