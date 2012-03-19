3c01259fe8f84b1841b587ae4df103d0d6cc57ed
Michael Scherer <misc@mageia.org>
2012-03-19 20:36:17 +0000

    style cleaning of the module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2645 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/thin.pp
+++ modules/puppet/manifests/thin.pp
@@ -5,17 +5,17 @@ class puppet::thin {
     include apache::mod_proxy
 
     apache::vhost_other_app { 'puppet_proxy':
-	vhost_file => 'puppet/apache_proxy_vhost.conf',
+        vhost_file => 'puppet/apache_proxy_vhost.conf',
     }
-   
-    apache::config { "/etc/httpd/conf.d/puppet.conf":
-        content => "Listen 8140",
+
+    apache::config { '/etc/httpd/conf.d/puppet.conf':
+        content => 'Listen 8140',
     }
 
     $service_name = 'thin_puppet_master'
     file { '/etc/puppet/thin.yml':
-	content => template('puppet/thin.yml'),
-	notify => Service[$service_name],
+        content => template('puppet/thin.yml'),
+        notify  => Service[$service_name],
     }
 
     file { '/usr/local/share/puppet.config.ru':
@@ -24,9 +24,9 @@ class puppet::thin {
 
     service { $service_name:
         provider => base,
-        require  => [Package['ruby-thin'], 
-                     File['/etc/puppet/thin.yml'],
-                     File['/usr/local/share/puppet.config.ru']],
+        require  => [ Package['ruby-thin'],
+                      File['/etc/puppet/thin.yml'],
+                      File['/usr/local/share/puppet.config.ru']],
         start    => 'thin -C /etc/puppet/thin.yml start',
         stop     => 'thin -C /etc/puppet/thin.yml stop',
         restart  => 'thin -C /etc/puppet/thin.yml restart',
