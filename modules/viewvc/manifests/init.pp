f11cd0870b947dcd7cae0515b679eecb5a46a104
Michael Scherer <misc@mageia.org>
2012-03-19 22:23:21 +0000

    clean the module viewvc
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2657 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/viewvc/manifests/init.pp
+++ modules/viewvc/manifests/init.pp
@@ -1,11 +1,14 @@
 class viewvc {
-    package { ['viewvc','python-svn','python-flup']: }
+    package {['viewvc',
+              'python-svn',
+              'python-flup']: }
+
     # http_expiration_time = 600
     # svn_roots = admin: svn://svn.mageia.org/svn/adm/
 
     file { '/etc/viewvc/viewvc.conf':
         content => template('viewvc/viewvc.conf'),
-        notify => Service['apache'],
+        notify  => Service['apache'],
         require => Package['viewvc'],
     }
 
@@ -13,22 +16,22 @@ class viewvc {
         webapp_file => 'viewvc/webapp.conf',
     }
 
-    local_script { "kill_viewvc":
-	    content => template('viewvc/kill_viewvc.sh'),
+    local_script { 'kill_viewvc':
+        content => template('viewvc/kill_viewvc.sh'),
     }
 
     cron { 'kill_viewvc':
-        command => "/usr/local/bin/kill_viewvc",
-        hour => "*",
-        minute => "*/5",
-        user => "apache",
-        environment => "MAILTO=root",
+        command     => '/usr/local/bin/kill_viewvc',
+        hour        => '*',
+        minute      => '*/5',
+        user        => 'apache',
+        environment => 'MAILTO=root',
     }
 
-    apache::vhost_base { "svnweb.$domain":
-        aliases => { "/viewvc" => "/var/www/viewvc/",
-                     "/" => "/usr/share/viewvc/bin/wsgi/viewvc.fcgi/" }, 
-        content => template("viewvc/vhost.conf")
+    apache::vhost_base { "svnweb.$::domain":
+        aliases => {'/viewvc' => '/var/www/viewvc/',
+                    '/'       => '/usr/share/viewvc/bin/wsgi/viewvc.fcgi/'},
+        content => template('viewvc/vhost.conf')
     }
 }
 
