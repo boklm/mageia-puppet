15a484af95641b55ba307ac4abc413ce414a4f49
Michael Scherer <misc@mageia.org>
2012-03-19 19:44:36 +0000

    cleanup to make puppet-lint happy
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2639 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/forums/manifests/init.pp
+++ deployment/forums/manifests/init.pp
@@ -1,22 +1,22 @@
 class forums {
 
-    phpbb::instance { "en": }
+    phpbb::instance { 'en': }
 
-    phpbb::instance { "de": }
+    phpbb::instance { 'de': }
 
-    phpbb::redirection_instance{ "fr":
-       url => "https://forums.mageia.org/en/viewforum.php?f=19"
+    phpbb::redirection_instance{ 'fr':
+        url => 'https://forums.mageia.org/en/viewforum.php?f=19'
     }
 
-    phpbb::redirection_instance{ "es":
-       url => "https://forums.mageia.org/en/viewforum.php?f=22"
+    phpbb::redirection_instance{ 'es':
+        url => 'https://forums.mageia.org/en/viewforum.php?f=22'
     }
 
-    phpbb::redirection_instance{ "zh-cn":
-       url => "https://forums.mageia.org/en/viewforum.php?f=27"
+    phpbb::redirection_instance{ 'zh-cn':
+        url => 'https://forums.mageia.org/en/viewforum.php?f=27'
     }
 
-    phpbb::redirection_instance{ "pt-br":
-       url => "https://forums.mageia.org/en/viewforum.php?f=28"
+    phpbb::redirection_instance{ 'pt-br':
+        url => 'https://forums.mageia.org/en/viewforum.php?f=28'
     }
 }
