c3407d6d437535597f2046ad8148164ad24de878
Michael Scherer <misc@mageia.org>
2012-03-19 22:02:50 +0000

    clean rsyncd module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2654 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/rsyncd/manifests/init.pp
+++ modules/rsyncd/manifests/init.pp
@@ -1,12 +1,12 @@
 class rsyncd($rsyncd_conf = 'rsyncd/rsyncd.conf') {
 
-    xinetd::service { "rsync":
-        content => template("rsyncd/xinetd")
+    xinetd::service { 'rsync':
+        content => template('rsyncd/xinetd')
     }
 
-    file { "rsyncd.conf":
-        path => "/etc/rsyncd.conf",
-        require => Package["rsync"],
+    file { 'rsyncd.conf':
+        path    => '/etc/rsyncd.conf',
+        require => Package['rsync'],
         content => template($rsyncd_conf)
     }
 }
