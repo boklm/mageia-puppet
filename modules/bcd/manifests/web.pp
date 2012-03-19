18414a07af3ea0eb171dd41c8d31374dcb251348
Michael Scherer <misc@mageia.org>
2012-03-19 19:51:46 +0000

    whitespace fix
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2642 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bcd/manifests/web.pp
+++ modules/bcd/manifests/web.pp
@@ -1,15 +1,15 @@
 class bcd::web {
-    include bcd::base 
+    include bcd::base
     $location = "$bcd::home/public_html"
 
     apache::vhost_base { "bcd.$::domain":
-	    location => $location,
-	    content  => template('bcd/vhost_bcd.conf'),
-	}
+        location => $location,
+        content  => template('bcd/vhost_bcd.conf'),
+    }
 
-    # not sure if that's useful, since the file is public and trivially 
+    # not sure if that's useful, since the file is public and trivially
     # bruteforced
-	file { "$bcd::home/htpasswd":
+    file { "$bcd::home/htpasswd":
         content => template('bcd/htpasswd')
     }
 }
