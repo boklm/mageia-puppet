ffeedb4902680fe6a88a8863c8d0ca373043a201
Michael Scherer <misc@mageia.org>
2012-03-19 19:44:28 +0000

    another small puppetlint cleanup
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2636 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/extlookup.pp
+++ manifests/extlookup.pp
@@ -1,4 +1,4 @@
 # see http://www.devco.net/archives/2009/08/31/complex_data_and_puppet.php
-$extlookup_datadir = "/etc/puppet/extdata"
-$extlookup_precedence = ["%{fqdn}", "common"]
+$extlookup_datadir = '/etc/puppet/extdata'
+$extlookup_precedence = ['%{fqdn}', 'common']
 
