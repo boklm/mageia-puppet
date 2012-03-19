c13dd25c4b85e2da9e090144c1e8f2c7433cd201
Michael Scherer <misc@mageia.org>
2012-03-19 22:23:23 +0000

    clean the module mga-mirrors
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2658 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/mga-mirrors/manifests/init.pp
+++ modules/mga-mirrors/manifests/init.pp
@@ -1,30 +1,30 @@
 class mga-mirrors {
-    
-    $vhost = "mirrors.$domain"
+
+    $vhost = "mirrors.$::domain"
 
     package { 'mga-mirrors': }
 
     apache::vhost_catalyst_app { $vhost:
-        script => "/usr/bin/mga_mirrors_fastcgi.pl", 
+        script  => '/usr/bin/mga_mirrors_fastcgi.pl',
         require => Package['mga-mirrors'],
     }
 
-    $pgsql_password = extlookup("mga_mirror_pgsql",'x')
+    $pgsql_password = extlookup('mga_mirror_pgsql','x')
 
     postgresql::remote_db_and_user { 'mirrors':
-        password => $pgsql_password,
-        description => "Mirrors database",
+        password    => $pgsql_password,
+        description => 'Mirrors database',
     }
- 
-    file { "/etc/mga-mirrors.ini":
-        group => apache,
-        mode => 640,
-        content => template("mga-mirrors/mga-mirrors.ini"),
+
+    file { '/etc/mga-mirrors.ini':
+        group   => 'apache',
+        mode    => '0640',
+        content => template('mga-mirrors/mga-mirrors.ini'),
         require => Package['mga-mirrors']
     }
 
-    file { "/etc/cron.d/mga_mirrors":
-        content => template("mga-mirrors/cron-mga_mirrors"),
+    file { '/etc/cron.d/mga_mirrors':
+        content => template('mga-mirrors/cron-mga_mirrors'),
         require => Package['mga-mirrors']
     }
 }
