396f4207c7ce53a0a6eaa29eafa53045100c93aa
Michael Scherer <misc@mageia.org>
2012-03-19 18:01:04 +0000

    quick commit to unbreak alamut puppet application
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2628 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/phpbb/manifests/locale_db.pp
+++ modules/phpbb/manifests/locale_db.pp
@@ -1,11 +1,11 @@
-define locale_db($tag = 'default',
-                 $user = $phpbb::base::user) {
+define phpbb::locale_db($tag = 'default',
+                        $user = $phpbb::base::user) {
     postgresql::database { $name:
         description => "$lang db for phpbb forum",
-        user => $user,
-        tag => $tag,
+        user        => $user,
+        tag         => $tag,
 # this break due to the way it is remotely declared
-# this should only be a issue in case of bootstrapping again 
+# this should only be a issue in case of bootstrapping again
 #        require => Postgresql::User[$user]
     }
 }
