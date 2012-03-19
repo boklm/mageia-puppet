587c2fcf569e0ff92ed4a77ac42c39687cf638b6
Michael Scherer <misc@mageia.org>
2012-03-19 19:44:33 +0000

    small cleanup of the module buildsystem
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2638 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/init.pp
+++ modules/buildsystem/manifests/init.pp
@@ -3,16 +3,16 @@ class buildsystem {
     class sync20101 inherits base {
         $build_login = $buildsystem::iurt::user::login
         # TODO user iurt::user::homedir too
-        local_script { "sync2010.1":
-	        content => template("buildsystem/sync2010.1"),
+        local_script { 'sync2010.1':
+	        content => template('buildsystem/sync2010.1'),
         }
     }
 
     # a script to build 2010.1 packages. used on jonund
     class iurt20101 inherits base {
         $build_login = $buildsystem::iurt::user::login
-        local_script { "iurt2010.1":
-	        content => template("buildsystem/iurt2010.1"),
+        local_script { 'iurt2010.1':
+	        content => template('buildsystem/iurt2010.1'),
         }
     }
 }
