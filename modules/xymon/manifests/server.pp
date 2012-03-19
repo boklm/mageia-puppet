353550b149fa1ce16c16093996ca8a79fe94b25b
Michael Scherer <misc@mageia.org>
2012-03-19 21:13:08 +0000

    make puppet-lint silent
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2646 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/xymon/manifests/server.pp
+++ modules/xymon/manifests/server.pp
@@ -19,22 +19,27 @@ class xymon::server {
         # hobbitlaunch (started by init script) may need to be restarted for
         # changes here, for hobbitd_rrd (e.g. TEST2RRD), it is sufficient to
         # kill hobbitd_rrd, hobbitlaunch will respawn it
-        '/etc/xymon/hobbitserver.cfg': content => template('xymon/hobbitserver.cfg');
+        '/etc/xymon/hobbitserver.cfg':
+            content => template('xymon/hobbitserver.cfg');
 
         # Define hosts and web view layout, and lists tests to be run against
         # host by e.g. network tests from xymon server
-        '/etc/xymon/bb-hosts': content => template('xymon/bb-hosts');
+        '/etc/xymon/bb-hosts':
+            content => template('xymon/bb-hosts');
 
         # Defines thresholds for test data reported by clients, e.g. load
         # disk, procs, ports, memory, as well as those which require some
-        # configuration server side to the client: files, msgs, 
-        '/etc/xymon/hobbit-clients.cfg': content => template('xymon/hobbit-clients.cfg');
+        # configuration server side to the client: files, msgs,
+        '/etc/xymon/hobbit-clients.cfg':
+            content => template('xymon/hobbit-clients.cfg');
 
         # Configuration for the xymon clients, which log files to process etc.
-        '/etc/xymon/client-local.cfg': content => template('xymon/client-local.cfg');
+        '/etc/xymon/client-local.cfg':
+            content => template('xymon/client-local.cfg');
 
         # Used for alerting, changes should be taken into effect immediately
-        '/etc/xymon/hobbit-alerts.cfg': content => template('xymon/hobbit-alerts.cfg');
+        '/etc/xymon/hobbit-alerts.cfg':
+            content => template('xymon/hobbit-alerts.cfg');
     }
 
     # Most changes should take effect immediately, but sometimes threshold
