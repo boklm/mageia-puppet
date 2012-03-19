ae4622db2e7ae19a0da9497373236d11835ba9f6
Michael Scherer <misc@mageia.org>
2012-03-19 21:13:10 +0000

    clean the module to make puppet-lint happy
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2647 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/bugzilla/manifests/init.pp
+++ modules/bugzilla/manifests/init.pp
@@ -1,62 +1,62 @@
 class bugzilla {
 
-    $bugzilla_location = "/usr/share/bugzilla/template/en/custom"
+    $bugzilla_location = '/usr/share/bugzilla/template/en/custom'
 
-    package { ['bugzilla',
-               'graphviz',
-               'perl-Template-GD', # needed for graphical_report suppo 
-               'perl-Test-Taint', 
-               'perl-JSON-RPC']: }
+    package {['bugzilla',
+              'graphviz',
+              'perl-Template-GD', # needed for graphical_report support
+              'perl-Test-Taint',
+              'perl-JSON-RPC']: }
 
-    $pgsql_password = extlookup("bugzilla_pgsql",'x')
-    $ldap_password = extlookup("bugzilla_ldap",'x')
+    $pgsql_password = extlookup('bugzilla_pgsql','x')
+    $ldap_password = extlookup('bugzilla_ldap','x')
 
     postgresql::remote_db_and_user { 'bugs':
-        description => "Bugzilla database",
-        password => $pgsql_password,
+        description => 'Bugzilla database',
+        password    => $pgsql_password,
     }
 
     file { '/etc/bugzilla/localconfig':
-      group => apache,
-      mode => 640,
-      content => template("bugzilla/localconfig"),
-      require => Package[bugzilla],
+      group   => 'apache',
+      mode    => '0640',
+      content => template('bugzilla/localconfig'),
+      require => Package['bugzilla'],
     }
 
 
     file { '/var/lib/bugzilla/params':
-      group => apache,
-      mode => 640,
-      content => template("bugzilla/params"),
-      require => Package[bugzilla],
+      group   => 'apache',
+      mode    => '0640',
+      content => template('bugzilla/params'),
+      require => Package['bugzilla'],
     }
 
-    apache::webapp_other{"bugzilla":
-        webapp_file => "bugzilla/webapp_bugzilla.conf",
+    apache::webapp_other { 'bugzilla':
+        webapp_file => 'bugzilla/webapp_bugzilla.conf',
     }
 
-    $bugs_vhost = "bugs.$domain"
-    $vhost_root = "/usr/share/bugzilla/www"
+    $bugs_vhost = "bugs.$::domain"
+    $vhost_root = '/usr/share/bugzilla/www'
 
-    apache::vhost_redirect_ssl { "$bugs_vhost": }
+    apache::vhost_redirect_ssl { $bugs_vhost: }
 
-    apache::vhost_base { "$bugs_vhost":
-        aliases => { "/bugzilla/" => $vhost_root },  
-        use_ssl => true,
+    apache::vhost_base { $bugs_vhost:
+        aliases  => { '/bugzilla/' => $vhost_root },
+        use_ssl  => true,
         location => $vhost_root,
-        vhost => $bugs_vhost,
+        vhost    => $bugs_vhost,
     }
 
     subversion::snapshot { $bugzilla_location:
-      source => "svn://svn.mageia.org/svn/web/templates/bugzilla/trunk",
-      require => Package[bugzilla],
+      source  => 'svn://svn.mageia.org/svn/web/templates/bugzilla/trunk',
+      require => Package['bugzilla'],
     }
 
-    file { "custom":
-      path => "/usr/share/bugzilla/template/en/custom",
-      ensure => directory,
-      group => apache,
-      mode => 640,
+    file { 'custom':
+      ensure  => directory,
+      path    => '/usr/share/bugzilla/template/en/custom',
+      group   => 'apache',
+      mode    => '0640',
       recurse => true,
       require => Subversion::Snapshot[$bugzilla_location],
     }
