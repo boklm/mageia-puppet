06d8e9fcd2c2a3f68ce2ad868d8e7089bf67fd03
Michael Scherer <misc@mageia.org>
2012-03-20 17:42:15 +0000

    fix whitespace and line too long
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2663 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/puppet/manifests/stored_config.pp
+++ modules/puppet/manifests/stored_config.pp
@@ -1,8 +1,9 @@
 class puppet::stored_config {
-# TODO uncomment when the following problem have been fixed 
+# TODO uncomment when the following problem have been fixed
 #   - how to bootstrap the installation of the infrastructure ( since we use
-#     stored_config for postgresql::remote_db_and_user, we need to have a sqlite3 
-#     database first and then declare the database, and then switch to it )
+#     stored_config for postgresql::remote_db_and_user, we need to have a
+#     sqlite3 database first and then declare the database, and then switch
+#     to it )
 #   - how do we decide when we get sqlite3 ( for small test servers ) and
 #     when do we decide to get the real pgsql server ( for production setup )
 #
