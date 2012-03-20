d0dd1343a7b0aa212e0690dea7b94ddc50ddacd0
Michael Scherer <misc@mageia.org>
2012-03-20 17:30:35 +0000

    adapt dns deployment to the new layout
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2661 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/dns/manifests/init.pp
+++ deployment/dns/manifests/init.pp
@@ -1,21 +1,20 @@
-
 class dns {
     class server {
-        include bind::bind_master
-        dns::zone { "mageia.org": }
+        include bind::master
+        dns::zone { 'mageia.org': }
 
-        dns::reverse_zone { "7.0.0.0.2.0.0.0.8.7.1.2.2.0.a.2.ip6.arpa": }
-        dns::reverse_zone { "2.1.0.0.0.0.0.1.b.0.e.0.1.0.a.2.ip6.arpa": }
+        dns::reverse_zone { '7.0.0.0.2.0.0.0.8.7.1.2.2.0.a.2.ip6.arpa': }
+        dns::reverse_zone { '2.1.0.0.0.0.0.1.b.0.e.0.1.0.a.2.ip6.arpa': }
     }
 
     define zone {
-        bind::zone_master { $name:
+        bind::zone::master { $name:
             content => template("dns/$name.zone")
-        }        
+        }
     }
 
     define reverse_zone {
-        bind::zone_reverse { $name:
+        bind::zone::reverse { $name:
             content => template("dns/$name.zone")
         }
     }
