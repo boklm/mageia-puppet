966fccea70a3b9bf860b15d1cdd515a83e0f9b90
Michael Scherer <misc@mageia.org>
2012-03-22 10:46:57 +0000

    clean the file from puppet-lint warning
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2667 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -1,19 +1,19 @@
 # to not repeat the settings everywhere
-Exec { 
-    path => "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin/", 
+Exec {
+    path => '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin/',
 }
 
 Package {
-    ensure => "present",
+    ensure => present,
 }
 
 File {
-    ensure => "present",
-    owner => root,
-    group => root,
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
     # on directory, this will be 755
     # see http://docs.puppetlabs.com/references/2.7.0/type.html#file
-    mode => 644,
+    mode   => '0644',
 }
 
 Group {
@@ -21,23 +21,23 @@ Group {
 }
 
 User {
-    ensure => present,
+    ensure     => present,
     managehome => true,
-    shell => "/bin/bash",
+    shell      => '/bin/bash',
 }
 
 Service {
     ensure => running,
 }
 
-define local_script($content, 
-                    $owner = "root", 
-                    $group = 'root', 
-                    $mode = '755') {
+define local_script($content,
+                    $owner = 'root',
+                    $group = 'root',
+                    $mode = '0755') {
     file { "/usr/local/bin/$name":
-        owner => $owner,
-        group => $group,
-        mode => $mode,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
         content => $content,
     }
 }
