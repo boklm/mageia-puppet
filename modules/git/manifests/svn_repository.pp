0cd9ffe43d5675fe4942e0f1e0793a22117cdbcc
Michael Scherer <misc@mageia.org>
2012-03-22 10:47:11 +0000

    split svn_repository in a separate file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2672 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/git/manifests/svn_repository.pp
@@ -0,0 +1,35 @@
+define git::svn_repository( $source,
+                            $std_layout = true,
+                            $refresh = '*/5') {
+    include git::svn
+    include git::server
+    # a cron job
+    # a exec
+    if $std_layout {
+        $options = '-s'
+    } else {
+        $options =  ''
+    }
+
+    exec { "/usr/bin/git svn init $options $source $name":
+        alias   => "git svn $name",
+        creates => $name,
+    }
+
+    file { '/usr/local/bin/update_git_svn.sh':
+        mode   => '0755',
+        source => 'puppet:///modules/git/update_git_svn.sh',
+    }
+
+    cron { "update $name":
+        # done in 2 times, so fetch can fill the repo after init
+        command => "/usr/local/bin/update_git_svn.sh $name" ,
+        minute  => $refresh
+    }
+
+    file { "$name/.git/hooks/pre-receive":
+        mode    => '0755',
+        content => template('git/pre-receive'),
+        require => Exec["git svn $name"]
+    }
+}
