05e904e53863052cd1c14f083772957040a96d10
Michael Scherer <misc@mageia.org>
2012-03-25 13:17:26 +0000

    fix syntax, and properly indent the file
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2693 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/apache/manifests/vhost/base.pp
+++ modules/apache/manifests/vhost/base.pp
@@ -8,43 +8,43 @@ define apache::vhost::base ($content = '',
                             $error_logfile = false,
                             $options = [],
                             $enable_public_html = false) {
-        include apache::base
-        $httpd_logdir = '/var/log/httpd'
-        $filename = "$name.conf"
+    include apache::base
+    $httpd_logdir = '/var/log/httpd'
+    $filename = "$name.conf"
 
-        if ! $vhost {
-            $real_vhost = $name
-        } else {
-            $real_vhost = $vhost
-        }
+    if ! $vhost {
+        $real_vhost = $name
+    } else {
+        $real_vhost = $vhost
+    }
 
-        if ! $access_logfile {
-            $real_access_logfile = "$httpd_logdir/${real_vhost}-access_log"
-        } else {
-            $real_access_logfile = $access_logfile
-        }
-        if ! $error_logfile {
-            $real_error_logfile = "$httpd_logdir/${real_vhost}-error_log"
-        } else {
-            $real_error_logfile = $error_logfile
-        }
+    if ! $access_logfile {
+        $real_access_logfile = "$httpd_logdir/${real_vhost}-access_log"
+    } else {
+        $real_access_logfile = $access_logfile
+    }
+    if ! $error_logfile {
+        $real_error_logfile = "$httpd_logdir/${real_vhost}-error_log"
+    } else {
+        $real_error_logfile = $error_logfile
+    }
 
-        if $use_ssl {
-            include apache::mod::ssl
-            if $wildcard_sslcert != true {
-                openssl::self_signed_cert{ $real_vhost:
-                    directory => '/etc/ssl/apache/',
-                    before    => Apache::Config["/etc/httpd/conf/vhosts.d/$filename"],
-                }
+    if $use_ssl {
+        include apache::mod::ssl
+        if $wildcard_sslcert != true {
+            openssl::self_signed_cert{ $real_vhost:
+                directory => '/etc/ssl/apache/',
+                before    => Apache::Config["/etc/httpd/conf/vhosts.d/$filename"],
             }
         }
+    }
 
-        if $enable_public_html {
-            include apache::mod::public_html
-        }
+    if $enable_public_html {
+        include apache::mod::public_html
+    }
 
-        apache::config { "/etc/httpd/conf/vhosts.d/$filename":
-            content => template('apache/vhost_base.conf')
-        }
+    apache::config { "/etc/httpd/conf/vhosts.d/$filename":
+        content => template('apache/vhost_base.conf')
     }
 }
+
