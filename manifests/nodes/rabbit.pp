7c904e9efc402e871753520c19d568dd202b23ee
Michael Scherer <misc@mageia.org>
2012-04-02 11:05:42 +0000

    remove unused autoinstallation stuff, date back to pre-mageia 1 time, and would be replaced by virtinst if needed nowadays
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2709 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes/rabbit.pp
+++ manifests/nodes/rabbit.pp
@@ -37,39 +37,4 @@ node rabbit {
     include libvirtd::kvm
     libvirtd::group_access { 'mga-iso_makers': }
 
-    # to ease the creation of test iso
-    $netinst_iso_path = '/var/lib/libvirt/netboot'
-
-    file { $netinst_iso_path:
-        ensure => directory,
-    }
-
-    libvirtd::storage { 'netinst_iso':
-        path    => $netinst_iso_path,
-        require => File[$netinst_iso_path],
-    }
-
-    include auto_installation::download
-    auto_installation::download::netboot_images { 'mandriva':
-        path        => $netinst_iso_path,
-        versions    => ['2010.0','2010.1'],
-        archs       => ['i586','x86_64'],
-        mirror_path => 'ftp://ftp.free.fr/pub/Distributions_Linux/MandrivaLinux/official/%{version}/%{arch}/install/images/',
-        files       => ['boot.iso'],
-        require     => File[$netinst_iso_path],
-    }
-
-    # for testing pxe support of libvirt
-    include auto_installation::variables
-    libvirtd::network {'pxe_network':
-        network     => '192.168.123.0/24',
-        tftp_root   => $auto_installation::variables::pxe_dir,
-        bridge_name => 'virbr1',
-    }
-
-    include auto_installation::pxe_menu
-    auto_installation::mandriva_installation_entry { 'pxe_test':
-        version => '2010.1',
-        arch    => 'i586',
-    }
 }
