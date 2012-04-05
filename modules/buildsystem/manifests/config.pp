f4baf1e69fbffafdbf3700b33514653416b83b6f
Nicolas Vigier <boklm@mageia.org>
2012-04-05 21:36:27 +0000

    add config class for buildsystem
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2727 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/buildsystem/manifests/config.pp
@@ -0,0 +1,10 @@
+class buildsystem::config(
+    architectures = ['i586', 'x86_64'],
+    dev_distros = ['cauldron'],
+    stable_distros = ['1'],
+    distrosections = ['core', 'nonfree', 'tainted'],
+    sectionsrepos = ['release', 'updates', 'updates_testing', 'backports',
+    		     'backports_testing']
+)
+{
+}
