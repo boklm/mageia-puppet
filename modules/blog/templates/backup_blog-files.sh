d2383bc7c1e7035d876ee689ebdfc45521962bf9
Damien Lallement <dams@mageia.org>
2012-04-16 16:03:25 +0000

    add uk blog
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2737 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/blog/templates/backup_blog-files.sh
+++ modules/blog/templates/backup_blog-files.sh
@@ -5,7 +5,7 @@ PATH_TO_FILE=${PATH_TO_FILE:-<%= blog_files_backupdir %>}
 [ ! -f $PATH_TO_FILE/count ] && echo 0 > $PATH_TO_FILE/count
 COUNT=$(cat "$PATH_TO_FILE/count")
 # Backup each locale
-for locale in de el en es fr it nl pl pt ro ru tr
+for locale in de el en es fr it nl pl pt ro ru tr uk
 do
 	if [ ! -d $PATH_TO_FILE/$locale ]
 	then
