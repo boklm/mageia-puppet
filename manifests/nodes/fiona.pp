1590afdfa610511a437b56ed667fe0579a15a48f
Damien Lallement <dams@mageia.org>
2012-05-04 14:47:08 +0000

    add 'rsnapshot' module for backup Mageia.Org servers
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2744 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes/fiona.pp
+++ manifests/nodes/fiona.pp
@@ -3,9 +3,8 @@ node fiona {
 # Location: IELO datacenter (marseille)
 #
 # TODO:
-# - buy the server
-# - install the server in datacenter
 # - install a backup system
     include common::default_mageia_server
     timezone::timezone { 'Europe/Paris': }
+    include rsnapshot
 }
---
+++ modules/rsnapshot/manifests/init.pp
@@ -0,0 +1,5 @@
+class rsnapshot {
+    class base {
+        package { ['rsnapshot']: }
+	}
+}
