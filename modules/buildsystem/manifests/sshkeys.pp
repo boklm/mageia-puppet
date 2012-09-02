b77e7d8c59525ab4153dbeb62cd8bf31246f37be
Thomas Backlund <tmb@mageia.org>
2012-09-02 12:03:19 +0000

    fix includes
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2825 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/sshkeys.pp
+++ modules/buildsystem/manifests/sshkeys.pp
@@ -1,7 +1,7 @@
 class buildsystem::sshkeys {
     include ssh::auth
-    include $buildsystem::scheduler::var
-    include $buildsystem::iurt::user
+    include buildsystem::scheduler::var
+    include buildsystem::iurt::user
 
     ssh::auth::key { $buildsystem::scheduler::var::login:
 	home => $buildsystem::scheduler::var::homedir,
