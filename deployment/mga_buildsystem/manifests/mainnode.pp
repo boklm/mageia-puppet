6c101dae65d722d91f29f153f1fe2cd3e27d1606
Nicolas Vigier <boklm@mageia.org>
2012-09-04 16:09:16 +0000

    Forward ports to arm1 and arm2 ssh, to access them from outside
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2831 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/mga_buildsystem/manifests/mainnode.pp
+++ deployment/mga_buildsystem/manifests/mainnode.pp
@@ -5,4 +5,18 @@ class mga_buildsystem::mainnode {
     include buildsystem::maintdb
     include buildsystem::binrepo
     include buildsystem::repoctl
+
+    # Forward ports to arm1 and arm2 ssh, to access them from outside
+    xinetd::port_forward {"forward_arm1":
+	$target_ip => 'arm1.mageia.org',
+	$target_port => '22',
+	$port => '4251',
+	$proto => 'tcp',
+    }
+    xinetd::port_forward {"forward_arm2":
+	$target_ip => 'arm2.mageia.org',
+	$target_port => '22',
+	$port => '4252',
+	$proto => 'tcp',
+    }
 }
