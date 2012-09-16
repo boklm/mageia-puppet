82e0dced4ee453153353173d54681c280f800e10
Nicolas Vigier <boklm@mageia.org>
2012-09-16 21:14:45 +0000

    add hourly, daily, weekly, monthly cron files
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2846 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/rsnapshot/manifests/init.pp
+++ modules/rsnapshot/manifests/init.pp
@@ -8,6 +8,23 @@ class rsnapshot {
 	    group => root,
 	    mode => 0700,
 	}
+
+	@rsnapshot::cron_file { 'hourly': }
+	@rsnapshot::cron_file { 'daily': }
+	@rsnapshot::cron_file { 'weekly': }
+	@rsnapshot::cron_file { 'monthly': }
+    }
+
+    define cron_file($rsnapshot_conf = []) {
+	$filepath = "/tmp/cron.${name}_rsnapshot-backups"
+	$rsnapshot_arg = ${name}
+	file { $filepath:
+	    ensure => present,
+	    content => template('rsnapshot/cron_file'),
+	    owner => root,
+	    group => root,
+	    mode => 0755,
+	}
     }
 
     # - 'backup' is an array of "source destination" to backup
@@ -25,11 +42,33 @@ class rsnapshot {
 	$weekly_interval = '4',
 	$monthly_interval = '3'
     ) {
-	file { "${rsnapshot::base::confdir}/${name}.conf":
+	$conffile = "${rsnapshot::base::confdir}/${name}.conf"
+	file { $conffile:
 	    owner => root,
 	    group => root,
 	    mode => 0700,
 	    content => template('rsnapshot/rsnapshot.conf'),
+	}
+
+	if ($hourly_interval != '0') {
+	    Rsnapshot::Cron_file <| title == 'hourly' |> {
+		$rsnapshot_conf +> $conffile,
+	    }
+	}
+	if ($daily_interval != '0') {
+	    Rsnapshot::Cron_file <| title == 'daily' |> {
+		$rsnapshot_conf +> $conffile,
+	    }
+	}
+	if ($dweekly_interval != '0') {
+	    Rsnapshot::Cron_file <| title == 'weekly' |> {
+		$rsnapshot_conf +> $conffile,
+	    }
+	}
+	if ($monthly_interval != '0') {
+	    Rsnapshot::Cron_file <| title == 'monthly' |> {
+		$rsnapshot_conf +> $conffile,
+	    }
 	}
     }
 }
