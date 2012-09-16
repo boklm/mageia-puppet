4fe5b9d42b49a7644f97fef2b094427c44f7bd36
Nicolas Vigier <boklm@mageia.org>
2012-09-16 20:16:46 +0000

    add backup define
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2843 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/rsnapshot/manifests/init.pp
+++ modules/rsnapshot/manifests/init.pp
@@ -1,5 +1,35 @@
 class rsnapshot {
-    class base {
+    class base($confdir = '/backups/conf') {
 	package { ['rsnapshot']: }
+
+	file { $confdir:
+	    ensure => directory,
+	    owner => root,
+	    group => root,
+	    mode => 0700,
+	}
+    }
+
+    # - 'backup' is an array of "source destination" to backup
+    # - 'backup_script' is an array of "script destination"
+    # - ${x}_interval is the number of hourly, daily, weekly, monthly
+    #   backups that should be kept. If you don't want hourly, daily,
+    #   weekly or monthly backups, set ${x}_interval to '0'
+    define backup(
+	$snapshot_root = '/backups',
+	$one_fs = '1',
+	$backup = [],
+	$backup_script = [],
+	$hourly_interval = '0',
+	$daily_interval = '6',
+	$weekly_interval = '4',
+	$monthly_interval = '3'
+    ) {
+	file { "${rsnapshot::base::confdir}/${name}.conf":
+	    owner => root,
+	    group => root,
+	    mode => 0700,
+	    content => template('rsnapshot/rsnapshot.conf'),
+	}
     }
 }
