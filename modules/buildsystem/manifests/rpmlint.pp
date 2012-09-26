8f8cdc3740acfcb1913949763caf2ae7a584ba6c
Nicolas Vigier <boklm@mageia.org>
2012-09-26 22:17:33 +0000

    Remove rpmlint config. We are now using the rpmlint-mageia-policy and rpmlint-mageia-mga2-policy (for Mageia <= 2) packages to install rpmlint configuration files.
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2857 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/rpmlint.pp
+++ modules/buildsystem/manifests/rpmlint.pp
@@ -1,20 +1,3 @@
 class buildsystem::rpmlint {
-    include buildsystem::scheduler::var
-    $sched_login = $buildsystem::scheduler::var::login
-
     package { 'rpmlint': }
-
-    file { '/etc/rpmlint/config':
-        require => Package['rpmlint'],
-        content => template('buildsystem/rpmlint.conf')
-    }
-
-    # directory that hold configuration auto extracted after upload
-    # of the rpmlint policy
-    # should belong to the scheduler user, as it need to write to it
-    file { '/etc/rpmlint/extracted.d/':
-        ensure  => directory,
-        require => Package['rpmlint'],
-        owner   => $sched_login,
-    }
 }
