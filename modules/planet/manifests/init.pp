0f8c8e534006c10886fc2838c6d92df58273f7f9
Nicolas Vigier <boklm@mageia.org>
2012-12-10 18:41:27 +0000

    planet: use mga-common::local_script
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2905 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/planet/manifests/init.pp
+++ modules/planet/manifests/init.pp
@@ -17,7 +17,7 @@ class planet {
         content  => template('planet/planet_vhosts.conf')
     }
 
-    local_script { 'deploy_new-planet.sh':
+    mga-common::local_script { 'deploy_new-planet.sh':
         content => template('planet/deploy_new-planet.sh')
     }
 
@@ -36,7 +36,7 @@ class planet {
                 ensure => directory,
         }
 
-        local_script { "backup_planet-files.sh":
+        mga-common::local_script { "backup_planet-files.sh":
             content => template("blog/backup_planet-files.sh")
         }
 
