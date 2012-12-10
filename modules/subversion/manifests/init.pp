f30df78eed5ef55d4f8056280d3e54d97a479ef7
Nicolas Vigier <boklm@mageia.org>
2012-12-10 18:41:00 +0000

    subversion: use mga-common::local_script
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2894 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/subversion/manifests/init.pp
+++ modules/subversion/manifests/init.pp
@@ -26,7 +26,7 @@ class subversion {
         }
 
         # workaround the lack of umask command in puppet < 2.7
-        local_script { 'create_svn_repo.sh':
+        mga-common::local_script { 'create_svn_repo.sh':
             content => template('subversion/create_svn_repo.sh')
         }
 
--- modules/subversion/manifests/mirror.pp
+++ modules/subversion/manifests/mirror.pp
@@ -1,6 +1,6 @@
 class subversion::mirror {
     include subversion::tools
-    local_script { 'create_svn_mirror.sh':
+    mga-common::local_script { 'create_svn_mirror.sh':
         content => template('subversion/create_svn_mirror.sh')
     }
 }
