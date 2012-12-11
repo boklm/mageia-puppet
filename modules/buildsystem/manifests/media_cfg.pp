3b5f0204276c7da23f96e3b73091a32a09b214bf
Nicolas Vigier <boklm@mageia.org>
2012-12-11 18:33:23 +0000

    buildsystem::media_cfg.pp: Fix scheduler login variable
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2929 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/media_cfg.pp
+++ modules/buildsystem/manifests/media_cfg.pp
@@ -4,8 +4,8 @@ define buildsystem::media_cfg($distro, $arch, $templatefile = 'buildsystem/media
     include buildsystem::repository
     
     file { "${buildsystem::var::repository::bootstrap_reporoot}/${distro}/${arch}/media/media_info/media.cfg":
-        owner  => $buildsystem::base::sched_login,
-        group  => $buildsystem::base::sched_login,
+        owner  => $buildsystem::var::scheduler::login,
+        group  => $buildsystem::var::scheduler::login,
 	content => template($templatefile),
     }
 }
