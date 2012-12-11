1c42a2f2952cac1c6dbb58c6e45eea444cb2e4d2
Thomas Backlund <tmb@mageia.org>
2012-12-11 23:05:36 +0000

    iurt20101 is gone
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2936 db665093-e049-48a9-b3c2-13f1584ce46d

--- manifests/nodes/jonund.pp
+++ manifests/nodes/jonund.pp
@@ -4,7 +4,6 @@ node jonund {
 #
     include common::default_mageia_server
     include mga_buildsystem::buildnode
-    include buildsystem::iurt20101
     timezone::timezone { 'Europe/Paris': }
 #    include shorewall
 #    include shorewall::default_firewall
