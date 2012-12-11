ca83312204a25f57675b7a0bfbf4ef277191d7fb
Nicolas Vigier <boklm@mageia.org>
2012-12-11 18:48:20 +0000

    buildsystem::var::repository: fix syntax
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2934 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/var/repository.pp
+++ modules/buildsystem/manifests/var/repository.pp
@@ -1,7 +1,7 @@
 class buildsystem::var::repository(
     $bootstrap_root = '/distrib/bootstrap',
     $mirror_root = '/distrib/mirror',
-    $distribdir = 'distrib',
+    $distribdir = 'distrib'
 ) {
     $bootstrap_reporoot = "${bootstrap_root}/${distribdir}"
     $mirror_reporoot = "${mirror_root}/${distribdir}"
