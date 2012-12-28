a2c0266e2e8e3178c1651cc4f23a0bcbb548a6ac
Nicolas Vigier <boklm@mageia.org>
2012-12-28 15:23:02 +0000

    repositories::subversion: remove cia options
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@2946 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/subversion.pp
+++ deployment/repositories/manifests/subversion.pp
@@ -1,15 +1,9 @@
 class repositories::subversion {
 
-    Subversion::Repository {
-        cia_post          => true,
-        cia_ignore_author => '^schedbot$',
-    }
-
     subversion::repository { '/svn/adm/':
         group        => 'mga-sysadmin',
         commit_mail  => ["sysadmin-commits@ml.$::domain"],
         syntax_check => ['check_puppet_templates','check_puppet'],
-        cia_module   => 'sysadm',
     }
 
     sudo::sudoers_config { 'puppet_update':
@@ -23,35 +17,28 @@ class repositories::subversion {
     subversion::repository { '/svn/org/':
         group        => 'mga-board',
         commit_mail  => ["board-commits@ml.$::domain"],
-	cia_post     => true,
-        cia_module   => 'org',
     }
 
     subversion::repository { '/svn/soft/':
         group        => 'mga-packagers',
         commit_mail  => ["soft-commits@ml.$::domain"],
         syntax_check => ['check_po'],
-        cia_module   => 'soft',
         i18n_mail    => ["mageia-i18n@$::domain"],
     }
 
     subversion::repository { '/svn/soft_publish/':
         group       => 'mga-packagers',
         commit_mail => ["soft-commits@ml.$::domain"],
-        cia_post    => true,
-        cia_module  => 'soft_publish',
     }
 
     subversion::repository { '/svn/web/':
         group        => 'mga-web',
         syntax_check => ['check_php'],
-        cia_module   => 'web',
     }
 
     subversion::repository { '/svn/packages/':
         group       => 'mga-packagers-committers',
         no_binary   => true,
 	commit_mail => ["packages-commits@ml.$::domain"],
-        cia_module  => 'packages',
     }
 }
