81390616681ae76a0435cb2cb02ba4b08bc85b77
Nicolas Vigier <boklm@mageia.org>
2013-02-21 23:52:45 +0000

    Add mgapeople module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3007 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/mgapeople/manifests/init.pp
@@ -0,0 +1,58 @@
+class mgapeople(
+  $site_name = 'people.mageia.org',
+  $groupbase = 'ou=Group,dc=mageia,dc=org',
+  $ldap_server,
+  $binddn,
+  $bindpw,
+  $vhost,
+  $vhostdir
+){
+  $mgapeople_login = 'mgapeople'
+  $bindpw_file = '/etc/mgapeople.ldapsecret'
+
+  group { $mgapeople_login:
+    ensure => present,
+  }
+
+  user { $mgapeople_login:
+    ensure     => present,
+    comment    => 'mgapeople user',
+    home       => "/var/lib/${mgapeople_login}",
+    managehome => true,
+    gid        => $mgapeople_login,
+  }
+
+  file { $bindpw_file:
+    ensure  => present,
+    owner   => $mgapeople_login,
+    group   => $mgapeople_login,
+    mode    => '0600',
+    content => $bindpw,
+  }
+
+  package { 'mgapeople':
+    ensure => installed,
+  }
+
+  file {'/etc/mgapeople.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('mgapeople/mgapeople.conf'),
+    require => Package['mgapeople'],
+  }
+
+  file { $vhostdir:
+    ensure => directory,
+    owner  => $mgapeople_login,
+    group  => $mgapeople_login,
+    mode   => '0755',
+  }
+
+  apache::vhost::base { $vhost:
+    location => $vhostdir,
+    require  => File[$vhostdir],
+  }
+}
+# vim: sw=2
