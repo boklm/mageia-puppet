a8cc86afba353c20f682762fdd05fe59e968ca79
Nicolas Vigier <boklm@mageia.org>
2013-04-22 17:47:14 +0000

    sympa/scenario/sender_restricted.pp: fix syntax
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3057 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/sympa/manifests/scenario/sender_restricted.pp
+++ modules/sympa/manifests/scenario/sender_restricted.pp
@@ -1,7 +1,7 @@
 define sympa::scenario::sender_restricted(
     $email            = false,
-    $ldap_group       = false
-    $allow_subscriber = false,
+    $ldap_group       = false,
+    $allow_subscriber = false
 ) {
     file { "/etc/sympa/scenari/send.restricted_$name":
         content => template('sympa/scenari/sender.restricted')
