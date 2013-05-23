c33d2a33b0de7bfeb1054bc68e9645184ee9883c
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:05:08 +0000

    buildsystem::var::distros: add parameters description
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3130 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/var/distros.pp
+++ modules/buildsystem/manifests/var/distros.pp
@@ -1,3 +1,8 @@
+# $distros:
+#   a hash variable containing distributions informations indexed by
+#   distribution name. Each distribution is itself an hash containing
+#   the following keys:
+#     - medias: an hash containing the different medias / repositories
 class buildsystem::var::distros(
     $distros
 ) { }
