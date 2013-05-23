d9e517923652a0de6f2b93617e06faaec1e0fffd
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:05:20 +0000

    buildsystem::var::distros: Add option to select arch
    Add an option to select supported arch of a distribution.
    
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3134 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/mga_buildsystem/manifests/config.pp
+++ deployment/mga_buildsystem/manifests/config.pp
@@ -19,6 +19,7 @@ class mga_buildsystem::config {
 	uploadmail_to => "packages-commits@ml.${::domain}",
     }
 
+    $std_arch = ['i586', 'x86_64']
     $std_repos = ['release','updates_testing','backports_testing','backports','updates']
     $std_medias = {
 	'core'    => $std_repos,
@@ -32,26 +33,31 @@ class mga_buildsystem::config {
     class { 'buildsystem::var::distros':
 	distros => {
 	    'cauldron' => {
+		'arch' => $std_arch,
 		'medias' => $std_medias,
 		'base_media' => $std_base_media,
 	    },
 
 	    '1'        => {
+		'arch' => $std_arch,
 		'medias' => $std_medias,
 		'base_media' => $std_base_media,
 	    },
 
 	    '2'        => {
+		'arch' => $std_arch,
 		'medias' => $std_medias,
 		'base_media' => $std_base_media,
 	    },
 
 	    'infra_1'  => {
+		'arch' => $std_arch,
 		'medias' => $infra_medias,
 		'base_media' => $std_base_media,
 	    },
 
 	    'infra_2'  => {
+		'arch' => $std_arch,
 		'medias' => $infra_medias,
 		'base_media' => $std_base_media,
 	    },
--- modules/buildsystem/manifests/var/distros.pp
+++ modules/buildsystem/manifests/var/distros.pp
@@ -5,6 +5,7 @@
 #     - medias: an hash containing the different medias / repositories
 #     - base_media: a list of medias that will be used by iurt to build
 #       the chroots
+#     - arch: list of arch supported by this distro
 class buildsystem::var::distros(
     $distros
 ) { }
