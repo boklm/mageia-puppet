c6d51581584f4fd5c81f9d48d34023b462617811
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:06:05 +0000

    buildsystem::repoctl: reindent
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3150 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/repoctl.pp
+++ modules/buildsystem/manifests/repoctl.pp
@@ -1,16 +1,16 @@
 class buildsystem::repoctl {
-	include buildsystem::config
+    include buildsystem::config
 
-	$distroreleases = [$buildsystem::config::dev_distros,
-	                   $buildsystem::config::stable_distros]
-	$distrosections = $buildsystem::config::distrosections
-	$sectionsrepos = $buildsystem::config::sectionsrepos
-	$arches = $buildsystem::config::architectures
+    $distroreleases = [$buildsystem::config::dev_distros,
+    $buildsystem::config::stable_distros]
+    $distrosections = $buildsystem::config::distrosections
+    $sectionsrepos = $buildsystem::config::sectionsrepos
+    $arches = $buildsystem::config::architectures
 
-	package{ 'repoctl': }
+    package{ 'repoctl': }
 
-	file { '/etc/repoctl.conf':
-	    content => template('buildsystem/repoctl.conf'),
-	    require => Package['repoctl'],
-	}
+    file { '/etc/repoctl.conf':
+	content => template('buildsystem/repoctl.conf'),
+	require => Package['repoctl'],
+    }
 }
