8873a211c47c595cf3a7b231c8c196462280fd45
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:06:49 +0000

    buildsystem::var::webstatus: fix svn url
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3164 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/var/webstatus.pp
+++ modules/buildsystem/manifests/var/webstatus.pp
@@ -1,5 +1,5 @@
 class buildsystem::var::webstatus(
-    $svn_url = "svn://svn.mageia.org/soft/buildsystem/web/",
+    $svn_url = "svn://svn.mageia.org/soft/build_system/web/",
     $hostname = "pkgsubmit.$::domain",
     $location = '/var/www/bs'
 ) {
