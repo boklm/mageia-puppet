06149a69c9e84d52cd258e7b82beedc2e4be22a7
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:07:32 +0000

    buildsystem::var::webstatus: add comments
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3180 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/var/webstatus.pp
+++ modules/buildsystem/manifests/var/webstatus.pp
@@ -1,3 +1,9 @@
+# $svn_url:
+#   svn url where the sources of webstatus are located
+# $hostname:
+#   vhost name of the webstatus page
+# $location:
+#   path of the directory where the webstatus files are located
 class buildsystem::var::webstatus(
     $svn_url = "svn://svn.mageia.org/soft/build_system/web/",
     $hostname = "pkgsubmit.$::domain",
