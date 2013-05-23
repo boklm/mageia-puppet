0179c980dc9ae12b12183015635304c4eb566490
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:05:17 +0000

    buildsystem: use webstatus hostname config in iurt.conf
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3133 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/buildsystem/manifests/iurt/config.pp
+++ modules/buildsystem/manifests/iurt/config.pp
@@ -1,5 +1,6 @@
 define buildsystem::iurt::config() {
     include buildsystem::var::iurt
+    include buildsystem::var::webstatus
     $distribution = $name
     # TODO rename the variable too in template
     $build_login = $buildsystem::var::iurt::login
