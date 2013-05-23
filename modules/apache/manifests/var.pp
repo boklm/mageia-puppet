3b5d8f6bef63ac15e4dfb0dbebdc6100a411b435
Nicolas Vigier <boklm@mageia.org>
2013-05-23 15:07:42 +0000

    apache: add option to select default redirect URL
    Add an option to change the URL to redirect to in case of unknown vhost.
    If the option is not provided, a 404 error is returned.
    
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3184 db665093-e049-48a9-b3c2-13f1584ce46d

--- modules/apache/manifests/var.pp
+++ modules/apache/manifests/var.pp
@@ -1,9 +1,12 @@
 # $httpdlogs_rotate:
 #   number of time the log file are rotated before being removed
+# $default_vhost_redirect:
+#   URL to redirect to in case of unknown vhost
 class apache::var(
     $httpdlogs_rotate = '24',
     $apache_user = 'apache',
-    $apache_group = 'apache'
+    $apache_group = 'apache',
+    $default_vhost_redirect = ''
 ) {
     if ($::lsbdistrelease == '1') or ($::lsbdistid == 'MandrivaLinux') {
         $pkg_conf = 'apache-conf'
