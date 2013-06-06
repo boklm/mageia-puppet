8458eea97309f32ce02d0c7509e83f5a3d3b4a50
Nicolas Vigier <boklm@mageia.org>
2013-06-06 19:08:28 +0000

    Add mga-advisories module
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3233 db665093-e049-48a9-b3c2-13f1584ce46d

---
+++ modules/mga-advisories/manifests/init.pp
@@ -0,0 +1,79 @@
+class mga-advisories(
+  $advisories_svn = 'svn://svn.mageia.org/svn/advisories',
+  $vhost,
+){
+  $mgaadv_login = 'mga-advisories'
+  $mgaadv_homedir = "/var/lib/${mgaadv_login}"
+  $vhostdir = "${mgaadv_homedir}/vhost"
+  $advisories_dir = "${mgaadv_homedir}/advisories"
+  $status_dir = "${mgaadv_homedir}/status"
+  $update_script = '/usr/local/bin/update_mga-advisories'
+
+  group { $mgaadv_login:
+    ensure => present,
+  }
+
+  user { $mgaadv_login:
+    ensure     => present,
+    comment    => 'mga-advisories user',
+    home       => $mgaadv_homedir,
+    managehome => true,
+    gid        => $mgaadv_login,
+  }
+
+  package { 'mga-advisories':
+    ensure => installed,
+  }
+
+  file {'/etc/mga-advisories.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('mga-advisories/mga-advisories.conf'),
+    require => Package['mga-advisories'],
+  }
+
+  file { [ $vhostdir, $status_dir ]:
+    ensure => directory,
+    owner  => $mgaadv_login,
+    group  => $mgaadv_login,
+    mode   => '0755',
+  }
+
+  apache::vhost::base { $vhost:
+    location => $vhostdir,
+    require  => File[$vhostdir],
+  }
+
+  apache::vhost::base { "ssl_$vhost":
+    use_ssl  => true,
+    vhost    => $vhost,
+    location => $vhostdir,
+    require  => File[$vhostdir],
+  }
+
+  subversion::snapshot { $advisories_dir:
+    source  => $advisories_svn,
+    user    => $mgaadv_login,
+    refresh => '0',
+    require => User[$mgaadv_login],
+  }
+
+  file { $update_script:
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('mga-advisories/update_script'),
+  }
+
+  cron { $update_script:
+    command => $update_script,
+    user    => $mgaadv_login,
+    hour    => '*',
+    minute  => '10',
+    require => Subversion::Snapshot[$advisories_dir],
+  }
+}
+# vim: sw=2
