d00b8f92324adf76340b03eba934b38d63d1b39d
Nicolas Vigier <boklm@mageia.org>
2013-06-27 18:39:06 +0000

    Also disable sparkleshare git repo
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3270 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/sparkleshare.pp
+++ deployment/repositories/manifests/sparkleshare.pp
@@ -1,11 +1,11 @@
 class repositories::sparkleshare {
-    $sp_dir = '/git/sparkleshare'
-    file { $sp_dir:
-        ensure => directory,
-    }
+    #$sp_dir = '/git/sparkleshare'
+    #file { $sp_dir:
+    #    ensure => directory,
+    #}
 
-    git::repository { "$sp_dir/test":
-        description => 'Test sparkleshare repository',
-        group       => 'mga-packagers-committers',
-    }
+    #git::repository { "$sp_dir/test":
+    #    description => 'Test sparkleshare repository',
+    #    group       => 'mga-packagers-committers',
+    #}
 }
