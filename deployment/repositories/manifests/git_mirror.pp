cbca5ff6e34c211fb99cde930d4d98f3af2d2664
Nicolas Vigier <boklm@mageia.org>
2013-06-27 18:35:50 +0000

    Disable old git repositories
    git-svn-id: svn+ssh://svn.mageia.org/svn/adm/puppet@3269 db665093-e049-48a9-b3c2-13f1584ce46d

--- deployment/repositories/manifests/git_mirror.pp
+++ deployment/repositories/manifests/git_mirror.pp
@@ -1,16 +1,16 @@
 class repositories::git_mirror {
-    file { '/git':
-        ensure => directory,
-    }
+    #file { '/git':
+    #    ensure => directory,
+    #}
 
-    git::mirror { '/git/forum/':
-        description => "Reference code for forum.$::domain",
-        source      => "git://git.$::domain/forum/",
-    }
+    #git::mirror { '/git/forum/':
+    #    description => "Reference code for forum.$::domain",
+    #    source      => "git://git.$::domain/forum/",
+    #}
 
-    git::mirror { '/git/initscripts/':
-        description => 'Reference code for Initscripts',
-        source      => "git://git.$::domain/initscripts/",
-    }
+    #git::mirror { '/git/initscripts/':
+    #    description => 'Reference code for Initscripts',
+    #    source      => "git://git.$::domain/initscripts/",
+    #}
 }
 
